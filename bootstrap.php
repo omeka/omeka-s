aa18c13071fe7da43cde629e3eee262cd0b0cc5a
Jim Safley <jimsafley@gmail.com>
2013-08-16 16:48:25 -0400

    Add table prefixing.

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,8 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
+use Doctrine\ORM\Events;
+use Doctrine\Common\Persistence\Event\LoadClassMetadataEventArgs;
 
 ini_set('display_errors', 1);
 require_once 'vendor/autoload.php';
@@ -12,4 +14,30 @@ $isDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . '/src'), $isDevMode);
 $config->setNamingStrategy(new UnderscoreNamingStrategy(CASE_LOWER));
 
+/**
+ * Prefix table names.
+ */
+class TablePrefix
+{
+    protected $prefix = '';
+
+    public function __construct($prefix)
+    {
+        $this->prefix = (string) $prefix;
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+        $classMetadata->setTableName($this->prefix . $classMetadata->getTableName());
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+                $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+}
+
 $em = EntityManager::create($conn, $config);
+$em->getEventManager()->addEventListener(Events::loadClassMetadata, new TablePrefix('omeka_'));

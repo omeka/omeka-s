4d0453727f4c6aab1ac9c8842a484903c5312d68
patrickmj <patrickmjchnm@gmail.com>
2013-08-23 15:19:58 -0400

    Add basic phpunit structure

---
+++ module/Omeka/test/OmekaTest/OmekaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OmekaTest\Controller;
+
+use PHPUnit_Framework_TestCase;
+
+class OmekaTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function testCanDoUnitTest() {
+        $this->assertTrue(true);
+    }
+}

f8feb149ebcae04f7de8c26a7150d1590aaec380
Jim Safley <jimsafley@gmail.com>
2013-09-18 14:56:41 -0400

    Continue writing tests.

--- module/Omeka/test/OmekaTest/Api/ApiManagerTest.php
+++ module/Omeka/test/OmekaTest/Api/ApiManagerTest.php
@@ -70,6 +70,32 @@ class ApiManagerTest extends \PHPUnit_Framework_TestCase
         $manager->registerResource('foo', $config);
     }
 
+    public function testGetResource()
+    {
+        $manager = new Manager;
+        $manager->registerResource('foo', $this->config);
+        $this->assertEquals($this->config, $manager->getResource('foo'));
+    }
+
+    public function testGetResources()
+    {
+        $manager = new Manager;
+        $manager->registerResource('foo', $this->config);
+        $manager->registerResource('bar', $this->config);
+        $this->assertEquals(array(
+            'foo' => $this->config,
+            'bar' => $this->config,
+        ), $manager->getResources());
+    }
+
+    public function testIsRegisteredWorks()
+    {
+        $manager = new Manager;
+        $manager->registerResource('foo', $this->config);
+        $this->assertTrue($manager->isRegistered('foo'));
+        $this->assertFalse($manager->isRegistered('bar'));
+    }
+
     public function testSetsAndGetsServiceLocator()
     {
         $manager = new Manager;
@@ -81,19 +107,32 @@ class ApiManagerTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testExecutes()
+    public function testGetAdapter()
     {
-        //~ $manager = new Manager;
-        //~ $manager->registerResource('foo', array(
-            //~ 'adapter_class' => 'Omeka\Api\Adapter\Db',
-            //~ 'adapter_data' => array(
-                //~ 'entity_class' => 'Omeka\Model\Entity\Foo',
-            //~ ),
-            //~ 'functions' => array(
-                //~ \Omeka\Api\Request::SEARCH,
-            //~ ),
-        //~ ));
-        //~ $request = $this->getMock('Omeka\Api\Request');
+        $manager = new Manager;
+        $manager->registerResource('foo', $this->config);
+        $mockServiceLocator = $this->getMock('Zend\ServiceManager\ServiceLocatorInterface');
+        $manager->setServiceLocator($mockServiceLocator);
+        $adapter = $manager->getAdapter('foo');
+        $this->assertInstanceOf('Omeka\Api\Adapter\AdapterInterface', $adapter);
+        $this->assertEquals($this->config['adapter_data'], $adapter->getData());
+        $this->assertSame($mockServiceLocator, $adapter->getServiceLocator());
     }
 
+    /**
+     * @expectedException Omeka\Api\Exception\InvalidRequestException
+     */
+    public function testExecuteRequiresRegisteredOperation()
+    {
+        $request = $this->getMock('Omeka\Api\Request');
+        $request->setResource('foo');
+        $request->expects($this->any())
+                ->method('getResource')
+                ->will($this->returnValue('foo'));
+        $request->expects($this->any())
+                ->method('getOperation')
+                ->will($this->returnValue('bar'));
+        $manager = new Manager;
+        $manager->execute($request);
+    }
 }

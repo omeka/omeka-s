39074af5bad600f601a9fb196c3c8ecb45706087
patrickmj <patrickmjchnm@gmail.com>
2013-09-19 13:08:48 -0400

    Adds cli installer and a couple simple tests

---
+++ cli-install.php
@@ -0,0 +1,28 @@
+<?php
+require('cli-config.php');
+
+use Omeka\Install\Installer;
+use Omeka\Service\EntityManagerFactory;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
+
+$factory = new EntityManagerFactory;
+$config = include 'config/application.config.php';
+$em = $factory->createEntityManager($config['entity_manager']);
+
+$serviceManager = new ServiceManager(new ServiceManagerConfig());
+$serviceManager->setService('ApplicationConfig', $config);
+$serviceManager->setService('EntityManager', $em);
+$serviceManager->get('ModuleManager')->loadModules();
+
+$installer = new Installer;
+$installer->setServiceLocator($serviceManager);
+$installer->loadTasks();
+$success = $installer->install();
+if($success) {
+    echo 'ok';    
+} else {
+    echo 'fail';   
+    print_r($installer->getMessages());
+}
+
---
+++ module/Omeka/test/OmekaTest/Install/InstallerTest.php
@@ -0,0 +1,74 @@
+<?php
+namespace OmekaTest\Install;
+
+use Omeka\Install\Installer;
+use Omeka\Install\TaskAbstract;
+use Omeka\Install\TaskResult;
+use OmekaTest\Bootstrap;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class FailPerformTask extends TaskAbstract
+{
+    public function perform() {
+        $this->result->setSuccess(false);
+    }
+};
+
+class SuccessPerformTask extends TaskAbstract
+{
+    public function perform() {
+        $this->result->setSuccess(true);
+    }
+};
+
+
+
+class InstallTest extends \PHPUnit_Framework_TestCase
+{
+    protected $tasks;
+    
+    protected function setUp()
+    {
+        $this->tasks['successPerformTask'] = new SuccessPerformTask;
+        $this->tasks['failPerformTask'] = new FailPerformTask;
+        parent::setUp();
+    }
+    
+    /**
+     * Make sure that despite successes in tasks, one fail means install fails
+     */
+    public function testInstallFailsOnTaskFail()
+    {
+        $installer = new Installer;
+        $manager = Bootstrap::getServiceManager();
+        $installer->setServiceLocator($manager);
+        $installer->addTask($this->tasks['successPerformTask']);
+        $installer->addTask($this->tasks['failPerformTask']);
+        $installer->addTask($this->tasks['successPerformTask']);
+        $status = $installer->install();
+        $this->assertFalse($status);
+    }
+    
+    public function testInstallSucceedsOnTasksSucceed()
+    {
+        $installer = new Installer;
+        $manager = Bootstrap::getServiceManager();
+        $installer->setServiceLocator($manager);
+        $installer->addTask($this->tasks['successPerformTask']);
+        $status = $installer->install();
+        $this->assertTrue($status);        
+    }
+    
+    /**
+     * Make sure tasks listed in application config have classes built for them.
+     * Check the inline comments in application.config.php for what the classes should do
+     */
+    public function testTasksHaveClasses()
+    {
+        $appConfig = Bootstrap::getApplicationConfig();
+        $taskNames = $appConfig['install_tasks'];
+        foreach($taskNames as $taskName) {
+            $this->assertTrue(class_exists($taskName), "$taskName class does not exist");
+        }
+    }
+}

9a77a7fed3904d870919d3dda940ae53b03994a6
Jim Safley <jimsafley@gmail.com>
2013-10-14 16:58:34 -0400

    Check uniqueness on more than one column.

--- module/Omeka/src/Omeka/Validator/Db/IsUnique.php
+++ module/Omeka/src/Omeka/Validator/Db/IsUnique.php
@@ -15,9 +15,14 @@ class IsUnique extends AbstractValidator
     const INVALID_FIELD = 'invalidField';
 
     /**
+     * @var array
+     */
+    protected $fields;
+
+    /**
      * @var string
      */
-    protected $field;
+    protected $values;
 
     /**
      * @var EntityInterface
@@ -33,26 +38,25 @@ class IsUnique extends AbstractValidator
      * @var array
      */
     protected $messageVariables = array(
-        'field' => 'field',
-        'value' => 'value',
+        'values' => 'values',
     );
 
     /**
      * @var array
      */
     protected $messageTemplates = array(
-        self::NOT_UNIQUE => 'The value "%value%" is not unique for the "%field%" field.',
+        self::NOT_UNIQUE => 'The value "%values%" is not unique for the "%value%" field.',
         self::INVALID_ENTITY => 'Invalid entity "%value%" passed to IsUnique validator.',
         self::INVALID_FIELD => 'Invalid field "%value%" passed to IsUnique validator.',
     );
 
     /**
-     * @param string $field The entity field to check for uniqueness
+     * @param array $fields The entity fields to check for uniqueness
      * @param EntityManager $entityManager
      */
-    public function __construct($field, EntityManager $entityManager)
+    public function __construct(array $fields, EntityManager $entityManager)
     {
-        $this->field = $field;
+        $this->fields = array_values($fields);
         $this->entityManager = $entityManager;
         parent::__construct();
     }
@@ -71,38 +75,33 @@ class IsUnique extends AbstractValidator
         }
 
         $classMetadata = $this->entityManager->getClassMetadata(get_class($entity));
-        // Check whether the passed field belongs to the passed entity. This
-        // prevents SQL injection of malicious user data.
-        if (!in_array($this->field, $classMetadata->fieldNames)) {
-            $this->error(self::INVALID_FIELD, $this->field);
-            return false;
+        foreach ($this->fields as $field) {
+            if (!in_array($field, $classMetadata->fieldNames)) {
+                $this->error(self::INVALID_FIELD, $field);
+                return false;
+            }
         }
 
-        // Set the value as the value of the specified field of the entity. The
-        // field must have a corresponding get*() method.
-        $getField = 'get' . ucfirst($this->field);
-        $value = $entity->$getField();
-        $this->setValue($value);
-
         $qb = $this->entityManager->createQueryBuilder();
-        // Check uniqueness on an entity that is not yet persistent. In this
-        // case, a value is unique if no entity has the specified field equal
-        // to the assigned value.
-        $qb->select('1')
-           ->from($classMetadata->name, 'entity')
-           ->where($qb->expr()->eq("entity.{$this->field}", ':value'))
-           ->setParameter('value', $value);
+        // Check uniqueness on an entity that is not yet persistent.
+        $qb->select('1')->from($classMetadata->name, 'entity');
+        $values = array();
+        foreach ($this->fields as $alias => $field) {
+            $getField = 'get' . ucfirst($field);
+            $values[$alias] = $entity->$getField();
+            $qb->andWhere($qb->expr()->eq("entity.$field", "?$alias"))
+                ->setParameter($alias, $values[$alias]);
+        }
+        $this->values = implode(', ', $values);
 
-        // Check uniqueness on a persistent entity. In this case, a value is
-        // unique if no entity, other than the persistent entity itself, has the
-        // specified field equal to the assigned value.
+        // Check uniqueness on a persistent entity.
         if (null !== $entity->getId()) {
             $qb->andWhere($qb->expr()->neq('entity.id', ':id'))
                ->setParameter('id', $entity->getId());
         }
 
         if ($qb->getQuery()->getOneOrNullResult()) {
-            $this->error(self::NOT_UNIQUE);
+            $this->error(self::NOT_UNIQUE, implode(', ', $this->fields));
             return false;
         }
         return true;

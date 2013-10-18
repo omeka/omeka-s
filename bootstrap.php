f5da536b6062bd75dc470c80824e30a8eac7e41c
Jim Safley <jimsafley@gmail.com>
2013-10-18 12:55:02 -0400

    Remove redundant init_autoloader file.

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', true);
 
-require 'init_autoloader.php';
+require 'vendor/autoload.php';
 
 $application = Zend\Mvc\Application::init(require 'config/application.config.php');
 $em = $application->getServiceManager()->get('EntityManager');
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', true);
 
-require 'init_autoloader.php';
+require 'vendor/autoload.php';
 
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();

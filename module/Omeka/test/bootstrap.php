50f11c5b27c4418b57603eea5472c226eb73f6e8
Jim Safley <jimsafley@gmail.com>
2013-11-11 12:40:28 -0500

    Specify that the test bootstrap is dropping/creating the test database.

--- module/Omeka/test/bootstrap.php
+++ module/Omeka/test/bootstrap.php
@@ -2,7 +2,7 @@
 require '../../../bootstrap.php';
 
 // Install a fresh database.
-echo 'Dropping database schema...' . PHP_EOL;
+echo 'Dropping test database schema...' . PHP_EOL;
 \Omeka\Test\DbTestCase::dropSchema();
-echo 'Creating database schema...' . PHP_EOL;
+echo 'Creating test database schema...' . PHP_EOL;
 \Omeka\Test\DbTestCase::installSchema();

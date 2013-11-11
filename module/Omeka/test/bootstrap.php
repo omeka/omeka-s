ce72ea1f92cbab9f1eca70cdf857490f7e33eca2
Jim Safley <jimsafley@gmail.com>
2013-11-11 10:40:45 -0500

    Show db drop/create notices during test bootstrap.

--- module/Omeka/test/bootstrap.php
+++ module/Omeka/test/bootstrap.php
@@ -2,5 +2,7 @@
 require '../../../bootstrap.php';
 
 // Install a fresh database.
+echo 'Dropping database schema...' . PHP_EOL;
 \Omeka\Test\DbTestCase::dropSchema();
+echo 'Creating database schema...' . PHP_EOL;
 \Omeka\Test\DbTestCase::installSchema();

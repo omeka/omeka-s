3a5971300d7210d8f717b979a759b940221c9975
John Flatness <john@zerocrates.org>
2013-12-12 14:36:49 -0500

    Add column support to table resolver.

--- module/Omeka/src/Omeka/Db/Migration/TableResolver.php
+++ module/Omeka/src/Omeka/Db/Migration/TableResolver.php
@@ -27,9 +27,24 @@ class TableResolver
 
     /**
      * Get the table name for the given entity.
+     *
+     * @param string $entityName Name of the Entity
+     * @return string Name of the underlying SQL table.
      */
     public function getTableName($entityName)
     {
         return $this->em->getClassMetadata($entityName)->getTableName();
+    }
+
+    /**
+     * Get the column name for the given field of an entity.
+     *
+     * @param string $entityName Name of the Entity
+     * @param string $fieldName Name of the field
+     * @return string Name of the underlying SQL column.
+     */
+    public function getColumnName($entityName, $fieldName)
+    {
+        return $this->em->getClassMetadata($entityName)->getColumnName($fieldName);
     }
 }

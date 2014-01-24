f6991e05caa14105ecacebcacc1ab0c1ea696eaf
patrickmj <patrickmjchnm@gmail.com>
2014-01-24 15:39:05 -0500

    Add browse and add views and controller actions

---
+++ module/Omeka/src/Omeka/Controller/Admin/UserController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Omeka\Controller\Admin;
+
+use Omeka\Api\ResponseFilter;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+    public function indexAction()
+    {}
+
+    public function addAction()
+    {
+        $api = $this->getServiceLocator()->get('ApiManager');
+        if ($this->getRequest()->isPost()) {
+            $data = $this->getRequest()->getPost()->toArray();
+            $response = $api->create('users', $data);
+            if ($response->isError()) {
+                print_r($response->getErrors());
+            }
+            $user = $response->getContent();
+            return new ViewModel(array('user' => $user));
+        }
+    }
+
+    public function browseAction()
+    {
+        $api = $this->getServiceLocator()->get('ApiManager');
+        $filter = new ResponseFilter();
+        $response = $api->search('users', array());
+        $users = $response->getContent();
+        return new ViewModel(array('users'=>$users));
+    }
+}

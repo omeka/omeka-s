2306857deeec80a577c1007071d986f66e7f64c1
John Flatness <john@zerocrates.org>
2014-02-14 17:33:14 -0500

    Add Omeka-specific auth components.

---
+++ module/Omeka/src/Omeka/Authentication/Storage/DoctrineWrapper.php
@@ -0,0 +1,115 @@
+<?php
+namespace Omeka\Authentication\Storage;
+
+use Doctrine\ORM\EntityRepository;
+use Zend\Authentication\Storage\StorageInterface;
+
+/**
+ * Auth storage wrapper for doctrine objects.
+ *
+ * Stores the ID instead of the full object, translates between ID and object
+ * automatically on read/write.
+ */
+class DoctrineWrapper implements StorageInterface
+{
+    /**
+     * @var StorageInterface
+     */
+    protected $storage;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repository;
+
+    /**
+     * Create the wrapper around the given storage method, looking up users
+     * from the given repository.
+     *
+     * @param StorageInterface $storage "Base" storage class
+     * @param EntityRepository $repository Repository storing Users
+     */
+    public function __construct(StorageInterface $storage, EntityRepository $repository)
+    {
+        $this->setStorage($storage);
+        $this->setRepository($repository);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isEmpty()
+    {
+        return $this->storage->isEmpty();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function read()
+    {
+        $identity = $this->storage->read();
+
+        if ($identity) {
+            return $this->repository->find($identity);
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function write($identity)
+    {
+        $this->storage->write($identity->getId());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clear()
+    {
+        $this->storage->clear();
+    }
+
+    /**
+     * Set the base storage class to wrap.
+     *
+     * @param StorageInterface $storage
+     */
+    public function setStorage(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    /**
+     * Get the storage class being wrapped.
+     *
+     * @return StorageInterface
+     */
+    public function getStorage()
+    {
+        return $this->storage;
+    }
+
+    /**
+     * Set the repository for looking up User objects.
+     *
+     * @param EntityRepository $repository
+     */
+    public function setRepository(EntityRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Get the repository for looking up User objects.
+     *
+     * @return EntityRepository
+     */
+    public function getRepository()
+    {
+        return $this->repository;
+    }
+}

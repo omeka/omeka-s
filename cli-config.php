518fb05ec62c44bb7908a6cd72e3374bee2c58f3
Jim Safley <jimsafley@gmail.com>
2014-03-25 18:02:54 -0400

    Init the Omeka application when using Doctrine schema tools.

--- cli-config.php
+++ cli-config.php
@@ -1,7 +1,7 @@
 <?php
 require 'bootstrap.php';
 
-$application = Zend\Mvc\Application::init(require 'config/application.config.php');
+$application = Omeka\Mvc\Application::init(require 'config/application.config.php');
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
     $application->getServiceManager()->get('Omeka\EntityManager')

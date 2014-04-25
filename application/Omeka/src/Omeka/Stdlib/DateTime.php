81a1172dc8e1834c4c2ddd00c111e6b4872fd381
Jim Safley <jimsafley@gmail.com>
2014-04-25 13:41:50 -0400

    Set current datetime when none passed.

--- application/Omeka/src/Omeka/Stdlib/DateTime.php
+++ application/Omeka/src/Omeka/Stdlib/DateTime.php
@@ -3,6 +3,9 @@ namespace Omeka\Stdlib;
 
 use Zend\Stdlib\JsonSerializable;
 
+/**
+ * A wrapper of PHP's DateTime that can be serialized as JSON.
+ */
 class DateTime implements JsonSerializable
 {
     /**
@@ -11,17 +14,21 @@ class DateTime implements JsonSerializable
     protected $dateTime;
 
     /**
-     * @param PhpDateTime $dateTime
+     * @param \DateTime|null $dateTime Null will set the current DateTime
      */
-    public function __construct(\DateTime $dateTime)
+    public function __construct(\DateTime $dateTime = null)
     {
+        if (null === $dateTime) {
+            // Set current date and time
+            $dateTime = new \DateTime;
+        }
         $this->dateTime = $dateTime;
     }
 
     /**
      * Get the underlying DateTime instance.
      *
-     * @return PhpDateTime
+     * @return \DateTime
      */
     public function getDateTime()
     {
@@ -29,7 +36,9 @@ class DateTime implements JsonSerializable
     }
 
     /**
-     * {@inheritDoc}
+     * Serialize DateTime as an ISO 8601 date.
+     *
+     * @return string
      */
     public function jsonSerialize()
     {

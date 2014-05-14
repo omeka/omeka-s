974b52caeb553b49696a14d47742a7c84246b84a
Jim Safley <jimsafley@gmail.com>
2014-05-14 11:28:36 -0400

    Require application/json Content-Type for certain request methods.

--- application/Omeka/src/Omeka/Controller/ApiController.php
+++ application/Omeka/src/Omeka/Controller/ApiController.php
@@ -2,7 +2,6 @@
 namespace Omeka\Controller;
 
 use Omeka\Api\Response;
-use Omeka\Api\Request;
 use Omeka\View\Model\ApiJsonModel;
 use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\Mvc\MvcEvent;
@@ -10,77 +9,119 @@ use Zend\Mvc\MvcEvent;
 class ApiController extends AbstractRestfulController
 {
     /**
-     * @var ApiJsonModel
+     * @var array
      */
-    protected $viewModel;
+    protected $viewOptions = array();
 
+    /**
+     * {@inheritDoc}
+     */
     public function get($id)
     {
         $resource = $this->params()->fromRoute('resource');
         $response = $this->api()->read($resource, $id);
-        $this->viewModel->setApiResponse($response);
-        return $this->viewModel;
+        return new ApiJsonModel($response, $this->getViewOptions());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getList()
     {
         $resource = $this->params()->fromRoute('resource');
         $data = $this->params()->fromQuery();
         $response = $this->api()->search($resource, $data);
-        $this->viewModel->setApiResponse($response);
-        return $this->viewModel;
+        return new ApiJsonModel($response, $this->getViewOptions());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function create($data)
     {
         $resource = $this->params()->fromRoute('resource');
         $response = $this->api()->create($resource, $data);
-        $this->viewModel->setApiResponse($response);
-        return $this->viewModel;
+        return new ApiJsonModel($response, $this->getViewOptions());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function update($id, $data)
     {
         $resource = $this->params()->fromRoute('resource');
         $response = $this->api()->update($resource, $id, $data);
-        $this->viewModel->setApiResponse($response);
-        return $this->viewModel;
+        return new ApiJsonModel($response, $this->getViewOptions());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function delete($id)
     {
         $resource = $this->params()->fromRoute('resource');
         $response = $this->api()->delete($resource, $id);
-        $this->viewModel->setApiResponse($response);
-        return $this->viewModel;
-    }
-
-    public function attachDefaultListeners()
-    {
-        $events = $this->getEventManager();
-        $events->attach(MvcEvent::EVENT_DISPATCH, array($this, 'setViewModel'));
-        parent::attachDefaultListeners();
+        return new ApiJsonModel($response, $this->getViewOptions());
     }
 
     /**
-     * Set the API JSON view model and its options.
+     * Validate the API request and set global options.
      *
-     * @param MvcEvent $e
+     * @param MvcEvent $event
      */
-    public function setViewModel(MvcEvent $e)
+    public function onDispatch(MvcEvent $event)
     {
-        $viewModel = new ApiJsonModel;
+        // Require application/json Content-Type for certain methods.
+        $method = strtolower($this->getRequest()->getMethod());
+        $contentType = $this->getRequest()->getHeader('Content-Type');
+        if (in_array($method, array('post', 'put', 'patch'))
+            && 'application/json' !== $contentType->getMediaType()
+        ) {
+            $response = new Response;
+            $response->setStatus(Response::ERROR_BAD_REQUEST);
+            $response->addError(Response::ERROR_BAD_REQUEST, sprintf(
+                'Invalid Content-Type header. Expecting "application/json", got "%s".',
+                $contentType->getMediaType()
+            ));
+            $return = new ApiJsonModel($response);
+            $event->setResult($return);
+            return $return;
+        }
 
+        // Set pretty print.
         $prettyPrint = $this->getRequest()->getQuery('pretty_print');
         if (null !== $prettyPrint) {
-            $viewModel->setOption('pretty_print', true);
+            $this->setViewOption('pretty_print', true);
         }
 
+        // Set the JSON-P callback.
         $callback = $this->getRequest()->getQuery('callback');
         if (null !== $callback) {
-            $viewModel->setOption('callback', $callback);
+            $this->setViewOption('callback', $callback);
         }
 
-        $this->viewModel = $viewModel;
+        // Finish dispatching the request.
+        parent::onDispatch($event);
+    }
+
+    /**
+     * Set a view model option.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function setViewOption($key, $value)
+    {
+        $this->viewOptions[$key] = $value;
+    }
+
+    /**
+     * Get all view options.
+     *
+     * return array
+     */
+    public function getViewOptions()
+    {
+        return $this->viewOptions;
     }
 }

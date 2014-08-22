e1dc8a47c115e29e57d99b28dab7b652cd6044bc
Jim Safley <jimsafley@gmail.com>
2014-08-21 21:53:15 -0400

    Add more functionality to item browse. Remove legacy markup.

--- application/Omeka/config/module.config.php
+++ application/Omeka/config/module.config.php
@@ -255,10 +255,11 @@ return array(
                     'default' => array(
                         'type' => 'Segment',
                         'options' => array(
-                            'route' => '/[:controller[/:action][/]]',
+                            'route' => '/[:controller[/:action][/[:id]]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'         => '\d+',
                             ),
                         ),
                     ),

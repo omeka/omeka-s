9bfeb2d84846b0444168061a2941f857dd71bce2
Jim Safley <jimsafley@gmail.com>
2014-09-25 14:51:05 -0400

    Use passthrough methods in API controller plugin. Add method to get first result from search.

---
+++ application/Omeka/test/OmekaTest/Mvc/Controller/Plugin/ApiTest.php
@@ -0,0 +1,201 @@
+<?php
+namespace OmekaTest\Mvc\Controller\Plugin;
+
+use Omeka\Mvc\Controller\Plugin\Api;
+use Omeka\Test\TestCase;
+
+class ApiTest extends TestCase
+{
+    public function testSearch()
+    {
+        $resource = 'test-resource';
+        $data = array('foo' => 'bar');
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('search')
+            ->with($this->equalTo($resource), $this->equalTo($data));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $api->search($resource, $data);
+    }
+
+    public function testCreate()
+    {
+        $resource = 'test-resource';
+        $data = array('foo' => 'bar');
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('create')
+            ->with($this->equalTo($resource), $this->equalTo($data));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $api->create($resource, $data);
+    }
+
+    public function testBatchCreate()
+    {
+        $resource = 'test-resource';
+        $data = array('foo' => 'bar');
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('batchCreate')
+            ->with($this->equalTo($resource), $this->equalTo($data));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $api->batchCreate($resource, $data);
+    }
+
+    public function testRead()
+    {
+        $resource = 'test-resource';
+        $id = 'test-id';
+        $data = array('foo' => 'bar');
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('read')
+            ->with($this->equalTo($resource), $this->equalTo($id), $this->equalTo($data));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $api->read($resource, $id, $data);
+    }
+
+    public function testUpdate()
+    {
+        $resource = 'test-resource';
+        $id = 'test-id';
+        $data = array('foo' => 'bar');
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('update')
+            ->with($this->equalTo($resource), $this->equalTo($id), $this->equalTo($data));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $api->update($resource, $id, $data);
+    }
+
+    public function testDelete()
+    {
+        $resource = 'test-resource';
+        $id = 'test-id';
+        $data = array('foo' => 'bar');
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('delete')
+            ->with($this->equalTo($resource), $this->equalTo($id), $this->equalTo($data));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $api->delete($resource, $id, $data);
+    }
+
+    public function testSearchOneWithContent()
+    {
+        $resource = 'test-resource';
+        $data = array('foo' => 'bar');
+        $content = array('foobar', 'bazbat');
+        $dataWithLimit = $data + array('limit' => 1);
+
+        $response = new \Omeka\Api\Response;
+        $response->setContent($content);
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('search')
+            ->with($this->equalTo($resource), $this->equalTo($dataWithLimit))
+            ->will($this->returnValue($response));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $response = $api->searchOne($resource, $data);
+        $this->assertEquals('foobar', $response->getContent());
+    }
+
+    public function testSearchOneWithoutContent()
+    {
+        $resource = 'test-resource';
+        $data = array('foo' => 'bar');
+        $content = array();
+        $dataWithLimit = $data + array('limit' => 1);
+
+        $response = new \Omeka\Api\Response;
+        $response->setContent($content);
+
+        $mockApiManager = $this->getMock('Omeka\Api\Manager');
+        $mockApiManager->expects($this->once())
+            ->method('search')
+            ->with($this->equalTo($resource), $this->equalTo($dataWithLimit))
+            ->will($this->returnValue($response));
+
+        $mockServiceManager = $this->getServiceManager(array(
+            'Omeka\ApiManager' => $mockApiManager,
+        ));
+
+        $mockController = $this->getMockForAbstractClass('Zend\Mvc\Controller\AbstractController');
+        $mockController->setServiceLocator($mockServiceManager);
+
+        $api = new Api;
+        $api->setController($mockController);
+        $response = $api->searchOne($resource, $data);
+        $this->assertNull($response->getContent());
+    }
+}

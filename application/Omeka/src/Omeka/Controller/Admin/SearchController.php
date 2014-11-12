9c8a6e336691562de963844b504fa4dc32e4f9e9
Jim Safley <jimsafley@gmail.com>
2014-11-12 15:56:14 -0500

    Use JS to submit advanced search form.

--- application/Omeka/src/Omeka/Controller/Admin/SearchController.php
+++ application/Omeka/src/Omeka/Controller/Admin/SearchController.php
@@ -9,48 +9,6 @@ class SearchController extends AbstractActionController
 {
     public function indexAction()
     {
-        if ($this->getRequest()->isPost()) {
-            $browseQuery= array();
-
-            // Build value queries
-            $value = $this->params()->fromPost('value', array('type' => array()));
-            foreach ($value['type'] as $index => $type) {
-                if (!isset($value['query'][$index])) {
-                    continue;
-                }
-                $query = $value['query'][$index];
-                $browseQuery['value'][$type][] = $query;
-            }
-
-            // Build property queries
-            $property = $this->params()->fromPost('property', array('type' => array()));
-            foreach ($property['type'] as $index => $type) {
-                if (!isset($property['query'][$index]) || !isset($property['id'][$index])) {
-                    continue;
-                }
-                $query = $property['query'][$index];
-                $id = $property['id'][$index];
-                $browseQuery['property'][$id][$type][] = $query;
-            }
-
-            // Build has property queries
-            $hasProperty = $this->params()->fromPost('has_property', array('type' => array()));
-            foreach ($hasProperty['type'] as $index => $type) {
-                if (!isset($hasProperty['id'][$index])) {
-                    continue;
-                }
-                $id = $hasProperty['id'][$index];
-                $browseQuery['has_property'][$id] = $type;
-            }
-
-            return $this->redirect()->toRoute('admin/default', array(
-                'controller' => $this->params()->fromPost('type', 'item'),
-                'action' => 'browse',
-            ), array(
-                'query' => $browseQuery,
-            ));
-        }
-
         $response = $this->api()->search('vocabularies');
         if ($response->isError()) {
             $this->apiError($response);

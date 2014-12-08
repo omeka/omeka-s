ec9e1510f0b7d867d250adeecc0e1872002784d2
Jim Safley <jimsafley@gmail.com>
2014-12-08 17:54:42 -0500

    Change state change controls to form buttons.

--- application/src/Controller/Admin/ModuleController.php
+++ application/src/Controller/Admin/ModuleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Omeka\Controller\Admin;
 
+use Omeka\Mvc\Exception;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
@@ -8,70 +9,12 @@ class ModuleController extends AbstractActionController
 {
     public function indexAction()
     {
-        return $this->redirect()->toRoute('admin/default', array(
-            'controller' => 'module',
-            'action' => 'browse',
-        ));
+        return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
     }
 
     public function browseAction()
     {
-        $view = new ViewModel;
         $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
-
-        // Handle bulk state changes (activate and deactivate only)
-        if ($this->getRequest()->isPost()) {
-            if ($ids = $this->params()->fromPost('activate')) {
-                foreach ($ids as $id) {
-                    $module = $manager->getModule($id);
-                    $manager->activate($module);
-                }
-                $this->messenger()->addSuccess($this->translate('The selected modules were successfully activated'));
-            } elseif ($ids = $this->params()->fromPost('deactivate')) {
-                foreach ($ids as $id) {
-                    $module = $manager->getModule($id);
-                    $manager->deactivate($module);
-                }
-                $this->messenger()->addSuccess($this->translate('The selected modules were successfully deactivated'));
-            }
-            return $this->redirect()->refresh();
-        }
-
-        // Handle individual state changes.
-        $action = $this->params()->fromQuery('action');
-        $id = $this->params()->fromQuery('id');
-        if ($action && $module = $manager->getModule($id)) {
-            if ('install' == $action) {
-                $manager->install($module);
-                $this->messenger()->addSuccess($this->translate('The module was successfully installed'));
-                if ($module->isConfigurable()) {
-                    return $this->redirect()->toRoute(
-                        'admin/default',
-                        array('controller' => 'module', 'action' => 'configure'),
-                        array('query' => array('id' => $module->getId()))
-                    );
-                }
-            } elseif ('uninstall' == $action) {
-                $manager->uninstall($module);
-                $this->messenger()->addSuccess($this->translate('The module was successfully uninstalled'));
-            } elseif ('configure' == $action) {
-                return $this->redirect()->toRoute(
-                    'admin/default',
-                    array('controller' => 'module', 'action' => 'configure'),
-                    array('query' => array('id' => $module->getId()))
-                );
-            } elseif ('activate' == $action) {
-                $manager->activate($module);
-                $this->messenger()->addSuccess($this->translate('The module was successfully activated'));
-            } elseif ('deactivate' == $action) {
-                $manager->deactivate($module);
-                $this->messenger()->addSuccess($this->translate('The module was successfully deactivated'));
-            } elseif ('upgrade' == $action) {
-                $manager->upgrade($module);
-                $this->messenger()->addSuccess($this->translate('The module was successfully upgraded'));
-            }
-            return $this->redirect()->refresh();
-        }
 
         // Get modules, filtering modules by state.
         $state = $this->params()->fromQuery('state');
@@ -92,6 +35,7 @@ class ModuleController extends AbstractActionController
             return strcmp($a->getName(), $b->getName());
         });
 
+        $view = new ViewModel;
         $view->setVariable('modules', $modules);
         $view->setVariable('filterState', $state);
         $view->setVariable('filterStates', array(
@@ -113,37 +57,164 @@ class ModuleController extends AbstractActionController
         return $view;
     }
 
+    /**
+     * Install a module.
+     */
+    public function installAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+        $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
+        $module = $manager->getModule($this->params()->fromPost('id'));
+        if (!$module) {
+            throw new Exception\NotFoundException;
+        }
+        $manager->install($module);
+        $this->messenger()->addSuccess($this->translate('The module was successfully installed'));
+        if ($module->isConfigurable()) {
+            return $this->redirect()->toRoute(
+                null, array('action' => 'configure'),
+                array('query' => array('id' => $module->getId())), true
+            );
+        }
+        return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+    }
+
+    /**
+     * Uninstall a module.
+     */
+    public function uninstallAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+        $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
+        $module = $manager->getModule($this->params()->fromPost('id'));
+        if (!$module) {
+            throw new Exception\NotFoundException;
+        }
+        $manager->uninstall($module);
+        $this->messenger()->addSuccess($this->translate('The module was successfully uninstalled'));
+        return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+    }
+
+    /**
+     * Activate a module or modules.
+     */
+    public function activateAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+        $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
+
+        // Handle bulk activations
+        $ids = $this->params()->fromPost('ids');
+        if (is_array($ids)) {
+            foreach ($ids as $id) {
+                $module = $manager->getModule($id);
+                if (!$module) {
+                    throw new Exception\NotFoundException;
+                }
+                $manager->activate($module);
+            }
+            $this->messenger()->addSuccess($this->translate('The modules were successfully activated'));
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+
+        // Handle a single activation
+        $module = $manager->getModule($this->params()->fromPost('id'));
+        if (!$module) {
+            throw new Exception\NotFoundException;
+        }
+        $manager->activate($module);
+        $this->messenger()->addSuccess($this->translate('The module was successfully activated'));
+        return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+    }
+
+    /**
+     * Deactivate a module or modules.
+     */
+    public function deactivateAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+        $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
+
+        // Handle bulk deactivations
+        $ids = $this->params()->fromPost('ids');
+        if (is_array($ids)) {
+            foreach ($ids as $id) {
+                $module = $manager->getModule($id);
+                if (!$module) {
+                    throw new Exception\NotFoundException;
+                }
+                $manager->deactivate($module);
+            }
+            $this->messenger()->addSuccess($this->translate('The modules were successfully deactivated'));
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+
+        // Handle a single deactivation
+        $module = $manager->getModule($this->params()->fromPost('id'));
+        if (!$module) {
+            throw new Exception\NotFoundException;
+        }
+        $manager->deactivate($module);
+        $this->messenger()->addSuccess($this->translate('The module was successfully deactivated'));
+        return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+    }
+
+    /**
+     * Upgrade a module.
+     */
+    public function upgradeAction()
+    {
+        if (!$this->getRequest()->isPost()) {
+            return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+        }
+        $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
+        $module = $manager->getModule($this->params()->fromPost('id'));
+        if (!$module) {
+            throw new Exception\NotFoundException;
+        }
+        $manager->upgrade($module);
+        $this->messenger()->addSuccess($this->translate('The module was successfully upgraded'));
+        return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
+    }
+
+    /**
+     * Configure a module.
+     */
     public function configureAction()
     {
-        $view = new ViewModel;
-
-        // Get the module
+        $manager = $this->getServiceLocator()->get('Omeka\ModuleManager');
         $id = $this->params()->fromQuery('id');
-        $module = $this->getServiceLocator()
-            ->get('ModuleManager')->getModule($id);
+        $module = $manager->getModule($id);
+        if (!$module) {
+            throw new Exception\NotFoundException;
+        }
 
-        if (null === $module) {
+        $moduleObject = $this->getServiceLocator()
+            ->get('ModuleManager')->getModule($id);
+        if (null === $moduleObject) {
             // Do not attempt to configure an unloaded module.
-            return $this->redirect()->toRoute('admin/default', array(
-                'controller' => 'module',
-                'action' => 'browse',
-            ));
+            throw new Exception\NotFoundException;
         }
 
         if ($this->getRequest()->isPost()) {
-            if (false !== $module->handleConfigForm($this)) {
+            if (false !== $moduleObject->handleConfigForm($this)) {
                 $this->messenger()->addSuccess($this->translate('The module was successfully configured'));
-                return $this->redirect()->toRoute('admin/default', array(
-                    'controller' => 'module',
-                    'action' => 'browse',
-                ));
+                return $this->redirect()->toRoute(null, array('action' => 'browse'), true);
             }
             $this->messenger()->addError($this->translate('There was a problem during configuration'));
         }
 
-        $view->setVariable('module', $this->getServiceLocator()
-            ->get('Omeka\ModuleManager')->getModule($id));
-        $view->setVariable('configForm', $module->getConfigForm($view));
+        $view = new ViewModel;
+        $view->setVariable('module', $module);
+        $view->setVariable('configForm', $moduleObject->getConfigForm($view));
         return $view;
     }
 }

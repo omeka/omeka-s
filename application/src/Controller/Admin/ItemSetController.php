f66d4ceaec618cf46f3d678a253882ced5a2de3f
Jim Safley <jimsafley@gmail.com>
2015-01-12 15:39:53 -0500

    Begin item set implementation.

---
+++ application/src/Controller/Admin/ItemSetController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Omeka\Controller\Admin;
+
+use Omeka\Form\ConfirmForm;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ItemSetController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return $this->redirect()->toRoute('admin/default', array(
+            'controller' => 'item-set',
+            'action' => 'browse',
+        ));
+    }
+
+    public function browseAction()
+    {
+        $view = new ViewModel;
+
+        $page = $this->params()->fromQuery('page', 1);
+        $query = $this->params()->fromQuery() + array('page' => $page);
+        $response = $this->api()->search('item_sets', $query);
+
+        $this->paginator($response->getTotalResults(), $page);
+        $view->setVariable('itemSets', $response->getContent());
+        $view->setVariable('confirmForm', new ConfirmForm(
+            $this->getServiceLocator(), null, array(
+                'button_value' => $this->translate('Confirm Delete'),
+            )
+        ));
+        return $view;
+    }
+}

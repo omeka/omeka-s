732d422d5d7419381b79c479280e6f7969a65ef3
kalbers <ken.albers@gmail.com>
2015-04-16 13:15:47 -0400

    Fixes scroll to move to newly added property. Closes #94

--- application/asset/js/resource-form.js
+++ application/asset/js/resource-form.js
@@ -15,7 +15,9 @@
                 },200);
             } else {
                 makeNewField(propertyLi);
-            $('div.resource-values.field[data-property-term="' + qName + '"] .values').append(makeNewValue(qName, true));
+                var wrapper = $('div.resource-values.field[data-property-term="' + qName + '"] .values');
+                wrapper.append(makeNewValue(qName));
+                scrollTo(wrapper);
             }
         });
 
@@ -53,7 +55,7 @@
             var wrapper = $(this).parents('.resource-values.field');
             var type = $(this).attr('class').replace(/o-icon-/, '').replace(/button/, '').replace(/add-value/, '').replace(/ /g, '');
             var qName = wrapper.data('property-term');
-            $('div.resource-values.field[data-property-term="' + qName + '"] .values').append(makeNewValue(qName, false, false, type));
+            $('div.resource-values.field[data-property-term="' + qName + '"] .values').append(makeNewValue(qName, false, type));
         });
 
         // Remove value.
@@ -127,7 +129,7 @@
             e.preventDefault();
             var propertyQname = $(this).data('property-term');
             var valuesData = $('.resource-details').data('resource-values');
-            $('.value.selecting-resource').replaceWith(makeNewValue(propertyQname, false, valuesData));
+            $('.value.selecting-resource').replaceWith(makeNewValue(propertyQname, valuesData));
             Omeka.closeSidebar($('.sidebar .sidebar'));
         });
 
@@ -161,7 +163,7 @@
         initPage();
     });
 
-    var makeNewValue = function(qName, focus, valueObject, valueType) {
+    var makeNewValue = function(qName, valueObject, valueType) {
         var valuesWrapper = $('div.resource-values.field[data-property-term="' + qName + '"]');
         var count = valuesWrapper.find('input.property').length;
         var propertyId = valuesWrapper.data('property-id');
@@ -240,14 +242,7 @@
             }
         }
 
-        return newValue;
-
-        if (focus) {
-            $('html, body').animate({
-                scrollTop: (valuesWrapper.offset().top -100)
-            },200);
-            $('textarea', newValue).focus();
-        } 
+        return newValue; 
     };
 
     var makeNewField = function(property) {
@@ -345,7 +340,7 @@
             for (var term in valuesJson) {
                 makeNewField(term);
                 for (var i=0; i < valuesJson[term].length; i++) {
-                    $('div.resource-values.field[data-property-term="' + term + '"] .values').append(makeNewValue(term, false, valuesJson[term][i]));
+                    $('div.resource-values.field[data-property-term="' + term + '"] .values').append(makeNewValue(term, valuesJson[term][i]));
                 }
             }
         }
@@ -367,5 +362,12 @@
             });
         }
     };
+
+    var scrollTo = function(wrapper) {
+        //focus on the value being edited
+        $('html, body').animate({
+            scrollTop: (wrapper.offset().top -100)
+        },200);
+    }
 })(jQuery);
 

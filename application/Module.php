46e0de69807a26fa8182e16dfc571a4d83b5425b
Jim Safley <jimsafley@gmail.com>
2015-04-29 13:17:16 -0400

    Update resource types
    Removes "Model" from "Omeka\Model\Entity", bringing the resource table in line with the new discriminator map.

--- application/Module.php
+++ application/Module.php
@@ -15,7 +15,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '0.1.20';
+    const VERSION = '0.1.21';
 
     /**
      * @var array View helpers that need service manager injection

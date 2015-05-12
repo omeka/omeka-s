1be74bf3b637628435a1c81e851a1cc6370d99a1
John Flatness <john@zerocrates.org>
2015-05-12 11:32:51 -0400

    Check for minimum MySQL version on install

--- application/src/Installation/Task/CheckDbConfigurationTask.php
+++ application/src/Installation/Task/CheckDbConfigurationTask.php
@@ -8,13 +8,23 @@ use Omeka\Installation\Installer;
  */
 class CheckDbConfigurationTask implements TaskInterface
 {
+    const MYSQL_MINIMUM_VERSION = '5.5.3';
+
     public function perform(Installer $installer)
     {
         try {
-            $installer->getServiceLocator()->get('Omeka\Connection')->connect();
+            $connection = $installer->getServiceLocator()->get('Omeka\Connection');
+            $connection->connect();
         } catch (\Exception $e) {
             $installer->addError($e->getMessage());
             return;
+        }
+
+        $version = $connection->getWrappedConnection()->getServerVersion();
+        if (version_compare($version, self::MYSQL_MINIMUM_VERSION, '<')) {
+            $installer->addError(sprintf(
+                'Omeka requires at least MySQL version %s, but this server is running version %s.',
+                self::MYSQL_MINIMUM_VERSION, $version));
         }
     }
 }

20545fc45ed93eb84030bbfb0fc71ceebc74dd2b
Jim Safley <jimsafley@gmail.com>
2015-05-21 14:13:51 -0400

    Add is_open flag to item set entity

--- application/Module.php
+++ application/Module.php
@@ -15,7 +15,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '0.1.23';
+    const VERSION = '0.1.24';
 
     /**
      * @var array View helpers that need service manager injection

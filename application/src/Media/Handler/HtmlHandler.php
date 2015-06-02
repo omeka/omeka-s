84554ff47cf9d37724a796e82152bc03112ffa6a
patrickmj <patrickmjchnm@gmail.com>
2015-06-02 15:44:47 -0400

    update to use new media form

--- application/src/Media/Handler/HtmlHandler.php
+++ application/src/Media/Handler/HtmlHandler.php
@@ -11,9 +11,32 @@ use Omeka\Stdlib\ErrorStore;
 use Zend\View\Renderer\PhpRenderer;
 use Zend\Form\Element\Textarea;
 
-class HtmlHandler extends AbstractHandler
+class HtmlHandler extends AbstractHandler implements MutableHandlerInterface
 {
 
+    public function updateForm(PhpRenderer $view, MediaRepresentation $media, array $options = array())
+    {
+        $view->headscript()->appendFile($view->assetUrl('js/ckeditor/ckeditor.js', 'Omeka'));
+        $view->headscript()->appendFile($view->assetUrl('js/ckeditor/adapters/jquery.js', 'Omeka'));
+        $textarea = new Textarea('o:media[__index__][o:html]');
+        $textarea->setOptions(array(
+            'label' => $view->translate('HTML'),
+            'info'  => $view->translate('HTML or plain text.'),
+        ));
+        $data = $media->mediaData();
+        $html = $data['html'];
+        $textarea->setAttributes(
+                array(
+                    'rows'     => 15,
+                    'id'       => 'media-html-__index__',
+                    'required' => true,
+                    'class'    => 'media-html',
+                    'value' => $html
+                ));
+        $field = $view->formField($textarea);
+        return $field;
+    }
+    
     /**
      * {@inheritDoc}
      */
@@ -59,10 +82,17 @@ class HtmlHandler extends AbstractHandler
         }
     }
     
+    public function update(Media $media, Request $request, ErrorStore $errorStore)
+    {
+        $data = $request->getContent();
+        $html = $data['o:media']['__index__']['o:html'];
+        $media->setData(array('html' => $html));
+    }
+    
     /**
      * {@inheritDoc}
      */
-    public function validateRequest(Request $request, ErrorStore $errorStore)
+    public function validateUpdateRequest(Request $request, ErrorStore $errorStore)
     {
     }
 

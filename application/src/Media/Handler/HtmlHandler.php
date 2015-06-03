d4222681a4337c374700f82d0aa70e76d72e6dd2
patrickmj <patrickmjchnm@gmail.com>
2015-06-03 12:06:02 -0400

    fix ckeditor on media edit page. fix #191

--- application/src/Media/Handler/HtmlHandler.php
+++ application/src/Media/Handler/HtmlHandler.php
@@ -18,6 +18,12 @@ class HtmlHandler extends AbstractHandler implements MutableHandlerInterface
     {
         $view->headscript()->appendFile($view->assetUrl('js/ckeditor/ckeditor.js', 'Omeka'));
         $view->headscript()->appendFile($view->assetUrl('js/ckeditor/adapters/jquery.js', 'Omeka'));
+        $js = "
+            $(document).ready(function() {
+                $('textarea.media-html').ckeditor();
+            });
+        ";
+        $view->headscript()->appendScript($js);
         $textarea = new Textarea('o:media[__index__][o:html]');
         $textarea->setOptions(array(
             'label' => $view->translate('HTML'),

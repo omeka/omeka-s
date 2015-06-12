9182139612632c44d73dcc82c0910975bea6c1ab
Jim Safley <jimsafley@gmail.com>
2015-06-12 12:36:44 -0400

    Set default message encoding to UTF-8

--- application/src/Service/Mailer.php
+++ application/src/Service/Mailer.php
@@ -2,6 +2,7 @@
 namespace Omeka\Service;
 
 use Traversable;
+use Zend\Stdlib\ArrayUtils;
 use Zend\Mail\Message;
 use Zend\Mail\MessageFactory;
 use Zend\Mail\Transport\TransportInterface;
@@ -34,13 +35,21 @@ class Mailer
     }
 
     /**
-     * Return a message object.
+     * Return a new message object.
+     *
+     * By default, encode messages as UTF-8.
      *
      * @param array|Traversable $options
      * @return Message
      */
-    public function getMessage($options = array())
+    public function createMessage($options = array())
     {
+        if ($options instanceof Traversable) {
+            $options = ArrayUtils::iteratorToArray($options);
+        }
+        if (!isset($options['encoding'])) {
+            $options['encoding'] = 'UTF-8';
+        }
         return MessageFactory::getInstance($options);
     }
 
@@ -54,7 +63,7 @@ class Mailer
         if ($message instanceof Message) {
             $this->transport->send($message);
         } else {
-            $this->transport->send($this->getMessage($message));
+            $this->transport->send($this->createMessage($message));
         }
     }
 }

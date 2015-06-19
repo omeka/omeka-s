5afe57cfed497614f4fc6742b07d639f5e397834
John Flatness <john@zerocrates.org>
2015-06-19 13:01:35 -0400

    Fix cache-related errors on User change

--- application/src/Service/AuthenticationServiceFactory.php
+++ application/src/Service/AuthenticationServiceFactory.php
@@ -5,6 +5,7 @@ use Omeka\Authentication\Adapter\KeyAdapter;
 use Omeka\Authentication\Adapter\PasswordAdapter;
 use Omeka\Authentication\Storage\DoctrineWrapper;
 use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\Adapter\Callback;
 use Zend\Authentication\Storage\NonPersistent;
 use Zend\Authentication\Storage\Session;
 use Zend\ServiceManager\FactoryInterface;
@@ -25,17 +26,23 @@ class AuthenticationServiceFactory implements FactoryInterface
     {
         $entityManager = $serviceLocator->get('Omeka\EntityManager');
         $status = $serviceLocator->get('Omeka\Status');
-        $userRepository = $entityManager->getRepository('Omeka\Entity\User');
 
-        if ($status->isApiRequest()) {
-            // Authenticate using key for API requests.
-            $keyRepository = $entityManager->getRepository('Omeka\Entity\ApiKey');
-            $storage = new DoctrineWrapper(new NonPersistent, $userRepository);
-            $adapter = new KeyAdapter($keyRepository, $entityManager);
+        // Skip auth retrieval entirely if we're upgrading
+        if ($status->needsVersionUpdate() && $status->needsMigration()) {
+            $storage = new NonPersistent;
+            $adapter = new Callback(function () { return null; });
         } else {
-            // Authenticate using user/password for all other requests.
-            $storage = new DoctrineWrapper(new Session, $userRepository);
-            $adapter = new PasswordAdapter($userRepository);
+            $userRepository = $entityManager->getRepository('Omeka\Entity\User');
+            if ($status->isApiRequest()) {
+                // Authenticate using key for API requests.
+                $keyRepository = $entityManager->getRepository('Omeka\Entity\ApiKey');
+                $storage = new DoctrineWrapper(new NonPersistent, $userRepository);
+                $adapter = new KeyAdapter($keyRepository, $entityManager);
+            } else {
+                // Authenticate using user/password for all other requests.
+                $storage = new DoctrineWrapper(new Session, $userRepository);
+                $adapter = new PasswordAdapter($userRepository);
+            }
         }
 
         $authService = new AuthenticationService($storage, $adapter);

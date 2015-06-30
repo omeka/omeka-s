6ec527c32610cc9b286c518c81bbd27dd4e21f14
John Flatness <john@zerocrates.org>
2015-06-30 10:27:48 -0400

    Set version 0.2.0-alpha

--- application/Module.php
+++ application/Module.php
@@ -15,7 +15,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '0.1.33';
+    const VERSION = '0.2.0-alpha';
 
     /**
      * @var array View helpers that need service manager injection

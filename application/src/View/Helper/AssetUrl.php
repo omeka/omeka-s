6fdf4bf0c0ea7973c1d157f0a7951c5619619238
Jim Safley <jimsafley@gmail.com>
2015-07-24 14:35:27 -0400

    Get asset URL for current theme

--- application/src/View/Helper/AssetUrl.php
+++ application/src/View/Helper/AssetUrl.php
@@ -1,6 +1,7 @@
 <?php
 namespace Omeka\View\Helper;
 
+use Omeka\Service\ThemeManager;
 use Omeka\Module\Manager as ModuleManager;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\View\Helper\AbstractHtmlElement;
@@ -9,6 +10,12 @@ class AssetUrl extends AbstractHtmlElement
 {
     const OMEKA_ASSETS_PATH = '%s/application/asset/%s';
     const MODULE_ASSETS_PATH = '%s/modules/%s/asset/%s';
+    const THEME_ASSETS_PATH = '%s/themes/%s/assets/%s';
+
+    /**
+     * @var ThemeManager
+     */
+    protected $themeManager;
 
     /**
      * @var ModuleManager
@@ -27,28 +34,36 @@ class AssetUrl extends AbstractHtmlElement
      */
     public function __construct(ServiceLocatorInterface $serviceLocator)
     {
+        $this->themeManager = $serviceLocator->get('Omeka\ThemeManager');
         $this->moduleManager = $serviceLocator->get('Omeka\ModuleManager');
         $this->activeModules = $this->moduleManager
             ->getModulesByState(ModuleManager::STATE_ACTIVE);
     }
 
     /**
-     * Return a path to an asset by module directory name.
+     * Return a path to an asset.
      *
-     * The module must be active. Does not check if the asset file exists.
+     * Returns the asset URL for the current theme if no module specified.
+     * Otherwise, returns the asset URL for the specified module, only if the
+     * module is active. Does not check if the asset file exists.
      *
      * @param string $file
-     * @param string $module
+     * @param string|null $module
      * @return string|null
      */
-    public function __invoke($file, $module)
+    public function __invoke($file, $module = null)
     {
         $basePath = $this->getView()->basePath();
+        if (null === $module) {
+            $theme = $this->themeManager->getCurrent();
+            return sprintf(self::THEME_ASSETS_PATH, $basePath, $theme, $file);
+        }
         if ('Omeka' == $module) {
             return sprintf(self::OMEKA_ASSETS_PATH, $basePath, $file);
         }
         if (array_key_exists($module, $this->activeModules)) {
             return sprintf(self::MODULE_ASSETS_PATH, $basePath, $module, $file);
         }
+        return null;
     }
 }

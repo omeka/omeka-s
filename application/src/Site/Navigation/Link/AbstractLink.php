7d7dbf56a4af2f6086a803cae3d5c6e1a282b70f
Jim Safley <jimsafley@gmail.com>
2015-10-07 12:43:45 -0400

    Add method to get view helpers via link types

--- application/src/Site/Navigation/Link/AbstractLink.php
+++ application/src/Site/Navigation/Link/AbstractLink.php
@@ -2,8 +2,29 @@
 namespace Omeka\Site\Navigation\Link;
 
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use Zend\View\HelperPluginManager;
 
 abstract class AbstractLink implements LinkInterface
 {
     use ServiceLocatorAwareTrait;
+
+    /**
+     * @var HelperPluginManager
+     */
+    protected $viewHelperManager;
+
+    /**
+     * Get a view helper from the manager.
+     *
+     * @param string $name
+     * @return TranslatorInterface
+     */
+    protected function getViewHelper($name)
+    {
+        if (!$this->viewHelperManager instanceof HelperPluginManager) {
+            $this->viewHelperManager = $this->getServiceLocator()
+                ->get('ViewHelperManager');
+        }
+        return $this->viewHelperManager->get($name);
+    }
 }

eb9d6de61db6c6b3666d8e3004b93ad3dc5fe8f8
John Flatness <john@zerocrates.org>
2015-10-09 15:23:07 -0400

    Allow deleting items and media assigned to a site
    (Fix #382)

--- application/Module.php
+++ application/Module.php
@@ -16,7 +16,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '0.2.9-alpha';
+    const VERSION = '0.3.0-alpha';
 
     /**
      * @var array View helpers that need service manager injection

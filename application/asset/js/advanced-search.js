056e92db6556ff1c8a8bb079d27aa1cbbcdf1380
Jim Safley <jimsafley@gmail.com>
2015-10-14 10:10:31 -0400

    Move advanced search JS to separate file

---
+++ application/asset/js/advanced-search.js
@@ -0,0 +1,67 @@
+$(document).ready( function() {
+
+// Remove all names from query form elements.
+$('.query-type, .query-text, .query-property').attr('name', null);
+
+// Add a value.
+$('#advanced-search').on('click', '.add-value', function(e) {
+    e.preventDefault();
+    var first = $(this).parents('.field').find('.value').first();
+    var clone = first.clone();
+    clone.children('input[type="text"]').val(null);
+    clone.children('select').prop('selectedIndex', 0);
+    clone.insertBefore($(this));
+});
+
+// Remove a value.
+$('#advanced-search').on('click', '.remove-value', function(e) {
+    e.preventDefault();
+    var values = $(this).parents('.inputs').children('.value');
+    $(this).parent('.value').remove();
+});
+
+// Bypass regular form handling for value, property, and has property queries.
+$('#advanced-search').submit(function(event) {
+
+    $('#value-queries').find('.value').each(function(index) {
+        var text = $(this).children('.query-text');
+        if (!$.trim(text.val())) {
+            return; // do not process an empty query
+        }
+        var type = $(this).children('.query-type');
+        $('<input>').attr('type', 'hidden')
+            .attr('name', 'value[' + type.val() + '][]')
+            .val(text.val())
+            .appendTo('#advanced-search');
+    });
+
+    $('#property-queries').find('.value').each(function(index) {
+        var text = $(this).children('.query-text');
+        if (!$.trim(text.val())) {
+            return; // do not process an empty query
+        }
+        var property = $(this).children('.query-property');
+        if (!$.isNumeric(property.val())) {
+            return; // do not process an invalid property
+        }
+        var type = $(this).children('.query-type');
+        $('<input>').attr('type', 'hidden')
+            .attr('name', 'property[' + property.val() + '][' + type.val() + '][]')
+            .val(text.val())
+            .appendTo('#advanced-search');
+    });
+
+    $('#has-property-queries').find('.value').each(function(index) {
+        var property = $(this).children('.query-property');
+        if (!$.isNumeric(property.val())) {
+            return; // do not process an invalid property
+        }
+        var type = $(this).children('.query-type');
+        $('<input>').attr('type', 'hidden')
+            .attr('name', 'has_property[' + property.val() + ']')
+            .val(type.val())
+            .appendTo('#advanced-search');
+    });
+});
+
+});

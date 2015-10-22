746830c2172db275fdfd39202681e12406fdeb25
Jim Safley <jimsafley@gmail.com>
2015-10-22 12:36:05 -0400

    Be less strict when page not in navigation

--- application/src/View/Helper/SitePagePagination.php
+++ application/src/View/Helper/SitePagePagination.php
@@ -24,15 +24,25 @@ class SitePagePagination extends AbstractHelper
     protected $nextPage;
 
     /**
-     * Return the pagination markup.
+     * @var bool Whether the page is found in site navigation.
+     */
+    protected $pageInNav = true;
+
+    /**
+     * Return the site page pagination markup.
+     *
+     * Returns null if the page is not found in site navigation.
      *
      * @throws Exception\RuntimeException
-     * @return string
+     * @return string|null
      */
     public function __invoke()
     {
         if (null === $this->page) {
             throw new Exception\RuntimeException('No site page provided');
+        }
+        if (!$this->pageInNav) {
+            return null;
         }
         return $this->getView()->partial(
             'common/site-page-pagination',
@@ -54,6 +64,8 @@ class SitePagePagination extends AbstractHelper
      */
     public function setPage(SitePageRepresentation $page)
     {
+        $this->page = $page;
+
         // Build an array containing all page IDs in navigation in the order
         // they appear.
         $pageIds = [];
@@ -72,7 +84,9 @@ class SitePagePagination extends AbstractHelper
 
         $key = array_search($page->id(), $pageIds);
         if (false === $key) {
-            throw new Exception\InvalidArgumentException('Page is not in site navigation');
+            // Page not found in navigation. Don't attempt to find prev/next.
+            $this->pageInNav = false;
+            return;
         }
 
         $api = $this->getView()->api();
@@ -89,7 +103,6 @@ class SitePagePagination extends AbstractHelper
             } catch (NotFoundException $e) {}
         }
 
-        $this->page = $page;
         $this->prevPage = $prevPage;
         $this->nextPage = $nextPage;
     }

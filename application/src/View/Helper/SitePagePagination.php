4000737845297159b77c1c5094b2b09f419cb8bc
Jim Safley <jimsafley@gmail.com>
2015-10-26 20:58:40 -0400

    Refactor page pagination view helper

--- application/src/View/Helper/SitePagePagination.php
+++ application/src/View/Helper/SitePagePagination.php
@@ -55,55 +55,27 @@ class SitePagePagination extends AbstractHelper
     }
 
     /**
-     * Set the current page.
+     * Set the current, previous, and next page.
      *
-     * Automatically sets the prev/next pages if any.
-     *
-     * @throws Exception\InvalidArgumentException
      * @param SitePageRepresentation $page
      */
     public function setPage(SitePageRepresentation $page)
     {
         $this->page = $page;
-
-        // Build an array containing all page IDs in navigation in the order
-        // they appear.
-        $pageIds = [];
-        $iterate = function ($linksIn) use (&$iterate, &$pageIds, $page)
-        {
-            foreach ($linksIn as $key => $data) {
-                if ('page' === $data['type']) {
-                    $pageIds[] = $data['data']['id'];
-                }
-                if (isset($data['links'])) {
-                    $iterate($data['links']);
-                }
-            }
-        };
-        $iterate($page->site()->navigation());
-
-        $key = array_search($page->id(), $pageIds);
-        if (false === $key) {
+        $linkedPages = $page->site()->linkedPages();
+        if (!array_key_exists($page->id(), $linkedPages)) {
             // Page not found in navigation. Don't attempt to find prev/next.
             $this->pageInNav = false;
             return;
         }
-
-        $api = $this->getView()->api();
-        $prevPage = null;
-        if (isset($pageIds[$key - 1])) {
-            try {
-                $prevPage = $api->read('site_pages', $pageIds[$key - 1])->getContent();
-            } catch (NotFoundException $e) {}
+        // Iterate the linked pages, setting the previous and next pages, if any.
+        while ($linkedPage = current($linkedPages)) {
+            if ($page->id() === $linkedPage->id()) {
+                $this->nextPage = next($linkedPages);
+                break;
+            }
+            $this->prevPage = $linkedPage;
+            next($linkedPages);
         }
-        $nextPage = null;
-        if (isset($pageIds[$key + 1])) {
-            try {
-                $nextPage = $api->read('site_pages', $pageIds[$key + 1])->getContent();
-            } catch (NotFoundException $e) {}
-        }
-
-        $this->prevPage = $prevPage;
-        $this->nextPage = $nextPage;
     }
 }

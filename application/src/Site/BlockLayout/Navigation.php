d9512f71c1e9475903b2d10f65d4d3285f0162aa
Ken Albers <ken.albers@gmail.com>
2015-11-06 14:43:24 -0500

    Possibly fix tab/space issue.

--- application/src/Site/BlockLayout/Navigation.php
+++ application/src/Site/BlockLayout/Navigation.php
@@ -7,13 +7,13 @@ use Zend\View\Renderer\PhpRenderer;
 
 class Navigation extends AbstractBlockLayout
 {
-	public function getLabel()
-	{
-		$translator = $this->getServiceLocator()->get('MvcTranslator');
-		return $translator->translate('Navigation');
-	}
+    public function getLabel()
+    {
+        $translator = $this->getServiceLocator()->get('MvcTranslator');
+        return $translator->translate('Navigation');
+    }
 
-	public function form(PhpRenderer $view, SiteRepresentation $site,
+    public function form(PhpRenderer $view, SiteRepresentation $site,
         SitePageBlockRepresentation $block = null
     ) {
         return 'A list of child pages';
@@ -21,11 +21,11 @@ class Navigation extends AbstractBlockLayout
 
     public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
     {
-    	$html = '';
-    	$html .= '<div class="navigation-block">';
-    	$html .= $view->navigation('Zend\Navigation\Site')->menu()->renderSubMenu();
-    	$html .= '</div>';
+        $html = '';
+        $html .= '<div class="navigation-block">';
+        $html .= $view->navigation('Zend\Navigation\Site')->menu()->renderSubMenu();
+        $html .= '</div>';
 
-    	return $html;
+        return $html;
     }
 }

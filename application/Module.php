6cb501474d8fd4c7664ca4234e7067f080f115c0
Jim Safley <jimsafley@gmail.com>
2015-11-19 12:43:45 -0500

    Set the form row partial on bootstrap

--- application/Module.php
+++ application/Module.php
@@ -46,6 +46,11 @@ class Module extends AbstractModule
         $serviceManager = $this->getServiceLocator();
         $viewHelperManager = $serviceManager->get('ViewHelperManager');
 
+        // Set the form row partial.
+        $formCollection = $viewHelperManager->get('formCollection');
+        $formRow = $viewHelperManager->get('formRow');
+        $formCollection->setElementHelper($formRow->setPartial('common/form-row'));
+
         // Inject the service manager into view helpers that need it.
         foreach ($this->viewHelpers as $helperName => $helperClass) {
             $viewHelperManager->setFactory($helperName,

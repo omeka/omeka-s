b4530c4bce3f1fd74b6709456bda72ca060386d1
Jim Safley <jimsafley@gmail.com>
2015-11-18 20:30:32 -0500

    Enable restrict browse to attached items

--- application/src/Controller/Site/ItemController.php
+++ application/src/Controller/Site/ItemController.php
@@ -12,6 +12,10 @@ class ItemController extends AbstractSiteController
         $this->setBrowseDefaults('created');
 
         $itemPool = is_array($site->itemPool()) ? $site->itemPool() : [];
+        $settings = $this->getServiceLocator()->get('Omeka\SiteSettings');
+        if ($settings->get('browse_attached_items', false)) {
+            $itemPool['site_id'] = $site->id();
+        }
         $query = array_merge($itemPool, $this->params()->fromQuery());
 
         $response = $this->api()->search('items', $query);

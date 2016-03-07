af29045d64c253d813acb0c44054fd638e85b2cd
John Flatness <john@zerocrates.org>
2016-03-07 14:18:06 -0500

    Reorder slugs to come after titles
    (#524)

--- application/src/Form/SiteForm.php
+++ application/src/Form/SiteForm.php
@@ -9,17 +9,6 @@ class SiteForm extends AbstractForm
         $this->setAttribute('id', 'site-form');
 
         $this->add([
-            'name' => 'o:slug',
-            'type' => 'Text',
-            'options' => [
-                'label' => $translator->translate('URL slug')
-            ],
-            'attributes' => [
-                'id' => 'slug',
-                'required' => true,
-            ],
-        ]);
-        $this->add([
             'name' => 'o:title',
             'type' => 'Text',
             'options' => [
@@ -27,6 +16,17 @@ class SiteForm extends AbstractForm
             ],
             'attributes' => [
                 'id' => 'title',
+                'required' => true,
+            ],
+        ]);
+        $this->add([
+            'name' => 'o:slug',
+            'type' => 'Text',
+            'options' => [
+                'label' => $translator->translate('URL slug')
+            ],
+            'attributes' => [
+                'id' => 'slug',
                 'required' => true,
             ],
         ]);

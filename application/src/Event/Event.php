0029bf2c392439c766c63b00e30cf61a5985d829
Jim Safley <jimsafley@gmail.com>
2016-03-24 16:17:26 -0400

    Add section nav events to Event class

--- application/src/Event/Event.php
+++ application/src/Event/Event.php
@@ -73,6 +73,9 @@ class Event extends ZendEvent
     const VIEW_BROWSE_AFTER = 'view.browse.after';
     const VIEW_ADD_AFTER    = 'view.add.after';
     const VIEW_EDIT_AFTER   = 'view.edit.after';
+    const VIEW_SHOW_SECTION_NAV = 'view.show.section_nav';
+    const VIEW_ADD_SECTION_NAV  = 'view.add.section_nav';
+    const VIEW_EDIT_SECTION_NAV = 'view.edit.section_nav';
 
     // Site settings event
     const SITE_SETTINGS_FORM = 'site_settings.form';

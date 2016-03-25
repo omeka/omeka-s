da8d91667b2f22a25015a0043b24de1a54f023c8
John Flatness <john@zerocrates.org>
2016-03-25 11:04:34 -0400

    Delocate File renderers
    (#544)

--- application/src/Media/FileRenderer/ImageRenderer.php
+++ application/src/Media/FileRenderer/ImageRenderer.php
@@ -2,13 +2,10 @@
 namespace Omeka\Media\FileRenderer;
 
 use Omeka\Api\Representation\MediaRepresentation;
-use Zend\ServiceManager\ServiceLocatorAwareTrait;
 use Zend\View\Renderer\PhpRenderer;
 
 class ImageRenderer implements RendererInterface
 {
-    use ServiceLocatorAwareTrait;
-
     public function render(PhpRenderer $view, MediaRepresentation $media,
         array $options = []
     ) {

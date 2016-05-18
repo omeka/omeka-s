08cdbba9c843713020df46a255f17f3728433f25
Jim Safley <jimsafley@gmail.com>
2016-05-18 18:08:35 -0400

    Users can view an attachment only if they have permission to view the attached item
    fixes #613

--- application/src/Db/Filter/VisibilityFilter.php
+++ application/src/Db/Filter/VisibilityFilter.php
@@ -18,31 +18,52 @@ class VisibilityFilter extends SQLFilter
      */
     protected $serviceLocator;
 
-    public function addFilterConstraint(ClassMetadata $targetEntity,
-        $targetTableAlias
-    ) {
-        if ('Omeka\Entity\Resource' !== $targetEntity->getName()) {
-            return '';
+    public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias) {
+        switch ($targetEntity->getName()) {
+            case 'Omeka\Entity\Resource':
+                return $this->getResourceConstraint($targetTableAlias);
+            case 'Omeka\Entity\SiteBlockAttachment':
+                // Users can view an attachment only if they have permission to
+                // view the attached item.
+                $constraint = $this->getResourceConstraint('r');
+                if ('' !== $constraint) {
+                    $constraint = sprintf(
+                        '%1$s.item_id = (SELECT r.id FROM resource r WHERE (%2$s) AND r.id = %1$s.item_id)',
+                        $targetTableAlias, $constraint
+                    );
+                }
+                return $constraint;
+            default:
+                return '';
         }
+    }
 
+    /**
+     * Get the constraint for a resource.
+     *
+     * @param string $alias
+     * @return string
+     */
+    protected function getResourceConstraint($alias)
+    {
         $acl = $this->serviceLocator->get('Omeka\Acl');
         if ($acl->userIsAllowed('Omeka\Entity\Resource', 'view-all')) {
             return '';
         }
 
         // Users can view public resources they do not own.
-        $constraints = ["$targetTableAlias.is_public = 1"];
+        $constraints = ["$alias.is_public = 1"];
         $identity = $this->serviceLocator->get('Omeka\AuthenticationService')->getIdentity();
         if ($identity) {
             // Users can view all resources they own.
             $constraints[] = 'OR';
             $constraints[] = sprintf(
-                "$targetTableAlias.owner_id = %s",
+                "$alias.owner_id = %s",
                 $this->getConnection()->quote($identity->getId(), Type::INTEGER)
             );
         }
 
-       return implode(' ', $constraints);
+        return implode(' ', $constraints);
     }
 
     public function setServiceLocator(ServiceLocatorInterface $serviceLocator)

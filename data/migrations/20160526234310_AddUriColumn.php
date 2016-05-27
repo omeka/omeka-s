d9b1b60a0e364791ebc9b07712f95ffab1ef80f5
Jim Safley <jimsafley@gmail.com>
2016-05-27 13:56:42 -0400

    Fix SQL

--- data/migrations/20160526234310_AddUriColumn.php
+++ data/migrations/20160526234310_AddUriColumn.php
@@ -9,7 +9,7 @@ class AddUriColumn extends AbstractMigration
     {
         $connection = $this->getConnection();
         $connection->query('ALTER TABLE value ADD uri LONGTEXT DEFAULT NULL');
-        $connection->query('UPDATE TABLE value SET uri = value, value = uri_label WHERE type = "uri"');
+        $connection->query('UPDATE value SET uri = value, value = uri_label WHERE type = "uri"');
         $connection->query('ALTER TABLE value DROP uri_label;');
     }
 }

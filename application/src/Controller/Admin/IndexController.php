d91ca6803d1903b3dd94cf0683682c21cfb8a930
Jim Safley <jimsafley@gmail.com>
2016-06-01 19:30:19 -0400

    Optimize dashboard

--- application/src/Controller/Admin/IndexController.php
+++ application/src/Controller/Admin/IndexController.php
@@ -8,18 +8,18 @@ class IndexController extends AbstractActionController
 {
     public function browseAction()
     {
-        $sitesResponse = $this->api()->search('sites', $this->params()->fromQuery());
-        $itemsResponse = $this->api()->search('items', $this->params()->fromQuery());
-        $itemSetsResponse = $this->api()->search('item_sets', $this->params()->fromQuery());
-        $vocabulariesResponse = $this->api()->search('vocabularies', $this->params()->fromQuery());
-        $resourceTemplatesResponse = $this->api()->search('resource_templates', $this->params()->fromQuery());
+        $sitesResponse = $this->api()->search('sites');
+        $itemsResponse = $this->api()->search('items', ['limit' => 0]);
+        $itemSetsResponse = $this->api()->search('item_sets', ['limit' => 0]);
+        $vocabulariesResponse = $this->api()->search('vocabularies', ['limit' => 0]);
+        $resourceTemplatesResponse = $this->api()->search('resource_templates', ['limit' => 0]);
 
         $view = new ViewModel;
         $view->setVariable('sites', $sitesResponse->getContent());
-        $view->setVariable('itemCount', count($itemsResponse->getContent()));
-        $view->setVariable('itemSetCount', count($itemSetsResponse->getContent()));
-        $view->setVariable('vocabularyCount', count($vocabulariesResponse->getContent()));
-        $view->setVariable('resourceTemplateCount', count($resourceTemplatesResponse->getContent()));
+        $view->setVariable('itemCount', $itemsResponse->getTotalResults());
+        $view->setVariable('itemSetCount', $itemSetsResponse->getTotalResults());
+        $view->setVariable('vocabularyCount', $vocabulariesResponse->getTotalResults());
+        $view->setVariable('resourceTemplateCount', $resourceTemplatesResponse->getTotalResults());
         return $view;
     }
 }

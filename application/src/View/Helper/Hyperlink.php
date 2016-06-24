45f5884d12dadd09a09599c0f254ea9f849e6630
Jim Safley <jimsafley@gmail.com>
2016-06-24 10:54:56 -0400

    Allow empty anchor tag

--- application/src/View/Helper/Hyperlink.php
+++ application/src/View/Helper/Hyperlink.php
@@ -8,20 +8,14 @@ class Hyperlink extends AbstractHtmlElement
     /**
      * Render a HTML hyperlink.
      *
-     * @param string $text The hyperlink text
+     * @param string|null $text The hyperlink text
      * @param string|null $href The hyperlink href URL
      * @param array $attributes The hyperlink attributes
      * @return string
      */
-    public function __invoke($text, $href, array $attributes = [])
+    public function __invoke($text = null, $href = null, array $attributes = [])
     {
-        $escape = $this->getView()->plugin('escapehtml');
-
-        if (null === $href) {
-            return $escape($text);
-        }
-
         $attributes['href'] = $href;
-        return '<a' . $this->htmlAttribs($attributes) . '>' . $escape($text) . '</a>';
+        return '<a' . $this->htmlAttribs($attributes) . '>' . $this->getView()->escapeHtml($text) . '</a>';
     }
 }

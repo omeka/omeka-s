85e619fca531dd04b4185decfe12e058002050f8
efalcy <efalcy@afi-sa.fr>
2016-07-05 19:02:20 +0200

    add glob function to FileWriter

--- application/src/File/FileWriter.php
+++ application/src/File/FileWriter.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 namespace Omeka\File;
 
 class FileWriter {
@@ -37,45 +37,6 @@ class FileWriter {
 
     public function chmod($path, $permission) {
         return chmod($path, $permission);
-    }
-
-class MockFileWriter {
-    protected $files = [];
-    public function moveUploadedFile($source,$destination) {
-        array_diff($this->files,[$source]);
-        $this->files[]=$destination;
-        return true;
-    }
-    public function is_dir($path) {
-        return true;
-    }
-
-    public function addFile($path) {
-        $this->files[]=$path;
-    }
-
-    public function fileExists($path) {
-        if (in_array($path,$this->files))
-            return true;
-        return false;
-    }
-
-    public function is_writable($path) {
-        return true;
-    }
-    public function chmod($path, $permission) {
-        return true;
-    }
-
-    public function rename($path, $destination) {
-        array_diff($this->files,[$path]);
-        $this->files[]=$destination;
-        return true;
-    }
-
-    public function mkdir($directory_name, $permissions='0777') {
-        echo $directory_name;
-        return true;
     }
 
     public function glob($pattern, $flag=0) {

6bb6c98ea6153556b593d12a1f1190433ecb9207
John Flatness <john@zerocrates.org>
2016-07-15 16:18:59 -0400

    Bump version to 0.7.0-alpha

--- application/Module.php
+++ application/Module.php
@@ -21,7 +21,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '0.6.2-alpha';
+    const VERSION = '0.7.0-alpha';
 
     /**
      * The vocabulary IRI used to define Omeka application data.

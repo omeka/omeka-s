04d06777f3607eee6a7302171006c5e1f45e721f
Jim Safley <jimsafley@gmail.com>
2016-08-03 20:44:56 -0400

    Remove unused use

--- application/src/Site/BlockLayout/PageTitle.php
+++ application/src/Site/BlockLayout/PageTitle.php
@@ -1,7 +1,6 @@
 <?php
 namespace Omeka\Site\BlockLayout;
 
-use Zend\Form\Element\Select;
 use Omeka\Api\Representation\SiteRepresentation;
 use Omeka\Api\Representation\SitePageBlockRepresentation;
 use Zend\View\Renderer\PhpRenderer;

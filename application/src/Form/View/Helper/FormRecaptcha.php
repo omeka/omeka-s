6c78d4c0b479cb462632430f4db9491343e4c45f
Jim Safley <jimsafley@gmail.com>
2016-09-22 11:25:51 -0400

    Remove prepareRecaptcha helper
    fixes #746

--- application/src/Form/View/Helper/FormRecaptcha.php
+++ application/src/Form/View/Helper/FormRecaptcha.php
@@ -13,6 +13,15 @@ class FormRecaptcha extends AbstractHelper
 
     public function render(ElementInterface $element)
     {
+        $view = $this->getView();
+        $view->headScript()->appendFile(
+            $view->assetUrl('js/recaptcha.js', 'Omeka')
+        );
+        $view->headScript()->appendFile(
+            'https://www.google.com/recaptcha/api.js?onload=recaptchaCallback&render=explicit',
+            'text/javascript',
+            ['async' => true, 'defer' => true]
+        );
         return sprintf(
             '<div %s></div>',
             $this->createAttributesString($element->getAttributes())

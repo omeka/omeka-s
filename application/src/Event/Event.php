1307c841d1e5777419679731b1df1296cedca870
patrickmj <patrickmjchnm@gmail.com>
2016-10-14 10:47:21 -0400

    generalize form events
    There's no reason to make separate event names when they are triggered from different classes. Instead, use the same event name, and attach it to different classes.

--- application/src/Event/Event.php
+++ application/src/Event/Event.php
@@ -96,16 +96,10 @@ class Event extends ZendEvent
     const SERVICE_REGISTERED_NAMES = 'service.registered_names';
 
     // Site settings events
-    const SITE_SETTINGS_ADD_ELEMENTS = 'site_settings.add_elements';
+    const ADD_ELEMENTS = 'form.add_elements';
     // Separate events because calling $form->getInputFilters()
     // resets everythhing
-    const SITE_SETTINGS_ADD_INPUT_FILTERS = 'site_settings.add_input_filters';
-
-    // Global settings events
-    const GLOBAL_SETTINGS_ADD_ELEMENTS = 'global_settings.add_elements';
-    // Separate events because calling $form->getInputFilters()
-    // resets everythhing
-    const GLOBAL_SETTINGS_ADD_INPUT_FILTERS = 'global_settings.add_input_filters';
+    const ADD_INPUT_FILTERS = 'form.add_input_filters';
 
     // Resource visibility SQL filter event
     const SQL_FILTER_RESOURCE_VISIBILITY = 'sql_filter.resource_visibility';

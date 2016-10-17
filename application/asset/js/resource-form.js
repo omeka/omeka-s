ec2eb8aa3406a3f973cc300b48a25cee078f17d5
Jim Safley <jimsafley@gmail.com>
2016-10-17 10:21:21 -0400

    Add visual cue for an invalid language tag
    Uses a strict regular expression that allows common input errors, such as "English". Refs #681

--- application/asset/js/resource-form.js
+++ application/asset/js/resource-form.js
@@ -31,6 +31,18 @@
             }
         });
 
+        $('input.value-language').on('keyup', function(e) {
+            var thisInput = $(this);
+            var languageTag = thisInput.val();
+            // @see http://stackoverflow.com/questions/7035825/regular-expression-for-a-language-tag-as-defined-by-bcp47
+            if ('' == languageTag
+                || languageTag.match(/^(((en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((([A-Za-z]{2,3}(-([A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?)|[A-Za-z]{4}|[A-Za-z]{5,8})(-([A-Za-z]{4}))?(-([A-Za-z]{2}|[0-9]{3}))?(-([A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*(-([0-9A-WY-Za-wy-z](-[A-Za-z0-9]{2,8})+))*(-(x(-[A-Za-z0-9]{1,8})+))?)|(x(-[A-Za-z0-9]{1,8})+))$/)) {
+                thisInput.css('background-color', 'transparent');
+            } else {
+                thisInput.css('background-color', '#f1dada');
+            }
+        });
+
         // Make new value inputs whenever "add value" button clicked.
         $('#properties').on('click', '.add-value', function(e) {
             e.preventDefault();

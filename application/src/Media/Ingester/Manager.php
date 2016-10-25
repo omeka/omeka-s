b3a10693bded8c4b4d1ff7bd6239af44c6475999
John Flatness <john@zerocrates.org>
2016-10-25 16:40:06 -0400

    Fix deprecation warning for ingester fallback

--- application/src/Media/Ingester/Manager.php
+++ application/src/Media/Ingester/Manager.php
@@ -19,7 +19,7 @@ class Manager extends AbstractPluginManager
         try {
             $instance = parent::get($name, $options, $usePeeringServiceManagers);
         } catch (ServiceNotFoundException $e) {
-            $instance = new Fallback($name, $this->getServiceLocator()->get('MvcTranslator'));
+            $instance = new Fallback($name, $this->creationContext->get('MvcTranslator'));
         }
         return $instance;
     }

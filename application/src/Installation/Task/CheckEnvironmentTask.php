306f91121815be71948a8e4c91274bee9fadc30f
John Flatness <john@zerocrates.org>
2016-11-03 13:53:52 -0400

    Actually check for real min php in install

--- application/src/Installation/Task/CheckEnvironmentTask.php
+++ application/src/Installation/Task/CheckEnvironmentTask.php
@@ -8,7 +8,7 @@ use Omeka\Installation\Installer;
  */
 class CheckEnvironmentTask implements TaskInterface
 {
-    const PHP_MINIMUM_VERSION = '5.5';
+    const PHP_MINIMUM_VERSION = '5.6';
 
     public static $requiredExtensions = [
         'PDO',

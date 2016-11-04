949a7a16a040b5ed552773a35224fe73825d77a2
John Flatness <john@zerocrates.org>
2016-11-04 17:26:27 -0400

    Add support for running tests from gulp
    (#721)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,43 @@
 'use strict';
 
 var gulp = require('gulp');
+var child_process = require('child_process');
+var Promise = require('bluebird');
+
+var composerDir = __dirname + '/vendor/bin';
+var buildDir = __dirname + '/build';
+
+function runCommand (cmd, args, options) {
+    return new Promise(function (resolve, reject) {
+        if (!options) {
+            options = {};
+        }
+        if (!options.stdio) {
+            options.stdio = 'inherit';
+        }
+        child_process.spawn(cmd, args, options)
+            .on('exit', function (code) {
+                if (code !== 0) {
+                    reject(new Error(`Command "${cmd}" exited with code ${code}.`));
+                } else {
+                    resolve();
+                }
+            });
+    });
+}
+
+function testCs() {
+    return runCommand('vendor/bin/php-cs-fixer', ['fix', '--dry-run', '--verbose', '--diff']);
+}
+
+function testPhp() {
+    return runCommand(composerDir + '/phpunit', [
+        '-d',
+        'date.timezone=America/New_York',
+        '--log-junit',
+        buildDir + '/test-results.xml'
+    ], {cwd: 'application/test'});
+}
 
 gulp.task('css', function () {
     var sass = require('gulp-sass');
@@ -20,4 +57,11 @@ gulp.task('css', function () {
 
 gulp.task('css:watch', function () {
     gulp.watch('./application/asset/sass/*.scss', ['css']);
+});
+
+
+gulp.task('test:cs', testCs);
+gulp.task('test:php', testPhp);
+gulp.task('test', function () {
+    return testPhp().then(testCs);
 });

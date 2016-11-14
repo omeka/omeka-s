1e6bbfb4b24457355a93b018ee77e028d277c2c7
John Flatness <john@zerocrates.org>
2016-11-14 13:30:21 -0500

    Use HTML5 validation for color picker
    (#777)

--- application/asset/js/color-picker.js
+++ application/asset/js/color-picker.js
@@ -1,32 +1,26 @@
-/**
- * Validate color picker user input
- *
- * Sets the color to the sample div if valid. If invalid, marks the input as
- * invalid. A valid color is a three- or six-digit hexadecimal form, or
- * "transparent".
- *
- * @param Object input
- */
-var validateColor = function(input) {
-    var sampleDiv = input.siblings('.color-picker-sample');
-    var color = input.val();
-    if ('' == color) {
-        input.css('background-color', '#ffffff');
-        sampleDiv.css('background-color', 'transparent');
-    } else if (color.match(/^#([0-9a-f]{3}){1,2}$/i) || 'transparent' == color) {
-        input.css('background-color', '#ffffff');
-        sampleDiv.css('background-color', color);
-    } else {
-        input.css('background-color', '#f1dada');
-        sampleDiv.css('background-color', 'transparent');
-    }
-}
-
 $(document).ready(function() {
+
+    /**
+     * Sets the color to the sample div if valid.
+     *
+     * Validation is handled by the "pattern" attribute.
+     *
+     * @param Object input
+     */
+    var updateColorSample = function(input) {
+        var sampleDiv = input.siblings('.color-picker-sample');
+        var color = input.val();
+        if ('' !== color && input[0].checkValidity()) {
+            sampleDiv.css('background-color', color);
+        } else {
+            sampleDiv.css('background-color', 'transparent');
+        }
+    }
+
     $('.color-picker').each(function() {
-        validateColor($(this).children('input'));
+        updateColorSample($(this).children('input'));
     });
     $(document).on('keyup', '.color-picker > input', function(e) {
-        validateColor($(this));
+        updateColorSample($(this));
     });
 });

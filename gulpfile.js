0d48cda098fde78faec70b52a29215cb055fa60d
John Flatness <john@zerocrates.org>
2017-02-15 17:31:56 -0500

    Implement more of Ant script into Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 'use strict';
 
-var gulp = require('gulp');
 var child_process = require('child_process');
-var Promise = require('bluebird');
 var fs = require('fs');
+var readline = require('readline');
+
+var Promise = require('bluebird');
+var dateFormat = require('dateformat');
+var glob = require('glob');
+var tmp = require('tmp');
+
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
+
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 var composerDir = __dirname + '/vendor/bin';
 var buildDir = __dirname + '/build';
+var dataDir = __dirname + '/application/data';
+var scriptsDir = dataDir + '/scripts';
+var langDir = __dirname + '/application/language';
 
 function download(url, path) {
     return new Promise(function (resolve, reject) {
@@ -84,10 +100,6 @@ function testPhp() {
 }
 
 gulp.task('css', function () {
-    var sass = require('gulp-sass');
-    var postcss = require('gulp-postcss');
-    var autoprefixer = require('autoprefixer');
-
     return gulp.src('./application/asset/sass/*.scss')
         .pipe(sass({
             outputStyle: 'compressed',
@@ -113,7 +125,99 @@ gulp.task('test', function () {
 gulp.task('deps', function () {
     return composer(['install']);
 });
-
 gulp.task('deps:update', function () {
     return composer(['update']);
 });
+
+gulp.task('dedist', function () {
+    return gulp.src(['./.htaccess.dist', './config/*.dist', './logs/*.dist', './application/test/config/*.dist'], {base: '.'})
+        .pipe(rename(function (path) {
+            path.extname = '';
+        }))
+        // workaround to prevent overwrites (possibly removable under Gulp 4)
+        .pipe(changed('.', {hasChanged: function (stream, cb, src, dest) {
+            fs.stat(dest, function (err) {
+                if (err) {
+                    stream.push(src);
+                }
+                cb();
+            });
+        }}))
+        .pipe(gulp.dest('.'))
+});
+
+gulp.task('db:schema', function () {
+    return runCommand('php', [scriptsDir + '/create-schema.php']);
+});
+gulp.task('db:proxies', function () {
+    return runCommand(composerDir + '/doctrine', ['orm:generate-proxies']);
+});
+gulp.task('db:create-migration', function () {
+    return new Promise(function(resolve, reject) {
+        var now = new Date();
+        var timestamp = dateFormat(now, 'UTC:yyyymmddhhMMss');
+        var rl = readline.createInterface({input: process.stdin, output: process.stdout});
+        rl.question('Migration name (UpperCamelCased): ', function (migrationName) {
+            rl.close();
+            gulp.src(dataDir + '/build/migration.php.tpl')
+                .pipe(replace(/@ClassName@/g, migrationName))
+                .pipe(rename(timestamp + '_' + migrationName + '.php'))
+                .pipe(gulp.dest(dataDir + '/migrations/'))
+                .on('end', resolve);
+        });
+    });
+})
+gulp.task('db', ['db:schema', 'db:proxies']);
+
+gulp.task('i18n:template', function () {
+    var pot = langDir + '/template.pot';
+    return Promise.all([
+        new Promise(function(resolve, reject) {
+            glob('**/*.{php,phtml}', {ignore: ['themes/**', 'modules/**']}, function (err, files) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                tmp.file({postfix: 'xgettext.pot'}, function (err, path, fd) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    var args = ['--language=php', '--from-code=utf-8', '--keyword=translate', '-o', path];
+                    runCommand('xgettext', args.concat(files)).then(function () {
+                        resolve(path);
+                    });
+                });
+            });
+        }),
+        new Promise(function (resolve, reject) {
+            tmp.file({postfix: 'tagged.pot'}, function (err, path, fd) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                var args = ['--language=php', '--from-code=utf-8', '--keyword=translate', '-o', path];
+                runCommand(composerDir + '/extract-tagged-strings.php', [], {stdio: ['pipe', fd, 'pipe']}).then(function () {
+                    resolve(path);
+                });
+            });
+        }),
+        new Promise(function (resolve, reject) {
+            tmp.file({postfix: 'vocab.pot'}, function (err, path, fd) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                var args = ['--language=php', '--from-code=utf-8', '--keyword=translate', '-o', path];
+                runCommand('php', [scriptsDir + '/extract-vocab-strings.php'], {stdio: ['pipe', fd, 'pipe']}).then(function () {
+                    resolve(path);
+                });
+            });
+        })
+    ])
+    .then(function (tempFiles) {
+        return runCommand('msgcat', tempFiles.concat(['-o', pot]));
+    });
+});
+
+gulp.task('init', ['dedist', 'deps']);

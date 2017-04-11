e19241acd274502d0005dd566c8dde21ea100b6b
Jim Safley <jimsafley@gmail.com>
2017-04-11 13:12:14 -0400

    Refactor ValueHydrator

--- application/src/Api/Adapter/AbstractResourceEntityAdapter.php
+++ application/src/Api/Adapter/AbstractResourceEntityAdapter.php
@@ -120,10 +120,7 @@ abstract class AbstractResourceEntityAdapter extends AbstractEntityAdapter
         }
 
         // Hydrate this resource's values.
-        $append = $request->getOperation() === Request::UPDATE
-            && $request->getOption('isPartial', false);
-        $valueHydrator = new ValueHydrator($this);
-        $valueHydrator->hydrate($data, $entity, $append);
+        $valueHydrator = (new ValueHydrator)->hydrate($request, $entity, $this);
 
         // o:owner
         $this->hydrateOwner($request, $entity);

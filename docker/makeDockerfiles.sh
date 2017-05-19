5533595ae598eb9cb16a03f99d84c8df7bcad45e
Ross McFadyen <ross.mcfadyen@digirati.com>
2017-05-19 21:00:07 +0100

    Update makeDockerfiles.sh

--- docker/makeDockerfiles.sh
+++ docker/makeDockerfiles.sh
@@ -23,7 +23,7 @@ function buildDockerFile {
   docker build --file=${DOCKERFILE} -t ${PACKAGE_NAME}:${2} .
 
   echo -e "\033[00;32m ===> Spinning up a container running ${2} and attempting to run unit tests \033[0m\n";
-  docker run -name ${2} -i -t ${PACKAGE_NAME}:${2} /bin/sh -c " ./node_modules/gulp/bin/gulp.js init"
+  docker run --name ${2} -i -t ${PACKAGE_NAME}:${2} /bin/sh -c " ./node_modules/gulp/bin/gulp.js init"
 
   echo -e "\033[00;32m ===> Pushing to Dockerhub\033[0m\n";
   docker tag ${PACKAGE_NAME}:${2} ${DOCKER_USER}/${PACKAGE_NAME}:${2}

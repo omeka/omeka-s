95c2cad438dae0b2fb87de5fb3981dba4b260f99
Ross McFadyen <ross.mcfadyen@digirati.com>
2017-05-22 13:26:29 +0100

    mysql wait

--- docker/makeDockerfiles.sh
+++ docker/makeDockerfiles.sh
@@ -23,22 +23,36 @@ function buildDockerFile {
   docker build --file=${DOCKERFILE} -t ${PACKAGE_NAME}:${2} .
   docker tag ${PACKAGE_NAME}:${2} ${DOCKER_USER}/${PACKAGE_NAME}:${2}
 
-  docker run --name mysql -d -p 3306:3306 -e "MYSQL_ALLOW_EMPTY_PASSWORD=yes"  mysql:5.7
-  sleep 2m
   docker exec -i mysql mysql -uroot  <<< "create database IF NOT EXISTS omeka_test;"
 
-
-  
   docker images
   
   echo -e "\033[00;32m ===> Spinning up a container running ${2} and attempting to run unit tests \033[0m\n";
   docker run -i -t --link mysql:mysql ${PACKAGE_NAME}:${2} /bin/sh -c "sed -i 's/^host.*/host = "mysql"/' application/test/config/database.ini && sed -i 's/^user.*/user = "root"/' application/test/config/database.ini && sed -i 's/^dbname.*/dbname = "omeka_test"/' application/test/config/database.ini &&./node_modules/gulp/bin/gulp.js test:php"
-  
+
+  docker exec -i mysql mysql -uroot  <<< "drop database omeka_test;"
+
   echo -e "\033[00;32m ===> Pushing to Dockerhub\033[0m\n";
   docker push ${DOCKER_USER}/${PACKAGE_NAME}:${2}
   cd -;
   echo -e "\033[00;32m====================S=U=C=C=E=S=S=======================\033[0m\n";
 }
+
+function startDB {
+
+ docker run --name mysql -d -p 3306:3306 -e "MYSQL_ALLOW_EMPTY_PASSWORD=yes"  mysql:5.7
+ until nc -z -v -w30 localhost 3306
+ do
+   echo "Waiting for database connection..."
+   # wait for 5 seconds before check again
+   sleep 5
+ done
+
+}
+
+startDB
+
+
 
 for php in "${PHP_VERSIONS[@]}"
 do

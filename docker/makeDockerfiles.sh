50f9e93d59080142cbb6b0fb6f1555124b54fbaf
Ross McFadyen <ross.mcfadyen@digirati.com>
2017-05-22 12:07:50 +0100

    mysql

--- docker/makeDockerfiles.sh
+++ docker/makeDockerfiles.sh
@@ -23,15 +23,15 @@ function buildDockerFile {
   docker build --file=${DOCKERFILE} -t ${PACKAGE_NAME}:${2} .
   docker tag ${PACKAGE_NAME}:${2} ${DOCKER_USER}/${PACKAGE_NAME}:${2}
 
-  docker run --name mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:5.7
-  docker exec -i mysql mysql -uroot  <<< "create database IF NOT EXISTS omeka_test;"
+  docker run --name omekas-mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:5.7
+  docker exec -i omekas-mysql mysql -uroot  <<< "create database IF NOT EXISTS omeka_test;"
 
 
   
   docker images
   
   echo -e "\033[00;32m ===> Spinning up a container running ${2} and attempting to run unit tests \033[0m\n";
-  docker run -i -t --link mysql:mysql ${PACKAGE_NAME}:${2} /bin/sh -c "sed -i 's/^host.*/host = "mysql"/' application/test/config/database.ini && sed -i 's/^user.*/user = "root"/' application/test/config/database.ini && sed -i 's/^dbname.*/dbname = "omeka_test"/' application/test/config/database.ini &&./node_modules/gulp/bin/gulp.js test:php"
+  docker run -i -t --link omekas-mysql:mysql ${PACKAGE_NAME}:${2} /bin/sh -c "sed -i 's/^host.*/host = "mysql"/' application/test/config/database.ini && sed -i 's/^user.*/user = "root"/' application/test/config/database.ini && sed -i 's/^dbname.*/dbname = "omeka_test"/' application/test/config/database.ini &&./node_modules/gulp/bin/gulp.js test:php"
   
   echo -e "\033[00;32m ===> Pushing to Dockerhub\033[0m\n";
   docker push ${DOCKER_USER}/${PACKAGE_NAME}:${2}

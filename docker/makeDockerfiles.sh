be20f8ecc523360bdeedbff15f26cb137600fb47
Stephen Fraser <stephenwf@users.noreply.github.com>
2017-05-24 09:08:12 +0100

    Standardise messaging

--- docker/makeDockerfiles.sh
+++ docker/makeDockerfiles.sh
@@ -30,40 +30,30 @@ function buildDockerFile {
   echo -e "\033[00;32m ===> Spinning up a container running ${2} and attempting to run unit tests \033[0m\n";
   docker run -i -t --link mysql:mysql ${PACKAGE_NAME}:${2} /bin/sh -c "sed -i 's/^host.*/host = "mysql"/' application/test/config/database.ini && sed -i 's/^user.*/user = "root"/' application/test/config/database.ini && sed -i 's/^dbname.*/dbname = "omeka_test"/' application/test/config/database.ini &&./node_modules/gulp/bin/gulp.js test:php"
   
+  echo -e "\033[00;32m ===> Dropping previous database\033[0m\n";
   docker exec -i mysql mysql -uroot  <<< "drop database omeka_test;"
   
-  
-  
   if [ $? -ne 0 ]; then
-    echo "Unit tests didn't pass, we won't we pushing this image"
+   echo -e "\033[00;31m ERROR: Unit tests didn't pass, we won't we pushing this image\033[0m\n";
   else
-  
     echo -e "\033[00;32m ===> Pushing to Dockerhub\033[0m\n";
     docker push ${DOCKER_USER}/${PACKAGE_NAME}:${2}
     cd -;
     echo -e "\033[00;32m====================S=U=C=C=E=S=S=======================\033[0m\n";
-  
-  fi
-  
-  
-  
+  fi 
 }
 
-function startDB {
+echo -e "\033[00;32m ===> Creating database container for running tests\033[0m\n";
 
- docker run --name mysql -d -p 3306:3306 -e "MYSQL_ALLOW_EMPTY_PASSWORD=yes"  mysql:5.7
- until nc -z -v -w30 localhost 3306
- do
-   echo "Waiting for database connection..."
+docker run --name mysql -d -p 3306:3306 -e "MYSQL_ALLOW_EMPTY_PASSWORD=yes"  mysql:5.7
+
+# Wait for database to be ready
+until nc -z -v -w30 localhost 3306
+do
+   echo -e "\033[00;33m ===> Waiting for database connection...\033[0m\n";
    # wait for 5 seconds before check again
    sleep 5
- done
-
-}
-
-startDB
-
-
+done
 
 for php in "${PHP_VERSIONS[@]}"
 do
@@ -71,8 +61,6 @@ do
   for server in "${WEB_SERVERS[@]}"
   do
     :
-    # Set up our docker ignore in the root for duration.
-    cp ./.dockerignore ../.dockerignore
 
     DOCKER_TAG="${php}-${server}"
     BASE_TEMPLATE="./Dockerfile"
@@ -87,7 +75,5 @@ do
 
     buildDockerFile ${OUTFILE} ${DOCKER_TAG}
 
-    # Remove ignore again.
-    rm -f ../.dockerignore
   done
 done

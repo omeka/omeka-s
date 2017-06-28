90749113ddae057b0dc5fc9a12a2d3a966ff6ed4
Jim Safley <jimsafley@gmail.com>
2017-06-27 22:35:27 -0400

    Move property edit to sidebar

--- application/asset/js/resource-template-form.js
+++ application/asset/js/resource-template-form.js
@@ -54,22 +54,12 @@ $('#content').on('click', '.resource-template-property-remove', function(event) 
     removeLink.hide();
 });
 
-var getDataTypeOptionsForm = function(dataType) {
-    var optionsForm = dataTypeOptionsForms[dataType];
-    return optionsForm ? optionsForm : null;
-}
-
-var openDataTypeOptionsForm = function(property) {
-    var dataType = property.find('.data-type-select :selected').val()
-    var sidebar = $('#data-type-options-sidebar');
-
-    Omeka.closeSidebar(sidebar);
-    $('#properties .property').removeClass('selected-data-type');
-
-    if (optionsForm = getDataTypeOptionsForm(dataType)) {
-        property.addClass('selected-data-type');
-        sidebar.find('.sidebar-content').html(optionsForm);
-        Omeka.openSidebar(sidebar);
+var setDataTypeOptionsForm = function(dataType) {
+    optionsForm = dataTypeOptionsForms[dataType];
+    if (optionsForm) {
+        $('#data-type-options').html(optionsForm);
+    } else {
+        $('#data-type-options').empty();
     }
 }
 
@@ -79,12 +69,38 @@ $('#data-type-options-forms > span').each(function() {
     dataTypeOptionsForms[span.data('data-type')] = span.data('options-form')
 });
 
-$('#properties').on('click', '.data-type-button', function(e) {
-    openDataTypeOptionsForm($(this).closest('.property'));
+$('#properties').on('click', '.property-edit', function(e) {
+    e.preventDefault();
+
+    var sidebar = $('#edit-sidebar');
+    Omeka.closeSidebar(sidebar);
+
+    var property = $(this).closest('.property');
+    var altLabel = property.find('.alternate-label')
+    var altComment = property.find('.alternate-comment')
+    var isRequired = property.find('.is-required')
+    var dataType = property.find('.data-type')
+
+    $('#alternate-label').val(altLabel.val());
+    $('#alternate-comment').val(altComment.val());
+    $('#is-required').prop('checked', isRequired.val());
+    $('#data-type').val(dataType.val());
+    setDataTypeOptionsForm(dataType.val());
+
+    $('#set-changes').off().on('click', function(e) {
+        altLabel.val($('#alternate-label').val());
+        altComment.val($('#alternate-comment').val());
+        $('#is-required').prop('checked') ? isRequired.val(1) : isRequired.val(null);
+        dataType.val($('#data-type').val());
+
+        Omeka.closeSidebar(sidebar);
+    });
+
+    Omeka.openSidebar(sidebar);
 });
 
-$('#properties').on('change', '.data-type-select', function(e) {
-    openDataTypeOptionsForm($(this).closest('.property'));
+$('#data-type').on('change', function(e) {
+    setDataTypeOptionsForm($(this).find(':selected').val());
 });
 
 });

88d98a6ef159d50a2581065f3422d3f4ce9802c5
Jim Safley <jimsafley@gmail.com>
2017-07-14 13:59:30 -0400

    Separate global Chosen options
    This will allow dynamically added Chosen selects to reuse the options.

--- application/asset/js/chosen-options.js
+++ application/asset/js/chosen-options.js
@@ -1,10 +1,11 @@
-$(document).ready(function () {
-
-$(".chosen-select").chosen({
+/**
+ * Chosen default options.
+ *
+ * @see https://harvesthq.github.io/chosen/
+ */
+var chosenOptions = {
     allow_single_deselect: true,
     disable_search_threshold: 10,
     width: '100%',
     include_group_label_in_selected: true,
-});
-
-});
+};

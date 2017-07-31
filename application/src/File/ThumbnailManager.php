6b03a8b7ff848f07646348c679c73438ef8de201
Jim Safley <jimsafley@gmail.com>
2017-07-31 16:21:06 -0400

    Validate thumbnail manager config

--- application/src/File/ThumbnailManager.php
+++ application/src/File/ThumbnailManager.php
@@ -22,6 +22,7 @@ class ThumbnailManager
     {
         $this->services = $services;
         $config = $services->get('Config');
+        $this->validateConfig($config);
         $this->config = $config['thumbnails'];
     }
 
@@ -93,5 +94,38 @@ class ThumbnailManager
     public function getThumbnailerOptions()
     {
         return $this->config['thumbnailer_options'];
+    }
+
+    public function validateConfig($config)
+    {
+        if (!isset($config['thumbnails'])) {
+            throw new Exception\ConfigException('Missing thumbnail configuration.'); // @translate
+        }
+        $config = $config['thumbnails'];
+        if (!isset($config['types']) || !is_array($config['types'])) {
+            throw new Exception\ConfigException('Missing thumbnail types configuration.'); // @translate
+        }
+        if (!isset($config['types']['large']) || !isset($config['types']['medium']) || !isset($config['types']['square'])) {
+            throw new Exception\ConfigException('Missing the large, medium, or square thumbnail type configuration.'); // @translate
+        }
+        foreach ($config['types'] as $type) {
+            if (!isset($type['constraint'])) {
+                throw new Exception\ConfigException('Missing constraint for a thumbnail type configuration.'); // @translate
+            }
+        }
+        if (!isset($config['fallbacks']) || !is_array($config['fallbacks'])) {
+            throw new Exception\ConfigException('Missing thumbnail fallbacks configuration.'); // @translate
+        }
+        if (!isset($config['fallbacks']['default']) || !is_array($config['fallbacks']['default']) || 2 !== count($config['fallbacks']['default'])) {
+            throw new Exception\ConfigException('Missing default thumbnail fallback configuration.'); // @translate
+        }
+        if (!isset($config['fallbacks']['fallbacks']) || !is_array($config['fallbacks']['fallbacks'])) {
+            throw new Exception\ConfigException('Missing thumbnail fallback configuration.'); // @translate
+        }
+        foreach ($config['fallbacks']['fallbacks'] as $fallback) {
+            if (!is_array($fallback) || 2 !== count($fallback)) {
+                throw new Exception\ConfigException('Invalid thumbnail fallback configuration.'); // @translate
+            }
+        }
     }
 }

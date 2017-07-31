bf8066404711c4947d6fc78f6ad72b8a92039314
Jim Safley <jimsafley@gmail.com>
2017-07-31 13:00:03 -0400

    Remove MEDIA_TYPE_ALIASES const

--- application/src/File/Manager.php
+++ application/src/File/Manager.php
@@ -65,54 +65,6 @@ class Manager
         'video/x-ms-wmv',
     ];
 
-    /**
-     * Map of nonstandard-to-standard media types.
-     */
-    const MEDIA_TYPE_ALIASES = [
-        // application/ogg
-        'application/x-ogg' => 'application/ogg',
-        // application/rtf
-        'text/rtf' => 'application/rtf',
-        // audio/midi
-        'audio/mid' => 'audio/midi',
-        'audio/x-midi' => 'audio/midi',
-        // audio/mpeg
-        'audio/mp3' => 'audio/mpeg',
-        'audio/mpeg3' => 'audio/mpeg',
-        'audio/x-mp3' => 'audio/mpeg',
-        'audio/x-mpeg' => 'audio/mpeg',
-        'audio/x-mpeg3' => 'audio/mpeg',
-        'audio/x-mpegaudio' => 'audio/mpeg',
-        'audio/x-mpg' => 'audio/mpeg',
-        // audio/ogg
-        'audio/x-ogg' => 'audio/ogg',
-        // audio/x-aac
-        'audio/aac' => 'audio/x-aac',
-        // audio/x-aiff
-        'audio/aiff' => 'audio/x-aiff',
-        // audio/x-ms-wma
-        'audio/x-wma' => 'audio/x-ms-wma',
-        'audio/wma' => 'audio/x-ms-wma',
-        // audio/mp4
-        'audio/x-mp4' => 'audio/mp4',
-        'audio/x-m4a' => 'audio/mp4',
-        // audio/x-wav
-        'audio/wav' => 'audio/x-wav',
-        // image/bmp
-        'image/x-ms-bmp' => 'image/bmp',
-        // image/x-icon
-        'image/icon' => 'image/x-icon',
-        // video/mp4
-        'video/x-m4v' => 'video/mp4',
-        // video/x-ms-asf
-        'video/asf' => 'video/x-ms-asf',
-        // video/x-ms-wmv
-        'video/wmv' => 'video/x-ms-wmv',
-        // video/x-msvideo
-        'video/avi' => 'video/x-msvideo',
-        'video/msvideo' => 'video/x-msvideo',
-    ];
-
     const EXTENSION_WHITELIST = [
         'aac', 'aif', 'aiff', 'asf', 'asx', 'avi', 'bmp', 'c', 'cc', 'class',
         'css', 'divx', 'doc', 'docx', 'exe', 'gif', 'gz', 'gzip', 'h', 'ico',
--- application/src/File/TempFile.php
+++ application/src/File/TempFile.php
@@ -9,6 +9,54 @@ use Zend\Math\Rand;
 class TempFile
 {
     /**
+     * Map of nonstandard-to-standard media types.
+     */
+    const MEDIA_TYPE_ALIASES = [
+        // application/ogg
+        'application/x-ogg' => 'application/ogg',
+        // application/rtf
+        'text/rtf' => 'application/rtf',
+        // audio/midi
+        'audio/mid' => 'audio/midi',
+        'audio/x-midi' => 'audio/midi',
+        // audio/mpeg
+        'audio/mp3' => 'audio/mpeg',
+        'audio/mpeg3' => 'audio/mpeg',
+        'audio/x-mp3' => 'audio/mpeg',
+        'audio/x-mpeg' => 'audio/mpeg',
+        'audio/x-mpeg3' => 'audio/mpeg',
+        'audio/x-mpegaudio' => 'audio/mpeg',
+        'audio/x-mpg' => 'audio/mpeg',
+        // audio/ogg
+        'audio/x-ogg' => 'audio/ogg',
+        // audio/x-aac
+        'audio/aac' => 'audio/x-aac',
+        // audio/x-aiff
+        'audio/aiff' => 'audio/x-aiff',
+        // audio/x-ms-wma
+        'audio/x-wma' => 'audio/x-ms-wma',
+        'audio/wma' => 'audio/x-ms-wma',
+        // audio/mp4
+        'audio/x-mp4' => 'audio/mp4',
+        'audio/x-m4a' => 'audio/mp4',
+        // audio/x-wav
+        'audio/wav' => 'audio/x-wav',
+        // image/bmp
+        'image/x-ms-bmp' => 'image/bmp',
+        // image/x-icon
+        'image/icon' => 'image/x-icon',
+        // video/mp4
+        'video/x-m4v' => 'video/mp4',
+        // video/x-ms-asf
+        'video/asf' => 'video/x-ms-asf',
+        // video/x-ms-wmv
+        'video/wmv' => 'video/x-ms-wmv',
+        // video/x-msvideo
+        'video/avi' => 'video/x-msvideo',
+        'video/msvideo' => 'video/x-msvideo',
+    ];
+
+    /**
      * @var StoreInterface
      */
     protected $store;
@@ -239,8 +287,8 @@ class TempFile
         }
         $finfo = new finfo(FILEINFO_MIME_TYPE);
         $mediaType = $finfo->file($this->getTempPath());
-        if (array_key_exists($mediaType, Manager::MEDIA_TYPE_ALIASES)) {
-            $mediaType = Manager::MEDIA_TYPE_ALIASES[$mediaType];
+        if (array_key_exists($mediaType, self::MEDIA_TYPE_ALIASES)) {
+            $mediaType = self::MEDIA_TYPE_ALIASES[$mediaType];
         }
         $this->mediaType = $mediaType;
         return $this->mediaType;

426a51f25d3a6607aa4d2086e25b04973fe6c844
John Flatness <john@zerocrates.org>
2017-11-07 10:36:04 -0500

    Set version to 1.0.0
    !

--- application/Module.php
+++ application/Module.php
@@ -13,7 +13,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '1.0.0-rc.1';
+    const VERSION = '1.0.0';
 
     /**
      * The vocabulary IRI used to define Omeka application data.

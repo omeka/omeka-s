154af2dfdcbab8325e2bf54c6373482cd1d7c631
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-11-13 00:00:00 +0100

    Fixed user adapter when the role doesn’t change.

--- application/src/Api/Adapter/UserAdapter.php
+++ application/src/Api/Adapter/UserAdapter.php
@@ -61,15 +61,17 @@ class UserAdapter extends AbstractEntityAdapter
 
         $role = $request->getValue('o:role');
         if ($role && $this->shouldHydrate($request, 'o:role')) {
-            $this->authorize($entity, 'change-role');
+            if ($role !== $entity->getRole()) {
+                $this->authorize($entity, 'change-role');
 
-            // Ask specially for permission to set an admin role
-            $acl = $this->getServiceLocator()->get('Omeka\Acl');
-            if ($acl->isAdminRole($role)) {
-                $this->authorize($entity, 'change-role-admin');
+                // Ask specially for permission to set an admin role
+                $acl = $this->getServiceLocator()->get('Omeka\Acl');
+                if ($acl->isAdminRole($role)) {
+                    $this->authorize($entity, 'change-role-admin');
+                }
+
+                $entity->setRole($role);
             }
-
-            $entity->setRole($role);
         }
 
         if ($this->shouldHydrate($request, 'o:is_active')) {

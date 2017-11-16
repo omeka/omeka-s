93f2122822357410bac91d0460ca4e4d7358ec83
Miguel Vieira <jmvieira@gmail.com>
2017-11-16 11:55:50 +0000

    Added script to update omeka from github.

---
+++ update.sh
@@ -0,0 +1,14 @@
+echo '- Updating from origin'
+git pull
+
+echo
+echo '- Install dependencies'
+gulp deps
+echo
+
+echo '- Compare changes in configuration'
+diff config/local.config.php config/local.config.php.dist
+echo
+
+echo '- Go to http://OMEKA_BASE_URL/install to run database migrations'
+echo

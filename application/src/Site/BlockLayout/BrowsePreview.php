618e93e15950a16aef23feafe7806a24f64d9840
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-04-03 00:00:00 +0200

    Fixed issue with radio when there are two blocks.

--- application/src/Site/BlockLayout/BrowsePreview.php
+++ application/src/Site/BlockLayout/BrowsePreview.php
@@ -4,9 +4,8 @@ namespace Omeka\Site\BlockLayout;
 use Omeka\Api\Representation\SiteRepresentation;
 use Omeka\Api\Representation\SitePageRepresentation;
 use Omeka\Api\Representation\SitePageBlockRepresentation;
-use Zend\Form\Element\Number;
-use Zend\Form\Element\Radio;
-use Zend\Form\Element\Text;
+use Zend\Form\Element;
+use Zend\Form\Form;
 use Zend\View\Renderer\PhpRenderer;
 
 class BrowsePreview extends AbstractBlockLayout
@@ -19,72 +18,73 @@ class BrowsePreview extends AbstractBlockLayout
     public function form(PhpRenderer $view, SiteRepresentation $site,
         SitePageRepresentation $page = null, SitePageBlockRepresentation $block = null
     ) {
-        $resourceType = new Radio("o:block[__blockIndex__][o:data][resource_type]");
-        $resourceType->setValueOptions([
-            'items' => $view->translate('Items'),
-            'item_sets' => $view->translate('Item sets'),
-            'media' => $view->translate('Media'),
-        ]);
-        $text = new Text("o:block[__blockIndex__][o:data][query]");
-        $heading = new Text("o:block[__blockIndex__][o:data][heading]");
-        $limit = new Number("o:block[__blockIndex__][o:data][limit]");
-        $linkText = new Text("o:block[__blockIndex__][o:data][link-text]");
-        $expandButton = '<a href="#" class="expand" aria-label="' . $view->translate('Expand'). '" title="' . $view->translate('Expand') . '"></a>';
+        $data = $block ? $block->data() : [];
 
-        if ($block) {
-            $resourceType->setAttribute('value', $block->dataValue('resource_type'));
-            $text->setAttribute('value', $block->dataValue('query'));
-            $heading->setAttribute('value', $block->dataValue('heading'));
-            $limit->setAttribute('value', $block->dataValue('limit'));
-            $linkText->setAttribute('value', $block->dataValue('link-text'));
-        } else {
-            $resourceType->setAttribute('value', 'items');
-            $linkText->setAttribute('value', $view->translate('Browse all'));
-            $limit->setAttribute('value', 12);
+        $form = new Form();
+        $form->add([
+            'name' => 'o:block[__blockIndex__][o:data][resource_type]',
+            'type' => Element\Select::class,
+            'options' => [
+                'label' => 'Resource type', // @translate
+                'value_options' => [
+                    'item_sets' => 'Item sets',  // @translate
+                    'items' => 'Items',  // @translate
+                    'media' => 'Media',  // @translate
+                ],
+            ],
+        ]);
+        $form->add([
+            'name' => 'o:block[__blockIndex__][o:data][query]',
+            'type' => Element\Text::class,
+            'options' => [
+                'label' => 'Query', // @translate
+                'info' => 'Display resources using this search query', // @translate
+            ],
+        ]);
+        $form->add([
+            'name' => 'o:block[__blockIndex__][o:data][heading]',
+            'type' => Element\Text::class,
+            'options' => [
+                'label' => 'Preview title', // @translate
+                'info' => 'Translatable heading above resource list, if any.', // @translate
+            ],
+        ]);
+        $form->add([
+            'name' => 'o:block[__blockIndex__][o:data][limit]',
+            'type' => Element\Number::class,
+            'options' => [
+                'label' => 'Max number', // @translate
+                'info' => 'Maximum number of resources to display in the preview.', // @translate
+            ],
+        ]);
+        $form->add([
+            'name' => 'o:block[__blockIndex__][o:data][link-text]',
+            'type' => Element\Text::class,
+            'options' => [
+                'label' => 'Preview title', // @translate
+                'info' => 'Translatable text for link to full browse view, if any.', // @translate
+            ],
+        ]);
+
+        if (empty($data)) {
+            $data = [
+                'resource_type' => 'items',
+                'query' => '',
+                'heading' => '',
+                'limit' => 12,
+                'link-text' => 'Browse all', // @translate
+            ];
         }
 
-        $html = '';
+        $form->setData([
+            'o:block[__blockIndex__][o:data][resource_type]' => $data['resource_type'],
+            'o:block[__blockIndex__][o:data][query]' => $data['query'],
+            'o:block[__blockIndex__][o:data][heading]' => $data['heading'],
+            'o:block[__blockIndex__][o:data][limit]' => $data['limit'],
+            'o:block[__blockIndex__][o:data][link-text]' => $data['link-text'],
+        ]);
 
-        $html .= '<div class="field"><div class="field-meta">';
-        $html .= '<label>' . $view->translate('Resource type') . '</label>';
-        $html .= '<a href="#" class="expand"></a>';
-        $html .= '</div>';
-        $html .= '<div class="inputs">' . $view->formRow($resourceType) . '</div>';
-        $html .= '</div>';
-
-        $html .= '<div class="field"><div class="field-meta">';
-        $html .= '<label>' . $view->translate('Query') . '</label>';
-        $html .= $expandButton;
-        $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Display resources using this search query') . '</div></div>';
-        $html .= '</div>';
-        $html .= '<div class="inputs">' . $view->formRow($text) . '</div>';
-        $html .= '</div>';
-
-        $html .= '<div class="field"><div class="field-meta">';
-        $html .= '<label>' . $view->translate('Preview title') . '</label>';
-        $html .= $expandButton;
-        $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Translatable heading above resource list') . '</div></div>';
-        $html .= '</div>';
-        $html .= '<div class="inputs">' . $view->formRow($heading) . '</div>';
-        $html .= '</div>';
-
-        $html .= '<div class="field"><div class="field-meta">';
-        $html .= '<label>' . $view->translate('Max number') . '</label>';
-        $html .= '<a href="#" class="expand"></a>';
-        $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Maximum number of resources to display in the preview.') . '</div></div>';
-        $html .= '</div>';
-        $html .= '<div class="inputs">' . $view->formRow($limit) . '</div>';
-        $html .= '</div>';
-
-        $html .= '<div class="field"><div class="field-meta">';
-        $html .= '<label>' . $view->translate('Browse link text') . '</label>';
-        $html .= $expandButton;
-        $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Translatable text for link to full browse view') . '</div></div>';
-        $html .= '</div>';
-        $html .= '<div class="inputs">' . $view->formRow($linkText) . '</div>';
-        $html .= '</div>';
-
-        return $html;
+        return $view->formCollection($form);
     }
 
     public function render(PhpRenderer $view, SitePageBlockRepresentation $block)

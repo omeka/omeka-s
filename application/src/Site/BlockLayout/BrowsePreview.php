969b288556a5937714c9c82d113614e0a074550e
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-04-03 00:00:00 +0200

    Added an option to select the resource type to browse.

--- application/src/Site/BlockLayout/BrowsePreview.php
+++ application/src/Site/BlockLayout/BrowsePreview.php
@@ -5,6 +5,7 @@ use Omeka\Api\Representation\SiteRepresentation;
 use Omeka\Api\Representation\SitePageRepresentation;
 use Omeka\Api\Representation\SitePageBlockRepresentation;
 use Zend\Form\Element\Number;
+use Zend\Form\Element\Radio;
 use Zend\Form\Element\Text;
 use Zend\View\Renderer\PhpRenderer;
 
@@ -18,6 +19,12 @@ class BrowsePreview extends AbstractBlockLayout
     public function form(PhpRenderer $view, SiteRepresentation $site,
         SitePageRepresentation $page = null, SitePageBlockRepresentation $block = null
     ) {
+        $resourceType = new Radio("o:block[__blockIndex__][o:data][resource_type]");
+        $resourceType->setValueOptions([
+            'items' => $view->translate('Items'),
+            'item_sets' => $view->translate('Item sets'),
+            'media' => $view->translate('Media'),
+        ]);
         $text = new Text("o:block[__blockIndex__][o:data][query]");
         $heading = new Text("o:block[__blockIndex__][o:data][heading]");
         $limit = new Number("o:block[__blockIndex__][o:data][limit]");
@@ -25,16 +32,27 @@ class BrowsePreview extends AbstractBlockLayout
         $expandButton = '<a href="#" class="expand" aria-label="' . $view->translate('Expand'). '" title="' . $view->translate('Expand') . '"></a>';
 
         if ($block) {
+            $resourceType->setAttribute('value', $block->dataValue('resource_type'));
             $text->setAttribute('value', $block->dataValue('query'));
             $heading->setAttribute('value', $block->dataValue('heading'));
             $limit->setAttribute('value', $block->dataValue('limit'));
             $linkText->setAttribute('value', $block->dataValue('link-text'));
         } else {
+            $resourceType->setAttribute('value', 'items');
             $linkText->setAttribute('value', $view->translate('Browse all'));
             $limit->setAttribute('value', 12);
         }
 
-        $html = '<div class="field"><div class="field-meta">';
+        $html = '';
+
+        $html .= '<div class="field"><div class="field-meta">';
+        $html .= '<label>' . $view->translate('Resource type') . '</label>';
+        $html .= '<a href="#" class="expand"></a>';
+        $html .= '</div>';
+        $html .= '<div class="inputs">' . $view->formRow($resourceType) . '</div>';
+        $html .= '</div>';
+
+        $html .= '<div class="field"><div class="field-meta">';
         $html .= '<label>' . $view->translate('Query') . '</label>';
         $html .= $expandButton;
         $html .= '<div class="collapsible"><div class="field-description">' . $view->translate('Display resources using this search query') . '</div></div>';
@@ -71,6 +89,8 @@ class BrowsePreview extends AbstractBlockLayout
 
     public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
     {
+        $resourceType = $block->dataValue('resource_type');
+
         parse_str($block->dataValue('query'), $query);
         $originalQuery = $query;
 
@@ -89,11 +109,18 @@ class BrowsePreview extends AbstractBlockLayout
             $query['sort_order'] = 'desc';
         }
 
-        $response = $view->api()->search('items', $query);
-        $items = $response->getContent();
+        $response = $view->api()->search($resourceType, $query);
+        $resources = $response->getContent();
+
+        $resourceTypes = [
+            'items' => 'item',
+            'item_sets' => 'item-set',
+            'media' => 'media',
+        ];
 
         return $view->partial('common/block-layout/browse-preview', [
-            'items' => $items,
+            'resourceType' => $resourceTypes[$resourceType],
+            'resources' => $resources,
             'heading' => $block->dataValue('heading'),
             'linkText' => $block->dataValue('link-text'),
             'query' => $originalQuery,

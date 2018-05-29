507367f9bde1ddd019189eecc54cc2a5ba8a8bd4
Jim Safley <jimsafley@gmail.com>
2018-05-29 13:21:52 -0400

    Account for null options when constructing reCAPTCHA

--- application/src/Service/Form/Element/RecaptchaFactory.php
+++ application/src/Service/Form/Element/RecaptchaFactory.php
@@ -9,6 +9,7 @@ class RecaptchaFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
+        $options = is_array($options) ? $options : [];
         $element = new Recaptcha(null, $options);
         $element->setClient($services->get('Omeka\HttpClient'));
         return $element;

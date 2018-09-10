7d4f6353224647eb42909e4ab9affc8a0c501bba
Jim Safley <jimsafley@gmail.com>
2018-09-10 13:47:21 -0400

    Use correct empty option

--- application/src/View/Helper/ResourceTemplateSelect.php
+++ application/src/View/Helper/ResourceTemplateSelect.php
@@ -36,7 +36,7 @@ class ResourceTemplateSelect extends AbstractHelper
     {
         $spec['type'] = Select::class;
         if (!isset($spec['options']['empty_option'])) {
-            $spec['options']['empty_option'] = 'Select class…'; // @translate
+            $spec['options']['empty_option'] = 'Select template…'; // @translate
         }
         $factory = new Factory($this->formElementManager);
         $element = $factory->createElement($spec);

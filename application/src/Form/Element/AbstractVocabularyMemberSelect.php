9feafa4cb40fe7d2e2f246e6560fe2d4a3fb3e1e
Jim Safley <jimsafley@gmail.com>
2018-09-14 16:59:41 -0400

    Move property-specific code to subclass

--- application/src/Form/Element/AbstractVocabularyMemberSelect.php
+++ application/src/Form/Element/AbstractVocabularyMemberSelect.php
@@ -39,13 +39,16 @@ abstract class AbstractVocabularyMemberSelect extends Select implements EventMan
      */
     abstract public function getResourceName();
 
+    /**
+     * Get default value options for this vocabulary member.
+     *
+     * @return array
+     */
     public function getValueOptions()
     {
         $events = $this->getEventManager();
-
         $resourceName = $this->getResourceName();
 
-        $termAsValue = $this->getOption('term_as_value');
         $query = $this->getOption('query');
         if (!is_array($query)) {
             $query = [];
@@ -53,89 +56,50 @@ abstract class AbstractVocabularyMemberSelect extends Select implements EventMan
         if (!isset($query['sort_by'])) {
             $query['sort_by'] = 'label';
         }
-        $applyTemplates = $this->getOption('apply_templates');
-        $applyTemplates = is_array($applyTemplates) ? $applyTemplates : false;
+        // Allow handlers to filter the query.
+        $args = $events->prepareArgs(['query' => $query]);
+        $events->trigger('form.vocab_member_select.query', $this, $args);
+        $query = $args['query'];
 
-        if ('properties' === $resourceName && $applyTemplates) {
-            // Apply resource templates to property select by only including
-            // properties from the passed templates.
-            $valueOptions = [];
-            foreach ($applyTemplates as $templateId) {
-                try {
-                    $template = $this->getApiManager()->read('resource_templates', $templateId)->getContent();
-                } catch (NotFoundException $e) {
-                    continue;
-                }
-                foreach ($template->resourceTemplateProperties() as $templateProperty) {
-                    $property = $templateProperty->property();
-                    if (!isset($valueOptions[$property->id()])) {
-                        $valueOptions[$property->id()] = [
-                            'label' => $property->label(),
-                            'value' => $termAsValue ? $property->term() : $property->id(),
-                            'alternate_labels' => [],
-                        ];
-                    }
-                    $valueOptions[$property->id()]['alternate_labels'][] = $templateProperty->alternateLabel();
-                }
+        $valueOptions = [];
+        $response = $this->getApiManager()->search($resourceName, $query);
+        $termAsValue = $this->getOption('term_as_value');
+        foreach ($response->getContent() as $member) {
+            $attributes = ['data-term' => $member->term()];
+            if ('properties' === $resourceName) {
+                $attributes['data-property-id'] = $member->id();
+            } elseif ('resource_classes' === $resourceName) {
+                $attributes['data-resource-class-id'] = $member->id();
             }
-            // Include alternate labels, if any.
-            foreach ($valueOptions as $propertyId => $option) {
-                $altLabels = array_unique(array_filter($valueOptions[$propertyId]['alternate_labels']));
-                if ($altLabels) {
-                    $valueOptions[$propertyId]['label'] = sprintf(
-                        '%s: %s',
-                        $valueOptions[$propertyId]['label'],
-                        implode('; ', $altLabels)
-                    );
-                }
-            }
-            // Sort options alphabetically.
-            usort($valueOptions, function ($a, $b) {
-                return strcasecmp($a['label'], $b['label']);
-            });
-        } else {
-            // Default vocabulary member value options.
-            $args = $events->prepareArgs(['query' => $query]);
-            $events->trigger('form.vocab_member_select.query', $this, $args);
-            $query = $args['query'];
-
-            $valueOptions = [];
-            $response = $this->getApiManager()->search($resourceName, $query);
-            foreach ($response->getContent() as $member) {
-                $attributes = ['data-term' => $member->term()];
-                if ('properties' === $resourceName) {
-                    $attributes['data-property-id'] = $member->id();
-                } elseif ('resource_classes' === $resourceName) {
-                    $attributes['data-resource-class-id'] = $member->id();
-                }
-                $option = [
-                    'label' => $member->label(),
-                    'value' => $termAsValue ? $member->term() : $member->id(),
-                    'attributes' => $attributes,
+            $option = [
+                'label' => $member->label(),
+                'value' => $termAsValue ? $member->term() : $member->id(),
+                'attributes' => $attributes,
+            ];
+            $vocabulary = $member->vocabulary();
+            if (!isset($valueOptions[$vocabulary->prefix()])) {
+                $valueOptions[$vocabulary->prefix()] = [
+                    'label' => $vocabulary->label(),
+                    'options' => [],
                 ];
-                $vocabulary = $member->vocabulary();
-                if (!isset($valueOptions[$vocabulary->prefix()])) {
-                    $valueOptions[$vocabulary->prefix()] = [
-                        'label' => $vocabulary->label(),
-                        'options' => [],
-                    ];
-                }
-                $valueOptions[$vocabulary->prefix()]['options'][] = $option;
             }
-            // Move Dublin Core vocabularies (dcterms & dctype) to the beginning.
-            if (isset($valueOptions['dcterms'])) {
-                $valueOptions = ['dcterms' => $valueOptions['dcterms']] + $valueOptions;
-            }
-            if (isset($valueOptions['dctype'])) {
-                $valueOptions = ['dctype' => $valueOptions['dctype']] + $valueOptions;
-            }
+            $valueOptions[$vocabulary->prefix()]['options'][] = $option;
+        }
+        // Move Dublin Core vocabularies (dcterms & dctype) to the beginning.
+        if (isset($valueOptions['dcterms'])) {
+            $valueOptions = ['dcterms' => $valueOptions['dcterms']] + $valueOptions;
+        }
+        if (isset($valueOptions['dctype'])) {
+            $valueOptions = ['dctype' => $valueOptions['dctype']] + $valueOptions;
         }
 
+        // Prepend configured value options.
         $prependValueOptions = $this->getOption('prepend_value_options');
         if (is_array($prependValueOptions)) {
             $valueOptions = $prependValueOptions + $valueOptions;
         }
 
+        // Allow handlers to filter the value options.
         $args = $events->prepareArgs(['valueOptions' => $valueOptions]);
         $events->trigger('form.vocab_member_select.value_options', $this, $args);
         $valueOptions = $args['valueOptions'];

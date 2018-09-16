2da77b22f42c4c2944abeaeb25e0b314693d7725
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-09-17 00:00:00 +0200

    Added pagination to the list of sites.

--- application/src/Site/BlockLayout/ListOfSites.php
+++ application/src/Site/BlockLayout/ListOfSites.php
@@ -12,6 +12,7 @@ class ListOfSites extends AbstractBlockLayout
 {
     protected $defaults = [
         'limit' => null,
+        'pagination' => false,
     ];
 
     public function getLabel()
@@ -37,9 +38,20 @@ class ListOfSites extends AbstractBlockLayout
                 'placeholder' => '100', // @translate
             ],
         ]);
+        $form->add([
+            'name' => 'o:block[__blockIndex__][o:data][pagination]',
+            'type' => Element\Checkbox::class,
+            'options' => [
+                'label' => 'Add pagination in case of a limit', // @translate
+            ],
+            'attributes' => [
+                'id' => 'list-of-sites-pagination',
+            ],
+        ]);
 
         $form->setData([
             'o:block[__blockIndex__][o:data][limit]' => $data['limit'],
+            'o:block[__blockIndex__][o:data][pagination]' => $data['pagination'],
         ]);
 
         return $view->formCollection($form);
@@ -48,17 +60,29 @@ class ListOfSites extends AbstractBlockLayout
     public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
     {
         $limit = $block->dataValue('limit', $this->defaults['limit']);
+        $pagination = $limit && $block->dataValue('pagination', $this->defaults['pagination']);
 
         $data = [];
-        if ($limit) {
+        if ($pagination) {
+            $currentPage = $view->params()->fromQuery('page', 1);
+            $data['page'] = $currentPage;
+            $data['per_page'] = $limit;
+        } elseif ($limit) {
             $data['limit'] = $limit;
         }
 
         $response = $view->api()->search('sites', $data);
+
+        if ($pagination) {
+            $totalCount = $response->getTotalResults();
+            $view->pagination(null, $totalCount, $currentPage, $limit);
+        }
+
         $sites = $response->getContent();
 
         return $view->partial('common/block-layout/list-of-sites', [
             'sites' => $sites,
+            'pagination' => $pagination,
         ]);
     }
 }

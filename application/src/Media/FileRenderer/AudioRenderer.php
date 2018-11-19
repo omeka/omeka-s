420c473f1b02b2015bc41d31622ae282ee359e68
John Flatness <john@zerocrates.org>
2018-11-19 17:55:16 -0500

    Add options for HTML5 media attrs
    (fix #1324)

--- application/src/Media/FileRenderer/AudioRenderer.php
+++ application/src/Media/FileRenderer/AudioRenderer.php
@@ -6,12 +6,33 @@ use Zend\View\Renderer\PhpRenderer;
 
 class AudioRenderer implements RendererInterface
 {
+    const DEFAULT_OPTIONS = [
+        'controls' => true,
+    ];
+
     public function render(PhpRenderer $view, MediaRepresentation $media,
         array $options = []
     ) {
+        $options = array_merge(self::DEFAULT_OPTIONS, $options);
+
+        $attrs[] = sprintf('src="%s"', $view->escapeHtml($media->originalUrl()));
+
+        if (isset($options['autoplay']) && $options['autoplay']) {
+            $attrs[] = 'autoplay';
+        }
+        if (isset($options['controls']) && $options['controls']) {
+            $attrs[] = 'controls';
+        }
+        if (isset($options['loop']) && $options['loop']) {
+            $attrs[] = 'loop';
+        }
+        if (isset($options['muted']) && $options['muted']) {
+            $attrs[] = 'muted';
+        }
+
         return sprintf(
-            '<audio src="%s" controls>%s</audio>',
-            $view->escapeHtml($media->originalUrl()),
+            '<audio %s>%s</audio>',
+            implode(' ', $attrs),
             $view->hyperlink($media->filename(), $media->originalUrl())
         );
     }
--- application/src/Media/FileRenderer/VideoRenderer.php
+++ application/src/Media/FileRenderer/VideoRenderer.php
@@ -6,12 +6,42 @@ use Zend\View\Renderer\PhpRenderer;
 
 class VideoRenderer implements RendererInterface
 {
+    const DEFAULT_OPTIONS = [
+        'controls' => true,
+    ];
+
     public function render(PhpRenderer $view, MediaRepresentation $media,
         array $options = []
     ) {
+        $options = array_merge(self::DEFAULT_OPTIONS, $options);
+
+        $attrs[] = sprintf('src="%s"', $view->escapeHtml($media->originalUrl()));
+
+        if (isset($options['width'])) {
+            $attrs[] = sprintf('width="%s"', $view->escapeHtml($options['width']));
+        }
+        if (isset($options['height'])) {
+            $attrs[] = sprintf('height="%s"', $view->escapeHtml($options['height']));
+        }
+        if (isset($options['poster'])) {
+            $attrs[] = sprintf('poster="%s"', $view->escapeHtml($options['poster']));
+        }
+        if (isset($options['autoplay']) && $options['autoplay']) {
+            $attrs[] = 'autoplay';
+        }
+        if (isset($options['controls']) && $options['controls']) {
+            $attrs[] = 'controls';
+        }
+        if (isset($options['loop']) && $options['loop']) {
+            $attrs[] = 'loop';
+        }
+        if (isset($options['muted']) && $options['muted']) {
+            $attrs[] = 'muted';
+        }
+
         return sprintf(
-            '<video src="%s" controls>%s</video>',
-            $view->escapeHtml($media->originalUrl()),
+            '<video %s>%s</video>',
+            implode(' ', $attrs),
             $view->hyperlink($media->filename(), $media->originalUrl())
         );
     }

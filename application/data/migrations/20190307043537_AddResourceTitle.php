d74add684aece5978bbc028c3edf2172fe8fff92
Jim Safley <jimsafley@gmail.com>
2019-03-09 10:40:47 -0500

    Migrate resource title to longtext; do not truncate resource title

--- application/data/migrations/20190307043537_AddResourceTitle.php
+++ application/data/migrations/20190307043537_AddResourceTitle.php
@@ -9,8 +9,7 @@ class AddResourceTitle implements MigrationInterface
     public function up(Connection $conn)
     {
         $conn->exec('
-        ALTER TABLE resource ADD title VARCHAR(190) DEFAULT NULL;
-        CREATE INDEX title ON resource (title);
+        ALTER TABLE resource ADD title LONGTEXT DEFAULT NULL;
         ALTER TABLE resource_template ADD title_property_id INT DEFAULT NULL;
         ALTER TABLE resource_template ADD CONSTRAINT FK_39ECD52E724734A3 FOREIGN KEY (title_property_id) REFERENCES property (id) ON DELETE SET NULL;
         CREATE INDEX IDX_39ECD52E724734A3 ON resource_template (title_property_id);');

dc52e6f0d1bd7eb5a2a03590ea5b183cba8eae33
John Flatness <john@zerocrates.org>
2019-04-12 18:05:56 -0400

    Add ON DELETE rule to site->owner FK
    (fix #1398)

--- application/Module.php
+++ application/Module.php
@@ -13,7 +13,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.1-alpha';
 
     /**
      * The vocabulary IRI used to define Omeka application data.

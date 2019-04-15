2689ce92fd0c5037032d56a66684dd21f7381808
John Flatness <john@zerocrates.org>
2019-04-15 16:14:43 -0400

    Convert migration to single SQL statements
    (#1399)

--- application/data/migrations/20190319020708_AddSiteHomepage.php
+++ application/data/migrations/20190319020708_AddSiteHomepage.php
@@ -8,9 +8,8 @@ class AddSiteHomepage implements MigrationInterface
 {
     public function up(Connection $conn)
     {
-        $conn->exec('
-ALTER TABLE site ADD homepage_id INT DEFAULT NULL;
-ALTER TABLE site ADD CONSTRAINT FK_694309E4571EDDA FOREIGN KEY (homepage_id) REFERENCES site_page (id) ON DELETE SET NULL;
-CREATE UNIQUE INDEX UNIQ_694309E4571EDDA ON site (homepage_id);');
+        $conn->exec('ALTER TABLE site ADD homepage_id INT DEFAULT NULL');
+        $conn->exec('ALTER TABLE site ADD CONSTRAINT FK_694309E4571EDDA FOREIGN KEY (homepage_id) REFERENCES site_page (id) ON DELETE SET NULL');
+        $conn->exec('CREATE UNIQUE INDEX UNIQ_694309E4571EDDA ON site (homepage_id);');
     }
 }

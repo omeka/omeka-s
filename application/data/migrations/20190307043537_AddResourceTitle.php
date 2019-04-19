7724ea0d6a854a59bea2a51fb5233776fe703e5c
Jim Safley <jimsafley@gmail.com>
2019-04-19 13:30:59 -0400

    Remove multiline execs

--- application/data/migrations/20190307043537_AddResourceTitle.php
+++ application/data/migrations/20190307043537_AddResourceTitle.php
@@ -8,10 +8,9 @@ class AddResourceTitle implements MigrationInterface
 {
     public function up(Connection $conn)
     {
-        $conn->exec('
-        ALTER TABLE resource ADD title LONGTEXT DEFAULT NULL;
-        ALTER TABLE resource_template ADD title_property_id INT DEFAULT NULL;
-        ALTER TABLE resource_template ADD CONSTRAINT FK_39ECD52E724734A3 FOREIGN KEY (title_property_id) REFERENCES property (id) ON DELETE SET NULL;
-        CREATE INDEX IDX_39ECD52E724734A3 ON resource_template (title_property_id);');
+        $conn->exec('ALTER TABLE resource ADD title LONGTEXT DEFAULT NULL;');
+        $conn->exec('ALTER TABLE resource_template ADD title_property_id INT DEFAULT NULL;');
+        $conn->exec('ALTER TABLE resource_template ADD CONSTRAINT FK_39ECD52E724734A3 FOREIGN KEY (title_property_id) REFERENCES property (id) ON DELETE SET NULL;');
+        $conn->exec('CREATE INDEX IDX_39ECD52E724734A3 ON resource_template (title_property_id);');
     }
 }

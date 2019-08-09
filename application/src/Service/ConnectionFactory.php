8bfe451b8c8c61c9f1e9184005d20c96be3a0fa7
John Flatness <john@zerocrates.org>
2019-08-09 17:23:30 -0400

    Force MySqlPlatform for doctrine
    Avoids usage of 5.7/8/Maria-high-version-exclusive features when
    creating db schema file
    
    (fix #1440)

--- application/src/Service/ConnectionFactory.php
+++ application/src/Service/ConnectionFactory.php
@@ -3,6 +3,7 @@ namespace Omeka\Service;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Omeka\Db\Logging\FileSqlLogger;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use Interop\Container\ContainerInterface;
@@ -29,9 +30,17 @@ class ConnectionFactory implements FactoryInterface
             throw new Exception\ConfigException('Missing database connection configuration');
         }
 
+        // Force the "generic" MySQL platform to avoid autodetecting and using exclusive features
+        // of newer versions
+        $platform = new MySqlPlatform;
+
         $config['connection']['driver'] = self::DRIVER;
         $config['connection']['charset'] = self::CHARSET;
+        $config['connection']['platform'] = $platform;
         $connection = DriverManager::getConnection($config['connection']);
+
+        // Manually-set platforms must have the event manager manually injected
+        $platform->setEventManager($connection->getEventManager());
 
         if (isset($config['connection']['log_path'])
             && is_file($config['connection']['log_path'])

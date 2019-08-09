303299e3f8724530a3d1730a887993f637b9f3af
John Flatness <john@zerocrates.org>
2019-08-09 16:44:42 -0400

    Use "lengths" in index annotations for value table
    Avoids the need for the CreateTableOverride class that manually
    specified the SQL for the values table

--- application/src/Service/ConnectionFactory.php
+++ application/src/Service/ConnectionFactory.php
@@ -4,7 +4,6 @@ namespace Omeka\Service;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Events;
-use Omeka\Db\Event\Listener\CreateTableOverride;
 use Omeka\Db\Logging\FileSqlLogger;
 use Zend\ServiceManager\Factory\FactoryInterface;
 use Interop\Container\ContainerInterface;
@@ -42,9 +41,6 @@ class ConnectionFactory implements FactoryInterface
             $connection->getConfiguration()
                 ->setSQLLogger(new FileSqlLogger($config['connection']['log_path']));
         }
-
-        $eventManager = $connection->getEventManager();
-        $eventManager->addEventListener(Events::onSchemaCreateTable, new CreateTableOverride);
 
         return $connection;
     }

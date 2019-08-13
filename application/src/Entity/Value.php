3fdf5eedc42ead4551a8843954e0bd89c7d1580d
julia-bauder <julia.bauder@gmail.com>
2019-08-13 13:29:33 -0500

    Escaped reserved word "value" (#1437)
    "Value" is a reserved word in some versions of MySQL/MariaDB. If you're using one of those versions, queries that access or modify the "value" table currently fail. Escaping "value" as a table and column name fixes the error for people running one of those versions, and should not affect usage by people not running one of those versions.

--- application/src/Entity/Value.php
+++ application/src/Entity/Value.php
@@ -5,7 +5,7 @@ namespace Omeka\Entity;
  * A value, representing the object in a RDF triple.
  *
  * @Entity
- * @Table(indexes={
+ * @Table(name="`value`", indexes={
  *     @Index(name="`value`", columns={"`value`"}, options={"lengths":{190}}),
  *     @Index(name="`uri`", columns={"`uri`"}, options={"lengths":{190}})
  * })
@@ -42,7 +42,7 @@ class Value extends AbstractEntity
     protected $lang;
 
     /**
-     * @Column(type="text", nullable=true)
+     * @Column(name="`value`", type="text", nullable=true)
      */
     protected $value;
 

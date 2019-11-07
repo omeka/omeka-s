606111ad3984bff12cdc7740a0d0d56dcca95381
Kim Nguyen <design@kimisgold.com>
2019-11-07 16:13:01 -0500

    Revise item set management workflow. (#1462)
    * Keep owner heading in selector open after
      clicking item set.
    * Clean up styles for marking item set for 
      removal.
    * Hide item set in selector if it exists in the
      item set table.
    * Update item set totals in selector after
      adding item sets.

--- application/asset/js/site-item-sets.js
+++ application/asset/js/site-item-sets.js
@@ -6,9 +6,16 @@ new Sortable(document.getElementById('site-item-set-rows'), {
 });
 
 var itemSets = $('#site-item-sets');
-var noItemSets = $('#no-site-item-sets');
 var itemSetsData = itemSets.data('itemSets');
 var rowTemplate = $($.parseHTML(itemSets.data('rowTemplate')));
+var totalCount = $('.selector-total-count');
+
+var parentToggle = function(e) {
+    e.stopPropagation();
+    if ($(this).children('li')) {
+        $(this).toggleClass('show');
+    }
+}
 
 var appendItemSet = function(id, title, email) {
     if (itemSets.find(".site-item-set-id[value='" + id + "']").length) {
@@ -21,46 +28,51 @@ var appendItemSet = function(id, title, email) {
     $('#site-item-set-rows').append(row);
 }
 
+var updateItemSetCount = function(itemSetId) {
+    var itemSet = $('[data-item-set-id="' + itemSetId + '"]');
+    var itemSetParent = itemSet.parents('.selector-parent');
+    var childCount = itemSetParent.find('.selector-child-count').first();
+    var newTotalCount = parseInt(totalCount.text()) - 1;
+    var newChildCount = parseInt(childCount.text()) - 1;
+    totalCount.text(newTotalCount);
+    childCount.text(newChildCount);
+}
+
 if (itemSetsData.length) {
     $.each(itemSetsData, function() {
         appendItemSet(this.id, this.title, this.email);
+        $('[data-item-set-id="' + this.id + '"]').hide();
+        updateItemSetCount(this.id);
     });
-    itemSets.show();
-} else {
-    noItemSets.show();
+    $('#item-sets-section').addClass('has-item-sets');
 }
 
-$('#item-set-selector .selector-child').on('click', function() {
+$('#item-set-selector .selector-child').on('click', function(e) {
+    e.stopPropagation();
     var itemSet = $(this);
+    var itemSetParent = itemSet.parents('.selector-parent');
+    itemSetParent.unbind('click');
     appendItemSet(
         itemSet.data('itemSetId'),
         itemSet.data('childSearch'),
         itemSet.data('ownerEmail')
     );
-    itemSets.show();
-    noItemSets.hide()
+    itemSet.hide();
+    updateItemSetCount(itemSet.data('itemSetId'));
+    itemSetParent.bind('click', parentToggle);
+    Omeka.scrollTo($('.site-item-set-row:last-child'));
 });
 
-itemSets.on('click', '.o-icon-delete', function(e) {
+itemSets.on('click', '.o-icon-delete, .o-icon-undo', function(e) {
     e.preventDefault();
-    var removeLink = $(this);
-    var undoLink = removeLink.siblings('.o-icon-undo');
-    var row = removeLink.closest('.site-item-set-row');
-    row.find('input').prop('disabled', true);
+    var row = $(this).closest('.site-item-set-row');
+    var hiddenInput = row.find('input');
+    if (hiddenInput.prop('disabled') !== true) {
+        hiddenInput.prop('disabled', true);
+    } else {
+        hiddenInput.prop('disabled', false);
+    }
     row.toggleClass('delete');
-    removeLink.hide();
-    undoLink.show();
-});
-
-itemSets.on('click', '.o-icon-undo', function(e) {
-    e.preventDefault();
-    var undoLink = $(this);
-    var removeLink = undoLink.siblings('.o-icon-delete');
-    var row = undoLink.closest('.site-item-set-row');
-    row.find('input').prop('disabled', false);
-    row.toggleClass('delete');
-    undoLink.hide();
-    removeLink.show();
 });
 
 });

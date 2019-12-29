dcf19447eecf1f36fc3ea14493660e327a176527
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-12-30 00:00:00 +0100

    Added option to specify the label information per site.

--- application/src/Form/SiteSettingsForm.php
+++ application/src/Form/SiteSettingsForm.php
@@ -83,6 +83,23 @@ class SiteSettingsForm extends Form
             ],
         ]);
         $generalFieldset->add([
+            'name' => 'property_label_information',
+            'type' => 'Select',
+            'options' => [
+                'label' => 'Property label information', // @translate
+                'info' => 'The additional information that accompanies labels on resource pages.', // @translate
+                'value_options' => [
+                    'none' => 'None', // @translate
+                    'vocab' => 'Show Vocabulary', // @translate
+                    'term' => 'Show Term', // @translate
+                ],
+            ],
+            'attributes' => [
+                'id' => 'property_label_information',
+                'value' => $settings->get('property_label_information', 'none'),
+            ],
+        ]);
+        $generalFieldset->add([
             'name' => 'show_user_bar',
             'type' => 'radio',
             'options' => [

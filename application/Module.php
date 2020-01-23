5d7e67853613c912e27ef86039b39668896be581
John Flatness <john@zerocrates.org>
2020-01-23 15:32:18 -0500

    Bump version to 2.1.0
    (a little early, to allow for themes/modules to target this version)

--- application/Module.php
+++ application/Module.php
@@ -14,7 +14,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '2.0.2';
+    const VERSION = '2.1.0';
 
     /**
      * The vocabulary IRI used to define Omeka application data.

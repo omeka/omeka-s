6a15515c4f702602c6e8b96a6b8c994f1115d071
Jim Safley <jimsafley@gmail.com>
2020-02-24 22:59:56 -0500

    Grant view-all privileges to include private items

--- application/src/Job/UpdateSiteItems.php
+++ application/src/Job/UpdateSiteItems.php
@@ -6,26 +6,34 @@ use Omeka\Job\Exception;
 
 class UpdateSiteItems extends AbstractJob
 {
+    /**
+     * Update site-item associations for one or more site.
+     */
     public function perform()
     {
-        $siteIds = $this->getArg('site_ids', []);
-        $replace = (bool) $this->getArg('replace', false);
+        $siteIds = $this->getArg('site_ids');
+        $replace = $this->getArg('replace', false);
         if (!is_array($siteIds)) {
             throw new Exception\InvalidArgumentException('No site_ids array passed to the UpdateSiteItems job');
         }
+
+        // Grant "view-all" privileges to include private items. We need this
+        // for situations when the Job has no owner, like during a migration.
+        $this->getServiceLocator()->get('Omeka\Acl')->allow('view-all');
+
         foreach ($siteIds as $siteId) {
             $this->updateSiteItems($siteId, $replace);
         }
     }
 
     /**
-     * Update site-item associations.
+     * Update site-item associations for one site.
      *
-     * There are two strategies for updating site/items: "add" and "replace".
-     * The "add" strategy is default because it is non-destructive---it simply
-     * adds associations that don't already exist in the item pool. The
-     * "replace" strategy deletes all existing associations and syncs the
-     * associations with the item pool. To replace, set $replace to true.
+     * There are two strategies for updating site/items: add and replace. The
+     * "add" strategy simply adds associations that don't already exist in the
+     * item pool (it is default because it is non-destructive). The "replace"
+     * strategy deletes all existing associations and syncs the associations
+     * with the configured item pool. To replace, set $replace to true.
      *
      * @param int $siteId
      * @param bool $replace
@@ -38,7 +46,7 @@ class UpdateSiteItems extends AbstractJob
 
         $itemPool = $conn->fetchColumn('SELECT item_pool FROM site WHERE id = ?', [$siteId], 0);
         if (false === $itemPool) {
-            throw new Exception\InvalidArgumentException('Invalid site ID');
+            throw new Exception\InvalidArgumentException(sprintf('Invalid site ID "%s" passed to the UpdateSiteItems job', $siteId));
         }
         $itemIds = $api->search('items', json_decode($itemPool, true), ['returnScalar' => 'id'])->getContent();
 
@@ -46,7 +54,7 @@ class UpdateSiteItems extends AbstractJob
             $conn->delete('item_site', ['site_id' => $siteId]);
         }
 
-        // Chunk to avoid query size limits.
+        // Chunk to avoid query/buffer/packet size limits.
         foreach (array_chunk($itemIds, 1000) as $itemIdsChunk) {
             $values = [];
             $bindValues = [];
@@ -55,10 +63,7 @@ class UpdateSiteItems extends AbstractJob
                 $bindValues[] = $itemId;
                 $bindValues[] = $siteId;
             }
-            $sql = sprintf(
-                'INSERT IGNORE INTO item_site (item_id, site_id) VALUES %s',
-                implode(',', $values)
-            );
+            $sql = sprintf('INSERT IGNORE INTO item_site (item_id, site_id) VALUES %s', implode(',', $values));
             $stmt = $conn->prepare($sql);
             foreach ($bindValues as $position => $value) {
                 $stmt->bindValue($position + 1, $value);

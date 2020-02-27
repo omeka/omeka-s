63b18d243efc4de4ac63df744b0d81a464c17b1b
Jim Safley <jimsafley@gmail.com>
2020-02-26 23:36:04 -0500

    Clarify item assignment actions

--- application/src/Form/SiteResourcesForm.php
+++ application/src/Form/SiteResourcesForm.php
@@ -25,9 +25,9 @@ class SiteResourcesForm extends Form
                 'label' => 'Assignment action',
                 'empty_option' => '[No action]', // @translate
                 'value_options' => [
-                    'add' => 'Add items from the following search', // @translate
-                    'replace' => 'Replace items with the following search', // @translate
-                    'remove_all' => 'Remove all items', // @translate
+                    'add' => 'Add: keep existing items and assign items from the following search', // @translate
+                    'replace' => 'Replace: unassign all items and assign items from the following search', // @translate
+                    'remove_all' => 'Remove all: unassign all items', // @translate
                 ],
             ],
         ]);

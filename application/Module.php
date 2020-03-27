06bce29ff72bb8bd0a071ed994a870ec566ecf43
John Flatness <john@zerocrates.org>
2020-03-27 12:00:28 -0400

    Set version 2.1.1
    (cherry picked from commit 11357cd9f71efe44001210f05e1e680f71532f55)

--- application/Module.php
+++ application/Module.php
@@ -14,7 +14,7 @@ class Module extends AbstractModule
     /**
      * This Omeka version.
      */
-    const VERSION = '2.1.0';
+    const VERSION = '2.1.1';
 
     /**
      * The vocabulary IRI used to define Omeka application data.

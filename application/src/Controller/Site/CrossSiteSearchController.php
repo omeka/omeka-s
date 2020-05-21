09c404bec301e6239b8c741d15560b435e2c350a
Jim Safley <jimsafley@gmail.com>
2020-05-21 18:57:44 -0400

    Add item sets to cross-site search

--- application/src/Controller/Site/CrossSiteSearchController.php
+++ application/src/Controller/Site/CrossSiteSearchController.php
@@ -13,7 +13,14 @@ class CrossSiteSearchController extends AbstractActionController
         return $view;
     }
 
-    public function advancedAction()
+    public function itemsAdvancedAction()
+    {
+        $view = new ViewModel;
+        $view->setVariable('site', $this->currentSite());
+        return $view;
+    }
+
+    public function itemSetsAdvancedAction()
     {
         $view = new ViewModel;
         $view->setVariable('site', $this->currentSite());
@@ -23,20 +30,12 @@ class CrossSiteSearchController extends AbstractActionController
     public function resultsAction()
     {
         $fulltextSearch = $this->params()->fromQuery('fulltext_search');
-        $resourceNames = $this->siteSettings()->get('search_resource_names', ['site_pages', 'items']);
-        if (1 === count($resourceNames)) {
-            return $this->redirect()->toRoute(
-                null,
-                ['action' => ('site_pages' === reset($resourceNames)) ? 'site-pages' : 'items'],
-                ['query' => ['fulltext_search' => $fulltextSearch]],
-                true
-            );
-        }
         $query = ['fulltext_search' => $fulltextSearch, 'limit' => 10];
         $view = new ViewModel;
         $view->setVariable('site', $this->currentSite());
         $view->setVariable('responseSitePages', $this->api()->search('site_pages', $query));
         $view->setVariable('responseItems', $this->api()->search('items', array_merge($query, ['in_sites' => true])));
+        $view->setVariable('responseItemSets', $this->api()->search('item_sets', array_merge($query, ['in_sites' => true])));
         return $view;
     }
 
@@ -60,6 +59,18 @@ class CrossSiteSearchController extends AbstractActionController
         $view = new ViewModel;
         $view->setVariable('site', $this->currentSite());
         $view->setVariable('items', $response->getContent());
+        return $view;
+    }
+
+    public function itemSetsAction()
+    {
+        $this->setBrowseDefaults('created');
+        $query = array_merge($this->params()->fromQuery(), ['in_sites' => true]);
+        $response = $this->api()->search('item_sets', $query);
+        $this->paginator($response->getTotalResults());
+        $view = new ViewModel;
+        $view->setVariable('site', $this->currentSite());
+        $view->setVariable('itemSets', $response->getContent());
         return $view;
     }
 }

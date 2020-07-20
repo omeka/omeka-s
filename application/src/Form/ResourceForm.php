fdc22e0c84b3af15700970ef6f52624b2c45e241
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-07-20 00:00:00 +0000

    Sorted resource template by label in resource form.

--- application/src/Form/ResourceForm.php
+++ application/src/Form/ResourceForm.php
@@ -36,7 +36,9 @@ class ResourceForm extends Form
                 'empty_option' => '',
                 'resource_value_options' => [
                     'resource' => 'resource_templates',
-                    'query' => [],
+                    'query' => [
+                        'sort_by' => 'label',
+                    ],
                     'option_text_callback' => function ($resourceTemplate) {
                         return $resourceTemplate->label();
                     },

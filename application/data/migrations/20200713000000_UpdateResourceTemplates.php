b4bb25749efe683a386fd23031f2dcf92c0f3f88
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-08-17 00:00:00 +0000

    Added backtick to sql queries.

--- application/data/migrations/20200713000000_UpdateResourceTemplates.php
+++ application/data/migrations/20200713000000_UpdateResourceTemplates.php
@@ -9,7 +9,7 @@ class UpdateResourceTemplates implements MigrationInterface
     public function up(Connection $conn)
     {
         $sql = <<<SQL
-ALTER TABLE resource_template_property CHANGE data_type data_type VARCHAR(766) DEFAULT NULL;
+ALTER TABLE `resource_template_property` CHANGE `data_type` `data_type` VARCHAR(766) DEFAULT NULL;
 SQL;
         $conn->exec($sql);
 
@@ -22,23 +22,24 @@ SQL;
         ];
         foreach ($indexes as $index) {
             $sql = <<<SQL
-SHOW INDEX FROM resource_template_property WHERE Key_name = '$index';
+SHOW INDEX FROM `resource_template_property` WHERE Key_name = '$index';
 SQL;
             if ($conn->fetchAll($sql)) {
                 $sql = <<<SQL
-ALTER TABLE resource_template_property DROP INDEX $index;
+ALTER TABLE `resource_template_property` DROP INDEX $index;
 SQL;
                 $conn->exec($sql);
             }
         }
 
         $sql = <<<SQL
-CREATE INDEX IDX_4689E2F116131EA549213EC ON resource_template_property (resource_template_id, property_id);
+CREATE INDEX IDX_4689E2F116131EA549213EC ON `resource_template_property` (`resource_template_id`, `property_id`);
 SQL;
         $conn->exec($sql);
 
         $sql = <<<SQL
-INSERT INTO setting (id, value) VALUES ('resource_default_datatypes', '["literal","resource","uri"]') ON DUPLICATE KEY UPDATE value=value;
+INSERT INTO `setting` (`id`, `value`)
+VALUES ('resource_default_datatypes', '["literal","resource","uri"]') ON DUPLICATE KEY UPDATE `value`=`value`;
 SQL;
         $conn->exec($sql);
     }

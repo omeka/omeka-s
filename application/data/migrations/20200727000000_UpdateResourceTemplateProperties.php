2a870f57176e04447d774705e9a61a8f91c54c17
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-08-17 00:00:00 +0000

    Added backtick to sql queries.

--- application/data/migrations/20200727000000_UpdateResourceTemplateProperties.php
+++ application/data/migrations/20200727000000_UpdateResourceTemplateProperties.php
@@ -14,16 +14,16 @@ SQL;
         $conn->exec($sql);
 
         $sql = <<<SQL
-UPDATE resource_template_property
-SET data_type = NULL
-WHERE data_type IS NULL OR TRIM(data_type) = "";
+UPDATE `resource_template_property`
+SET `data_type` = NULL
+WHERE `data_type` IS NULL OR TRIM(`data_type`) = "";
 SQL;
         $conn->exec($sql);
 
         $sql = <<<SQL
-UPDATE resource_template_property
-SET data_type = CONCAT('["', REPLACE(TRIM(data_type), "\n", '","'), '"]')
-WHERE data_type IS NOT NULL AND SUBSTRING(data_type, 1, 1) != "[";
+UPDATE `resource_template_property`
+SET `data_type` = CONCAT('["', REPLACE(TRIM(`data_type`), "\n", '","'), '"]')
+WHERE `data_type` IS NOT NULL AND SUBSTRING(`data_type`, 1, 1) != "[";
 SQL;
         $conn->exec($sql);
     }

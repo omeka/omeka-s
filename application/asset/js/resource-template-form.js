eb22680a27e68694a070fbac2c0da3a5a82c7376
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-08-24 00:00:00 +0000

    Added a setting to list allowed languages in a resource template.

--- application/asset/js/resource-template-form.js
+++ application/asset/js/resource-template-form.js
@@ -81,7 +81,7 @@ propertyList.on('click', '.property-edit', function(e) {
     $('#data-type').val(dataTypes.val().split(','));
     $('#data-type').trigger('chosen:updated');
     $('#default-value').val(defaultValue.val());
-    $('#allowed-languages').val(allowedLanguages.val());
+    $('#edit-sidebar #allowed-languages').val(allowedLanguages.val());
 
     $('#set-changes').off('click.setchanges').on('click.setchanges', function(e) {
         altLabel.val($('#alternate-label').val());
@@ -108,7 +108,7 @@ propertyList.on('click', '.property-edit', function(e) {
         $('#is-private').prop('checked') ? isPrivate.val(1) : isPrivate.val(null);
         dataTypes.val($('#data-type').val());
         defaultValue.val($('#default-value').val());
-        allowedLanguages.val($('#allowed-languages').val());
+        allowedLanguages.val($('#edit-sidebar #allowed-languages').val());
         Omeka.closeSidebar($('#edit-sidebar'));
     });
 
--- application/src/Form/ResourceTemplateForm.php
+++ application/src/Form/ResourceTemplateForm.php
@@ -4,6 +4,8 @@ namespace Omeka\Form;
 use Omeka\Form\Element\ResourceClassSelect;
 use Laminas\EventManager\Event;
 use Laminas\EventManager\EventManagerAwareTrait;
+use Laminas\Form\Element;
+use Laminas\Form\Fieldset;
 use Laminas\Form\Form;
 
 class ResourceTemplateForm extends Form
@@ -53,6 +55,26 @@ class ResourceTemplateForm extends Form
             ],
         ]);
 
+        $this->add([
+            'type' => Fieldset::class,
+            'name' => 'o:settings',
+            'options' => [
+                'label' => 'Other settings', // @translate
+            ],
+        ]);
+        $settingsFieldset = $this->get('o:settings');
+        $settingsFieldset
+            ->add([
+                'name' => 'allowed_languages',
+                'type' => Element\Textarea::class,
+                'options' => [
+                    'label' => 'Allowed languages for properties', // @translate
+                ],
+                'attributes' => [
+                    'id' => 'allowed_languages',
+                ],
+            ]);
+
         $event = new Event('form.add_elements', $this);
         $this->getEventManager()->triggerEvent($event);
 
@@ -65,10 +87,42 @@ class ResourceTemplateForm extends Form
             'name' => 'o:resource_class[o:id]',
             'allow_empty' => true,
         ]);
+        $inputFilter->get('o:settings')
+            ->add([
+                'name' => 'allowed_languages',
+                'required' => false,
+                'filters' => [
+                    [
+                        'name' => \Laminas\Filter\Callback::class,
+                        'options' => [
+                            'callback' => [$this, 'stringToList'],
+                        ],
+                    ],
+                ],
+            ]);
 
         // Separate events because calling $form->getInputFilters() resets
         // everything.
         $event = new Event('form.add_input_filters', $this, ['inputFilter' => $inputFilter]);
         $this->getEventManager()->triggerEvent($event);
+    }
+
+    public function setData($data)
+    {
+        if (isset($data['o:settings']['allowed_languages']) && is_array($data['o:settings']['allowed_languages'])) {
+            $data['o:settings']['allowed_languages'] = implode("\n", $data['o:settings']['allowed_languages']);
+        }
+        return parent::setData($data);
+    }
+
+    /**
+     * Get each line of a string separately.
+     *
+     * @param string $string
+     * @return array
+     */
+    public function stringToList($string)
+    {
+        return array_filter(array_map('trim', explode("\n", str_replace(["\r\n", "\n\r", "\r"], ["\n", "\n", "\n"], $string))), 'strlen');
     }
 }

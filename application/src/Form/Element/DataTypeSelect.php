4296abd0cfc9db2edf4166723c343a66023323ec
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-09-03 00:00:00 +0000

    Fixed merge.

--- application/src/Form/Element/DataTypeSelect.php
+++ application/src/Form/Element/DataTypeSelect.php
@@ -2,7 +2,7 @@
 namespace Omeka\Form\Element;
 
 use Omeka\DataType\Manager as DataTypeManager;
-use Zend\Form\Element\Select;
+use Laminas\Form\Element\Select;
 
 class DataTypeSelect extends Select
 {

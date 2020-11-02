4821123783d1fe00dd32e0900a3e85e74e8cfbae
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-11-02 00:00:00 +0000

    Added a mode "auto" to cli to use proc_open when available.

--- application/src/Service/CliFactory.php
+++ application/src/Service/CliFactory.php
@@ -4,6 +4,7 @@ namespace Omeka\Service;
 use Omeka\Stdlib\Cli;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Interop\Container\ContainerInterface;
+use Omeka\Service\Exception\RuntimeException;
 
 class CliFactory implements FactoryInterface
 {
@@ -20,8 +21,29 @@ class CliFactory implements FactoryInterface
         $strategy = null;
         if (isset($config['cli']['execute_strategy'])) {
             $strategy = $config['cli']['execute_strategy'];
+            if ($strategy === 'auto') {
+                if ($this->functionIsEnabled('proc_open')) {
+                    $strategy = 'proc_open';
+                } elseif ($this->functionIsEnabled('exec')) {
+                    $strategy = 'exec';
+                } else {
+                    throw new RuntimeException('Neither "proc_open()" nor "exec()" are available.'); // @translate
+                }
+            }
         }
 
         return new Cli($logger, $strategy);
+    }
+
+    /**
+     * Check if a function is available.
+     *
+     * @param string $function
+     * @return bool
+     */
+    protected function functionIsEnabled($function)
+    {
+        return function_exists($function)
+            && !in_array($function, array_map('trim', explode(',', ini_get('disable_functions'))));
     }
 }

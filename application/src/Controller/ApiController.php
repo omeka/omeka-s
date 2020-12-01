5fa1da9cecac93d432f0bb287bf7555cb65897c7
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-12-01 00:00:00 +0000

    Removed the possibility to query a single id in rest api.

--- application/src/Controller/ApiController.php
+++ application/src/Controller/ApiController.php
@@ -229,18 +229,16 @@ class ApiController extends AbstractRestfulController
     /**
      * Check if the request contains an identifier.
      *
-     * This method overrides parent in order to allow to query on ids.
+     * This method overrides parent in order to allow to query on one or
+     * multiple ids.
      *
      * {@inheritDoc}
      * @see \Laminas\Mvc\Controller\AbstractRestfulController::getIdentifier()
      */
     protected function getIdentifier($routeMatch, $request)
     {
-        $id = parent::getIdentifier($routeMatch, $request);
-        // When the id is an array, it is a query with one or multiple ids.
-        return is_array($id)
-            ? false
-            : $id;
+        $identifier = $this->getIdentifierName();
+        return $routeMatch->getParam($identifier, false);
     }
 
     /**

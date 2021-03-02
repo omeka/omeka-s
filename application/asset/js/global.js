087315f83a72d481f0e491bcfff9f712973d4866
Jim Safley <jimsafley@gmail.com>
2021-03-02 13:44:50 -0500

    Move advanced search JS to template so behavior is loaded however template is loaded

--- application/asset/js/global.js
+++ application/asset/js/global.js
@@ -1,33 +1,5 @@
 $(document).ready(function() {
-        // Set up multi-value templates
-
-        $('.multi-value.field').each(function() {
-            var field = $(this);
-            var value = field.find('.value').first().clone();
-            var valueHtml = value.wrap('<div></div>').parent().html();
-            field.data('field-template', valueHtml);
-        });
-
-
-        // Add a value.
-        $('form').on('click', '.multi-value .add-value', function(e) {
-            e.preventDefault();
-            var fieldContainer = $(this).parents('.field');
-            var template = fieldContainer.data('field-template');
-            var newValue = $(template);
-            newValue.children('input[type="text"]').val(null);
-            newValue.children('select').prop('selectedIndex', 0);
-            newValue.appendTo(fieldContainer.find('.inputs'));
-            newValue.trigger('o:value-created');
-        });
-        
-        // Remove a value.
-        $('form').on('click', '.multi-value .remove-value', function(e) {
-            e.preventDefault();
-            $(this).closest('.value').remove();
-        });
-
-     // Set classes for expandable/collapsible content. 
+        // Set classes for expandable/collapsible content.
         $(document).on('click', 'a.expand, a.collapse', function(e) {
             e.preventDefault();
             var toggle = $(this);

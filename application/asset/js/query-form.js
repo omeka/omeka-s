68455cc2741e7ee016370b06f4c830010b39e038
Jim Safley <jimsafley@gmail.com>
2021-03-03 10:08:10 -0500

    Add query restore feature

--- application/asset/js/query-form.js
+++ application/asset/js/query-form.js
@@ -7,7 +7,13 @@ $(document).ready(function () {
         Omeka.openSidebar(sidebar);
         Omeka.populateSidebarContent(sidebar, selectingElement.data('sidebar-content-url') + '?' + selectingElement.find('.query-form-text').val());
     });
+    $('#content').on('click', '.query-form-restore', function (e) {
+        selectingElement = $(this).closest('.query-form-element');
+        selectingElement.find('.query-form-text').val(selectingElement.data('query'));
+        Omeka.closeSidebar(sidebar);
+    });
     $('#content').on('click', '.query-form-clear', function (e) {
+        selectingElement = $(this).closest('.query-form-element');
         selectingElement.find('.query-form-text').val('');
         Omeka.closeSidebar(sidebar);
     });

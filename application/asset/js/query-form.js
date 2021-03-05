5dfe7865227c71315792df9add92dba94ef8d526
Jim Safley <jimsafley@gmail.com>
2021-03-05 18:47:54 -0500

    Display query preview in sidebar

--- application/asset/js/query-form.js
+++ application/asset/js/query-form.js
@@ -1,37 +1,43 @@
 $(document).ready(function () {
     let selectingElement;
-    const sidebar = $('<div class="sidebar" id="query-sidebar"><div class="sidebar-content"></div></div>');
-    sidebar.appendTo('#content');
+    const sidebarEdit = $('<div class="sidebar" id="query-sidebar-edit"><div class="sidebar-content"></div></div>');
+    const sidebarPreview = $('<div class="sidebar" id="query-sidebar-preview"><div class="sidebar-content"></div></div>');
+    sidebarEdit.appendTo('#content');
+    sidebarPreview.appendTo('#content');
     $('#content').on('click', '.query-form-edit', function (e) {
         selectingElement = $(this).closest('.query-form-element');
-        Omeka.openSidebar(sidebar);
-        const url = selectingElement.data('sidebar-content-url');
+        Omeka.closeSidebar(sidebarPreview);
+        Omeka.openSidebar(sidebarEdit);
+        const url = selectingElement.data('sidebar-edit-url');
         // The advanced search form will not recognize the first parameter if it begins with "?".
         const query = selectingElement.find('.query-form-query').val().trim().replace(/^\?+/, '');
-        Omeka.populateSidebarContent(sidebar, `${url}?${query}`);
+        Omeka.populateSidebarContent(sidebarEdit, `${url}?${query}`);
     });
     $('#content').on('click', '.query-form-restore', function (e) {
         selectingElement = $(this).closest('.query-form-element');
         selectingElement.find('.query-form-query').val(selectingElement.data('query'));
-        Omeka.closeSidebar(sidebar);
+        Omeka.closeSidebar(sidebarEdit);
+        Omeka.closeSidebar(sidebarPreview);
     });
     $('#content').on('click', '.query-form-clear', function (e) {
         selectingElement = $(this).closest('.query-form-element');
         selectingElement.find('.query-form-query').val('');
-        Omeka.closeSidebar(sidebar);
+        Omeka.closeSidebar(sidebarEdit);
+        Omeka.closeSidebar(sidebarPreview);
     });
     $('#content').on('click', '.query-form-set', function (e) {
         const form = $('#advanced-search');
         Omeka.cleanSearchQuery(form);
         selectingElement.find('.query-form-query').val(form.serialize());
-        Omeka.closeSidebar(sidebar);
+        Omeka.closeSidebar(sidebarEdit);
     });
     $('#content').on('click', '.query-form-preview', function (e) {
-        const url = selectingElement.data('previewUrl');
+        Omeka.openSidebar(sidebarPreview);
+        const url = selectingElement.data('sidebar-preview-url');
         const query = $('#advanced-search').serialize();
-        window.open(`${url}?${query}`, '_blank');
+        Omeka.populateSidebarContent(sidebarPreview, `${url}?${query}`);
     });
     $('#content').on('click', '.query-form-reset', function (e) {
-        Omeka.populateSidebarContent(sidebar, selectingElement.data('sidebar-content-url'));
+        Omeka.populateSidebarContent(sidebarEdit, selectingElement.data('sidebar-edit-url'));
     });
 });
--- application/src/Controller/Admin/QueryController.php
+++ application/src/Controller/Admin/QueryController.php
@@ -9,10 +9,22 @@ use Laminas\Mvc\Controller\AbstractActionController;
 
 class QueryController extends AbstractActionController
 {
-    public function sidebarSearchAction()
+    public function sidebarEditAction()
     {
         $view = new ViewModel;
         $view->setTerminal(true);
+        return $view;
+    }
+
+    public function sidebarPreviewAction()
+    {
+        $this->setBrowseDefaults('created');
+        $response = $this->api()->search('items', $this->params()->fromQuery());
+        $this->paginator($response->getTotalResults());
+
+        $view = new ViewModel;
+        $view->setTerminal(true);
+        $view->setVariable('items', $response->getContent());
         return $view;
     }
 }

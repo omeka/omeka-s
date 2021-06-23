156cda0e786189af8361a0e176a6dd043373945a
Kim Nguyen <design@kimisgold.com>
2021-06-23 12:21:37 -0400

    Fix option variable assignments, reduce attachment code.

--- application/src/Site/BlockLayout/Asset.php
+++ application/src/Site/BlockLayout/Asset.php
@@ -27,43 +27,46 @@ class Asset extends AbstractBlockLayout
         $block->setData($data);
       }
       
-      public function alignmentClassSelect(PhpRenderer $view, SitePageBlockRepresentation $block = null
-      ) {
-          $alignmentLabels = ['default', 'float left', 'float right', 'center'];
-          $alignmentValues = ['default', 'left', 'right', 'center'];
-          $alignment = $block ? $block->dataValue('alignment', 'default') : 'default';
-          $select = new Select('o:block[__blockIndex__][o:data][alignment]');
-          $select->setValueOptions(array_combine($alignmentValues, $alignmentLabels))->setValue($alignment);
-          $selectLabel = 'Alignment'; // @translate
-          $select->setAttributes(['title' => $selectLabel, 'aria-label' => $selectLabel]);
-          $html = '<div class="field"><div class="field-meta">';
-          $html .= '<label class="thumbnail-option" for="o:block[__blockIndex__][o:data][alignment]">' . $selectLabel . '</label></div>';
-          $html .= '<div class="inputs">' . $view->formSelect($select) . '</div></div>';
-          return $html;
+    public function alignmentClassSelect(PhpRenderer $view, SitePageBlockRepresentation $block = null
+    ) {
+        $alignmentLabels = ['default', 'float left', 'float right', 'center'];
+        $alignmentValues = ['default', 'left', 'right', 'center'];
+        $alignment = $block ? $block->dataValue('alignment', 'default') : 'default';
+        $select = new Select('o:block[__blockIndex__][o:data][alignment]');
+        $select->setValueOptions(array_combine($alignmentValues, $alignmentLabels))->setValue($alignment);
+        $selectLabel = 'Alignment'; // @translate
+        $select->setAttributes(['title' => $selectLabel, 'aria-label' => $selectLabel]);
+        $html = '<div class="field"><div class="field-meta">';
+        $html .= '<label class="thumbnail-option" for="o:block[__blockIndex__][o:data][alignment]">' . $selectLabel . '</label></div>';
+        $html .= '<div class="inputs">' . $view->formSelect($select) . '</div></div>';
+        return $html;
+    }
+
+    public function prepareAssetAttachments(PhpRenderer $view, $blockData = null) {
+      $attachments = [];
+      if ($blockData) {  
+        foreach ($blockData as $key => $value) {
+          if (isset($value['id'])) {
+            $assetId = $value['id'];
+            $asset = $view->api()->read('assets', $assetId)->getContent();
+            $attachments[$key]['asset'] = $asset;
+            if ($value['page'] !== '') {
+              $attachments[$key]['page'] = $view->api()->read('site_pages', $value['page'])->getContent();
+            }
+            $attachments[$key]['alt_link_title'] = $value['alt_link_title'];
+            $attachments[$key]['caption'] = $value['caption'];
+          }
+        }
       }
+      return $attachments;
+    }
 
     public function form(PhpRenderer $view, SiteRepresentation $site, SitePageRepresentation $page = null, SitePageBlockRepresentation $block = null
     ) {
-        $escape = $view->plugin('escapeHtml');
-        $html = '';
         $siteId = $site->id();
         $apiUrl = $site->apiUrl();
         $blockData = ($block) ? $block->data() : '';
-        $attachments = [];
-        if ($blockData !== '') {  
-          foreach ($blockData as $key => $value) {
-            if (isset($value['id'])) {
-              $assetId = $value['id'];
-              $asset = $view->api()->read('assets', $assetId)->getContent();
-              $attachments[$key]['asset'] = $asset;
-              if ($value['page'] !== '') {
-                $attachments[$key]['page'] = $view->api()->read('site_pages', $value['page'])->getContent();
-              }
-              $attachments[$key]['alt_link_title'] = $value['alt_link_title'];
-              $attachments[$key]['caption'] = $value['caption'];
-            }
-          }
-        }
+        $attachments = $this->prepareAssetAttachments($view,$asset);
         $alignmentClassSelect = $this->alignmentClassSelect($view, $block);
         return $view->partial('common/asset', [
           'block' => $blockData,
@@ -73,27 +76,11 @@ class Asset extends AbstractBlockLayout
           'alignmentClassSelect' => $alignmentClassSelect,
         ]);
     }
-
     
     public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
     {
         $blockData = ($block) ? $block->data() : '';
-        $attachments = [];
-        if ($block !== '') {
-          foreach ($blockData as $key => $value) {
-            if (isset($value['id'])) {
-              $assetId = $value['id'];
-              $asset = $view->api()->read('assets', $assetId)->getContent();
-              $attachments[$key]['asset'] = $asset;
-              if ($value['page'] !== '') {
-                $attachments[$key]['page'] = $view->api()->read('site_pages', $value['page'])->getContent();
-              }
-              $attachments[$key]['alt_link_title'] = $value['alt_link_title'];
-              $attachments[$key]['caption'] = $value['caption'];
-            }
-          }
-        }
-
+        $attachments = $this->prepareAssetAttachments($view, $blockData);
         $customClass = $block->dataValue('className');
         $alignment = $block->dataValue('alignment');
         return $view->partial('common/block-layout/asset', [

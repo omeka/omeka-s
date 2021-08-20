57c1250ba9193b32fd99378f0a40a1211ef355db
Jim Safley <jimsafley@gmail.com>
2021-08-19 22:34:18 -0400

    Begin implementing resource value annotations

--- application/src/DataType/Resource/Item.php
+++ application/src/DataType/Resource/Item.php
@@ -1,7 +1,10 @@
 <?php
 namespace Omeka\DataType\Resource;
 
-class Item extends AbstractResource
+use Omeka\DataType\ValueAnnotatableInterface;
+use Laminas\View\Renderer\PhpRenderer;
+
+class Item extends AbstractResource implements ValueAnnotatableInterface
 {
     public function getName()
     {
@@ -11,5 +14,25 @@ class Item extends AbstractResource
     public function getLabel()
     {
         return 'Item'; // @translate
+    }
+
+    public function valueAnnotationPrepareForm(PhpRenderer $view)
+    {
+    }
+
+    public function valueAnnotationForm(PhpRenderer $view)
+    {
+        $form = '
+        <span class="display-title"></span>
+        <input type="hidden" class="value_resource_id" data-value-key="value_resource_id">
+        <input type="hidden" class="display_title" data-value-key="display_title">
+        %s';
+        return sprintf(
+            $form,
+            $view->hyperlink($view->translate('Items'), '#', [
+                'class' => 'o-icon-items button value-annotation-resource-select',
+                'data-sidebar-content-url' => $view->url('admin/default', ['controller' => 'item', 'action' => 'sidebar-select'], false),
+            ])
+        );
     }
 }

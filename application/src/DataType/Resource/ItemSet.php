67709ee9ccd3513cb910d3bab0071844069d0738
Jim Safley <jimsafley@gmail.com>
2021-08-20 13:20:27 -0400

    Implement item set and media resource value annotations

--- application/src/DataType/Resource/ItemSet.php
+++ application/src/DataType/Resource/ItemSet.php
@@ -1,7 +1,10 @@
 <?php
 namespace Omeka\DataType\Resource;
 
-class Itemset extends AbstractResource
+use Omeka\DataType\ValueAnnotatableInterface;
+use Laminas\View\Renderer\PhpRenderer;
+
+class Itemset extends AbstractResource implements ValueAnnotatableInterface
 {
     public function getName()
     {
@@ -11,5 +14,25 @@ class Itemset extends AbstractResource
     public function getLabel()
     {
         return 'Item Set'; // @translate
+    }
+
+    public function valueAnnotationPrepareForm(PhpRenderer $view)
+    {
+    }
+
+    public function valueAnnotationForm(PhpRenderer $view)
+    {
+        $form = '
+        <span class="display-title"></span>
+        <input type="hidden" class="value_resource_id" data-value-key="value_resource_id">
+        <input type="hidden" class="display_title" data-value-key="display_title">
+        %s';
+        return sprintf(
+            $form,
+            $view->hyperlink($view->translate('Item sets'), '#', [
+                'class' => 'o-icon-item-sets button value-annotation-resource-select',
+                'data-sidebar-content-url' => $view->url('admin/default', ['controller' => 'item-set', 'action' => 'sidebar-select'], false),
+            ])
+        );
     }
 }
--- application/src/DataType/Resource/Media.php
+++ application/src/DataType/Resource/Media.php
@@ -1,7 +1,10 @@
 <?php
 namespace Omeka\DataType\Resource;
 
-class Media extends AbstractResource
+use Omeka\DataType\ValueAnnotatableInterface;
+use Laminas\View\Renderer\PhpRenderer;
+
+class Media extends AbstractResource implements ValueAnnotatableInterface
 {
     public function getName()
     {
@@ -11,5 +14,25 @@ class Media extends AbstractResource
     public function getLabel()
     {
         return 'Media'; // @translate
+    }
+
+    public function valueAnnotationPrepareForm(PhpRenderer $view)
+    {
+    }
+
+    public function valueAnnotationForm(PhpRenderer $view)
+    {
+        $form = '
+        <span class="display-title"></span>
+        <input type="hidden" class="value_resource_id" data-value-key="value_resource_id">
+        <input type="hidden" class="display_title" data-value-key="display_title">
+        %s';
+        return sprintf(
+            $form,
+            $view->hyperlink($view->translate('Media'), '#', [
+                'class' => 'o-icon-media button value-annotation-resource-select',
+                'data-sidebar-content-url' => $view->url('admin/default', ['controller' => 'media', 'action' => 'sidebar-select'], false),
+            ])
+        );
     }
 }

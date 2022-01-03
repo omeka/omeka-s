3f718ac58ef52e310aa4fa5285575d1ac3da962b
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-01-03 00:00:00 +0000

    Updated some doctrine queries.

--- application/data/migrations/20210225095734_addAltText.php
+++ application/data/migrations/20210225095734_addAltText.php
@@ -8,7 +8,7 @@ class addAltText implements MigrationInterface
 {
     public function up(Connection $conn)
     {
-        $conn->query('ALTER TABLE asset ADD alt_text LONGTEXT DEFAULT NULL');
-        $conn->query('ALTER TABLE media ADD alt_text LONGTEXT DEFAULT NULL');
+        $connection->executeQuery('ALTER TABLE asset ADD alt_text LONGTEXT DEFAULT NULL');
+        $connection->executeQuery('ALTER TABLE media ADD alt_text LONGTEXT DEFAULT NULL');
     }
 }

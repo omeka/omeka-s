40e9bd2e5479ddf16c6133a54ff1d1f4b7a195ca
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-01-17 00:00:00 +0000

    Added jp2 to the media-type map.
    Updated the script to build the media types map with a specific list (fix #1806).

---
+++ application/data/media-types/media-type-specific.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Specific media-types that are not included in default Apache media types.
+ *
+ * This list is appended to the Apache list to build the Omeka list.
+ */
+
+return [
+    'image/jp2' => [
+        'jp2',
+        'j2k',
+    ],
+];

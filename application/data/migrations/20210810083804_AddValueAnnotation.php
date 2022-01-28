4720a6d53fcd29f29b39ae40e5625000663216bc
John Flatness <john@zerocrates.org>
2022-01-28 18:02:09 -0500

    Merge value annotations migrations down to one

--- application/data/migrations/20210810083804_AddValueAnnotation.php
+++ application/data/migrations/20210810083804_AddValueAnnotation.php
@@ -11,7 +11,7 @@ class AddValueAnnotation implements MigrationInterface
         $conn->query('CREATE TABLE value_annotation (id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;');
         $conn->query('ALTER TABLE value_annotation ADD CONSTRAINT FK_C03BA4EBF396750 FOREIGN KEY (id) REFERENCES resource (id) ON DELETE CASCADE;');
         $conn->query('ALTER TABLE value ADD value_annotation_id INT DEFAULT NULL;');
-        $conn->query('ALTER TABLE value ADD CONSTRAINT FK_1D7758349B66727E FOREIGN KEY (value_annotation_id) REFERENCES value_annotation (id);');
+        $conn->query('ALTER TABLE value ADD CONSTRAINT FK_1D7758349B66727E FOREIGN KEY (value_annotation_id) REFERENCES value_annotation (id) ON DELETE SET NULL;');
         $conn->query('CREATE UNIQUE INDEX UNIQ_1D7758349B66727E ON value (value_annotation_id);');
     }
 }

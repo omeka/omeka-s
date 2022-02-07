2b75496049f95630228e98b573d680255236756d
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-02-07 00:00:00 +0000

    Updated the script to build the media types map with a specific list (fix #1806).

--- application/data/scripts/create-media-type-map.php
+++ application/data/scripts/create-media-type-map.php
@@ -6,7 +6,7 @@
  * projects. We use this map when a file's media type is known but there is no
  * file extension.
  */
-$url = 'http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types';
+$url = 'https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types';
 $mediaTypes = [];
 foreach (explode(PHP_EOL, file_get_contents($url)) as $mt) {
     if ('#' == substr($mt, 0, 1) || !$mt) {
@@ -15,5 +15,10 @@ foreach (explode(PHP_EOL, file_get_contents($url)) as $mt) {
     preg_match_all('/[^\s]+/', $mt, $matches);
     $mediaTypes[array_shift($matches[0])] = $matches[0];
 }
+
+$specificMediaTypes = include dirname(__DIR__) . '/media-types/media-type-specific.php';
+$mediaTypes = array_merge($mediaTypes, $specificMediaTypes);
+ksort($mediaTypes);
+
 $data = sprintf("<?php\nreturn %s;\n", var_export($mediaTypes, true));
 file_put_contents(dirname(__DIR__) . '/media-types/media-type-map.php', $data);

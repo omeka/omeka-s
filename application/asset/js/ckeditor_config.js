cfd9494cd2d8ed5dd438bb94a35521e5c999dfbf
Jim Safley <jimsafley@gmail.com>
2022-03-23 13:13:06 -0400

    Trigger event for CKEditor config

--- application/asset/js/ckeditor_config.js
+++ application/asset/js/ckeditor_config.js
@@ -1,47 +1,20 @@
-/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
-CKEDITOR.editorConfig = function( config ) {
-    // Define changes to default configuration here.
-    // For complete reference see:
-    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-    let eventData;
-
+// @see https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+CKEDITOR.editorConfig = function(config) {
     // Configure the toolbar
-    eventData = {
-        toolbar: [
-            {
-                name: 'advanced',
-                items : [
-                    'Sourcedialog',
-                    '-',
-                    'Link', 'Unlink', 'Anchor',
-                    '-',
-                    'Format', 'Styles', 'PasteFromWord'
-                ]
-            },
-            '/',
-            {
-                items: [
-                    'Bold', 'Italic', 'Underline', 'Strike',
-                    '-',
-                    'NumberedList', 'BulletedList', 'Indent', 'Outdent', 'Blockquote',
-                ]
-            }
-        ]
-    };
-    $(document).trigger('ckeditor:toolbar', eventData);
-    config.toolbar = eventData.toolbar;
-
+    config.toolbar = [
+        {
+            name: 'advanced',
+            items : ['Sourcedialog', '-', 'Link', 'Unlink', 'Anchor', '-', 'Format', 'Styles', 'PasteFromWord']
+        },
+        '/',
+        {
+            items: ['Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', 'Indent', 'Outdent', 'Blockquote']
+        }
+    ];
     // Disable content filtering
     config.allowedContent = true;
-
     // Add extra plugins
-    eventData = {
-        extraPlugins: ['sourcedialog']
-    };
-    $(document).trigger('ckeditor:extra-plugins', eventData);
-    config.extraPlugins = eventData.extraPlugins;
+    config.extraPlugins = ['sourcedialog'];
+    // Allow other scripts to modify configuration.
+    $(document).trigger('ckeditor:config', config);
 };

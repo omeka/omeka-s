1d48e29239dff6fa39004c735d1f28db771ac918
Jim Safley <jimsafley@gmail.com>
2022-07-08 21:35:46 -0400

    Remove extra space

--- application/src/Controller/IiifViewerController.php
+++ application/src/Controller/IiifViewerController.php
@@ -34,7 +34,7 @@ class IiifViewerController extends AbstractActionController
         // Get the user-defined Mirador configuration and apply it to the
         // default configuration. Here we use an allow-list instead of a
         // recursive merge to prevent malicious configurations.
-        $miradorConfigUser =  json_decode($this->params()->fromQuery('mirador_config'), true);
+        $miradorConfigUser = json_decode($this->params()->fromQuery('mirador_config'), true);
         if (isset($miradorConfigUser['window.sideBarOpen'])) {
             $miradorConfig['window']['sideBarOpen'] = $miradorConfigUser['window.sideBarOpen'];
         }

c1a6703eccdae1b55baa74c20cf7e3cf7c9521d5
Jim Safley <jimsafley@gmail.com>
2022-08-09 12:59:49 -0400

    Set browse links as active only if their paths and queries are identical (#695)

--- application/config/module.config.php
+++ application/config/module.config.php
@@ -757,8 +757,10 @@ return [
         'invokables' => [
             'page' => Site\Navigation\Link\Page::class,
             'url' => Site\Navigation\Link\Url::class,
-            'browse' => Site\Navigation\Link\Browse::class,
             'browseItemSets' => Site\Navigation\Link\BrowseItemSets::class,
+        ],
+        'factories' => [
+            'browse' => Service\Site\Navigation\Link\BrowseFactory::class,
         ],
     ],
     'media_ingesters' => [
---
+++ application/src/Site/Navigation/Page/UriWithQuery.php
@@ -0,0 +1,35 @@
+<?php
+namespace Omeka\Site\Navigation\Page;
+
+use Laminas\Http\Request;
+use Laminas\Navigation\Page\Uri;
+use Laminas\Uri\UriFactory;
+
+class UriWithQuery extends Uri
+{
+    public function isActive($recursive = false)
+    {
+        if (!$this->active) {
+            if ($this->getRequest() instanceof Request) {
+                $uriCurrent = $this->getRequest()->getUri();
+                $uriCurrentQuery = $uriCurrent->getQueryAsArray();
+                unset($uriCurrentQuery['page'], $uriCurrentQuery['sort_by'], $uriCurrentQuery['sort_order']);
+                $uriCurrent->setQuery($uriCurrentQuery);
+
+                $uriPage = UriFactory::factory($this->getUri());
+                $uriPageQuery = $uriPage->getQueryAsArray();
+                unset($uriPageQuery['page'], $uriPageQuery['sort_by'], $uriPageQuery['sort_order']);
+                $uriPage->setQuery($uriPageQuery);
+
+                $identicalPaths = $uriCurrent->getPath() === $uriPage->getPath();
+                $identicalQueries = $uriCurrent->getQuery() === $uriPage->getQuery();
+
+                if ($identicalPaths && $identicalQueries) {
+                    $this->active = true;
+                    return true;
+                }
+            }
+        }
+        return parent::isActive($recursive);
+    }
+}

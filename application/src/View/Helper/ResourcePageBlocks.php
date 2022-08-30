b3c908847440a264a70abb1106db8a6d31801214
Jim Safley <jimsafley@gmail.com>
2022-08-30 14:08:50 -0400

    Add a way to check if a resource/region has blocks before getting markup (#1908)

--- application/src/View/Helper/ResourcePageBlocks.php
+++ application/src/View/Helper/ResourcePageBlocks.php
@@ -18,36 +18,54 @@ class ResourcePageBlocks extends AbstractHelper
      *
      * @param Representation\AbstractResourceEntityRepresentation $resource
      * @param string $regionName
-     * @return ResourcePageBlocks
+     * @return string
      */
     public function __invoke(Representation\AbstractResourceEntityRepresentation $resource, $regionName = 'main')
     {
+        if (!$this->hasBlocks($resource, $regionName)) {
+            return '';
+        }
         $view = $this->getView();
-        $resourceClass = get_class($resource);
-        switch ($resourceClass) {
-            case Representation\ItemRepresentation::class:
-                $resourceName = 'items';
-                break;
-            case Representation\ItemSetRepresentation::class:
-                $resourceName = 'item_sets';
-                break;
-            case Representation\MediaRepresentation::class:
-                $resourceName = 'media';
-                break;
-            default:
-                return '<!-- ' . sprintf($view->translate('Resource page blocks error: invalid resource "%s"'), $resourceClass) . ' -->';
-        }
-        if (!isset($this->resourcePageBlocks[$resourceName])) {
-            return '<!-- ' . sprintf($view->translate('Resource page blocks error: resource not supported "%s"'), $resourceName) . ' -->';
-        }
-        if (!isset($this->resourcePageBlocks[$resourceName][$regionName])) {
-            return '<!-- ' . sprintf($view->translate('Resource page blocks error: region not supported "%s"'), $regionName) . ' -->';
-        }
+        $resourceName = $this->getResourceName($resource);
         $blockMarkup = [];
         foreach ($this->resourcePageBlocks[$resourceName][$regionName] as $blockName) {
             $blockLayout = $this->blockLayoutManager->get($blockName);
             $blockMarkup[] = $blockLayout->render($view, $resource);
         }
         return implode('', $blockMarkup);
+    }
+
+    /**
+     * Does this resource/region have blocks?
+     *
+     * @param Representation\AbstractResourceEntityRepresentation $resource
+     * @param string $regionName
+     * @return bool
+     */
+    public function hasBlocks(Representation\AbstractResourceEntityRepresentation $resource, $regionName = 'main')
+    {
+        $resourceName = $this->getResourceName($resource);
+        return isset($this->resourcePageBlocks[$resourceName][$regionName]);
+    }
+
+    /**
+     * Get the resource name of the passed resource.
+     *
+     * @param Representation\AbstractResourceEntityRepresentation $resource
+     * @return string
+     */
+    protected function getResourceName(Representation\AbstractResourceEntityRepresentation $resource)
+    {
+        $resourceClass = get_class($resource);
+        switch ($resourceClass) {
+            case Representation\ItemRepresentation::class:
+                return 'items';
+            case Representation\ItemSetRepresentation::class:
+                return 'item_sets';
+            case Representation\MediaRepresentation::class:
+                return 'media';
+            default:
+                throw new \Exception('Invalid resource');
+        }
     }
 }

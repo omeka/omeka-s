omeka/omeka-s

commit 613c45c7af285b9194f5557f828fba9d5dbbdc57

Don't allow inactive users to continue sessions

application/src/Authentication/Storage/DoctrineWrapper.php
@@ -53,7 +53,7 @@ class DoctrineWrapper implements StorageInterface
         $identity = $this->storage->read();
         if ($identity) {
             try {
-                return $this->repository->find($identity);
+                return $this->repository->findOneBy(['id' => $identity, 'isActive' => true]);
             } catch (DBALException $e) {
                 // The user table does not exist.
                 return null;

commit dd8fc8c30501306e0121af7b161c7ca78b6ab274

Explicitly cache identity lookups from Doctrine
This fixes a performance regression from
613c45c7af285b9194f5557f828fba9d5dbbdc57 (also backported to 3.2.3)

Previously we relied on Doctrine's inherent ID lookup caching to avoid
redundant queries for the current user, but adding the query requirement
to also check if the user was active resulted in Doctrine always
performing the query, leading to large numbers of identical queries for
the current user.

To avoid this, we store the result of the first lookup against Doctrine
and return its result for all later requests for the user identity
within the same request.

application/src/Authentication/Storage/DoctrineWrapper.php
@@ -24,6 +24,18 @@ class DoctrineWrapper implements StorageInterface
     protected $repository;
 
     /**
+     * Cached identity lookup result
+     *
+     * False (the default) indicates no cached result, and the lookup must be
+     * performed against the database. Null indicates a cached result of no
+     * identity (no user logged in or the user was invalid), and the user
+     * entity object is stored here if a user is logged in.
+     *
+     * @var Omeka\Entity|null|bool
+     */
+    protected $cachedIdentity = false;
+
+    /**
      * Create the wrapper around the given storage method, looking up users
      * from the given repository.
      *
@@ -50,25 +62,33 @@ class DoctrineWrapper implements StorageInterface
 
     public function read()
     {
-        $identity = $this->storage->read();
-        if ($identity) {
+        if ($this->cachedIdentity !== false) {
+            return $this->cachedIdentity;
+        }
+
+        $identity = null;
+        $id = $this->storage->read();
+        if ($id) {
             try {
-                return $this->repository->findOneBy(['id' => $identity, 'isActive' => true]);
+                $identity = $this->repository->findOneBy(['id' => $id, 'isActive' => true]);
             } catch (DBALException $e) {
                 // The user table does not exist.
-                return null;
             }
         }
-        return null;
+
+        $this->cachedIdentity = $identity;
+        return $identity;
     }
 
     public function write($identity)
     {
+        $this->cachedIdentity = $identity;
         $this->storage->write($identity->getId());
     }
 
     public function clear()
     {
+        $this->cachedIdentity = false;
         $this->storage->clear();
     }
 
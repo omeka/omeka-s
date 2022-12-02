e410377755d0d599722a029faf5d728672dc82b3
John Flatness <john@zerocrates.org>
2022-12-02 15:34:46 -0500

    Discard plural rules of .mo files without plurals
    Avoids most "plural rules don't match" exceptions by eliminating
    the declared plural rule at load time if the translation file doesn't
    actually contain any plural translations.

--- application/src/Service/Delegator/TranslatorDelegatorFactory.php
+++ application/src/Service/Delegator/TranslatorDelegatorFactory.php
@@ -3,6 +3,7 @@ namespace Omeka\Service\Delegator;
 
 use Interop\Container\ContainerInterface;
 use Omeka\I18n\Translator;
+use Laminas\I18n\Translator\Loader;
 use Laminas\ServiceManager\Factory\DelegatorFactoryInterface;
 
 class TranslatorDelegatorFactory implements DelegatorFactoryInterface
@@ -10,6 +11,11 @@ class TranslatorDelegatorFactory implements DelegatorFactoryInterface
     public function __invoke(ContainerInterface $container, $name,
         callable $callback, array $options = null
     ) {
-        return new Translator($callback());
+        $translator = $callback();
+        $translator->getPluginManager()->setFactory(Loader\Gettext::class, function ($loaders) {
+            $loader = new \Omeka\I18n\GettextLoader;
+            return $loader;
+        });
+        return new Translator($translator);
     }
 }

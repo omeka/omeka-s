0d866fdfcb2fb878712b5fd125c6bafa176c26ae
Jim Safley <jimsafley@gmail.com>
2023-01-11 20:14:11 -0500

    Restore deleted code

--- application/src/Controller/AbstractApiController.php
+++ application/src/Controller/AbstractApiController.php
@@ -142,6 +142,12 @@ abstract class AbstractApiController extends AbstractRestfulController
     {
         $request = $this->getRequest();
 
+        // Set the output format.
+        $this->setViewOption('format', $request->getQuery('format'));
+
+        // Set the Accept header.
+        $this->setViewOption('accept_header', $request->getHeader('Accept'));
+
         // Set pretty print.
         $prettyPrint = $request->getQuery('pretty_print');
         if (null !== $prettyPrint) {

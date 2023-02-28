6b7eec073c6118d58c11defbf8d41dda4bd41be4
Jim Safley <jimsafley@gmail.com>
2023-02-27 19:05:14 -0500

    Move render() back to fallback

--- application/src/Media/FileRenderer/AbstractRenderer.php
+++ application/src/Media/FileRenderer/AbstractRenderer.php
@@ -6,12 +6,6 @@ use Omeka\Api\Representation\MediaRepresentation;
 
 abstract class AbstractRenderer implements RendererInterface
 {
-    public function render(PhpRenderer $view, MediaRepresentation $media, array $options = [])
-    {
-        $link = $options['link'] ?? 'original';
-        return $view->hyperlink($media->filename(), $this->getLinkUrl($media, $link));
-    }
-
     /**
      * Get the URL for the given type of link
      *
--- application/src/Media/FileRenderer/FallbackRenderer.php
+++ application/src/Media/FileRenderer/FallbackRenderer.php
@@ -3,4 +3,9 @@ namespace Omeka\Media\FileRenderer;
 
 class FallbackRenderer extends AbstractRenderer
 {
+    public function render(PhpRenderer $view, MediaRepresentation $media, array $options = [])
+    {
+        $link = $options['link'] ?? 'original';
+        return $view->hyperlink($media->filename(), $this->getLinkUrl($media, $link));
+    }
 }

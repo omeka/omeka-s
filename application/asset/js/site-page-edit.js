078653c17f7b091af9a176a22c667155c274a4b8
John Flatness <john@zerocrates.org>
2023-03-28 17:44:40 -0400

    More specific selector for site asset block
    Don't accidentally get any other .selected-asset than the one in the
    asset-options sidebar.
    
    (variation on the remaining bit of #1981)

--- application/asset/js/site-page-edit.js
+++ application/asset/js/site-page-edit.js
@@ -107,7 +107,7 @@
     }
 
      function populateAssetAttachment(attachment) {
-        var asset = $('.selected-asset');
+        var asset = $('#asset-options .selected-asset');
         var assetTitle = asset.find('.selected-asset-name').html();
         var assetImage = asset.find('img').clone().attr('class', '');
         var assetId = asset.find('.selected-asset-id').val();

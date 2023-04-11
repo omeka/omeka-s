8d0d28804e1e284c3ba652a053ccba0739bff507
John Flatness <john@zerocrates.org>
2023-04-10 21:24:40 -0400

    Catch missing "bracketed" text from #2006
    (cherry picked from commit 403ed53031507c43e2f76dac27db754d28711f09)

--- application/src/Form/ResourceForm.php
+++ application/src/Form/ResourceForm.php
@@ -89,7 +89,7 @@ class ResourceForm extends Form
             ],
             'options' => [
                 'label' => 'Owner', // @translate
-                'empty_option' => $owner ? null : '[no owner]', // @translate
+                'empty_option' => $owner ? null : '[No owner]', // @translate
                 'resource_value_options' => [
                     'resource' => 'users',
                     'query' => [],

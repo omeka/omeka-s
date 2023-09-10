21cfcdfc1769383581c8028e4bc5cf97839202ca
Jim Safley <jimsafley@gmail.com>
2023-09-09 20:51:00 -0400

    Add title to property and class select options

--- application/src/Form/Element/PropertySelect.php
+++ application/src/Form/Element/PropertySelect.php
@@ -44,6 +44,7 @@ class PropertySelect extends AbstractVocabularyMemberSelect
                         'attributes' => [
                             'data-term' => $property->term(),
                             'data-property-id' => $property->id(),
+                            'title' => $member->comment(),
                         ],
                     ];
                 }

ed21ad916e6662c81913ea1652583e625e9cf247
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-11-20 00:00:00 +0000

    Applied templates to resource templates for Select.

--- application/src/Form/Element/ResourceTemplateSelect.php
+++ application/src/Form/Element/ResourceTemplateSelect.php
@@ -12,4 +12,46 @@ class ResourceTemplateSelect extends AbstractGroupByOwnerSelect
     {
         return $resource->label();
     }
+
+    /**
+     * Get value options for templates.
+     *
+     * If the "apply_templates" option is set, get only the configured resource
+     * templates.
+     * Otherwise get the default value options.
+     */
+    public function getValueOptions(): array
+    {
+        $applyTemplates = $this->getOption('apply_templates');
+        if (!is_array($applyTemplates)) {
+            // Use default method.
+            return parent::getValueOptions();
+        }
+
+        // Keep original query for further processing.
+        $originalQuery = $this->getOption('query');
+
+        $applyTemplates = array_values($applyTemplates);
+
+        if ($applyTemplates) {
+            $query = $originalQuery;
+            if (!is_array($query) || empty($query)) {
+                $query = ['id' => $applyTemplates];
+            } elseif (empty($query['id'])) {
+                $query['id'] = $applyTemplates;
+            } elseif (is_array($query['id'])) {
+                $query['id'] = array_unique(array_merge(array_values($query['id']), $applyTemplates));
+            } else {
+                $query['id'] = array_unique(array_merge([$query['id']], $applyTemplates));
+            }
+        }
+
+        $this->setOption('query', $query);
+
+        $valueOptions = parent::getValueOptions();
+
+        $this->setOption('query', $originalQuery);
+
+        return $valueOptions;
+    }
 }

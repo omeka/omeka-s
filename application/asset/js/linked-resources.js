117c863ac5c9a857e35abbd579f0db36a3e2ae3c
Jim Safley <jimsafley@gmail.com>
2024-04-05 16:57:48 -0400

    Enable dynamic linked resources interface

--- application/asset/js/linked-resources.js
+++ application/asset/js/linked-resources.js
@@ -1,7 +1,51 @@
 $(document).ready(function () {
+
+    // Render linked resources.
+    const renderLinkedResources = function(page, resourceProperty) {
+        let url = container.data('url');
+        searchParams = new URLSearchParams;
+        if (page) {
+            searchParams.append('page', page);
+        }
+        if (resourceProperty) {
+            searchParams.append('resource_property', resourceProperty);
+        }
+        url += '?' + searchParams.toString();
+        $.get(url, function(data) {
+            container.html(data);
+        });
+    };
+
+    // Render linked resources on initial load.
     const container = $('#linked-resources-container');
-    const url = container.data('url');
-    $.get(url, function(data) {
-        container.html(data);
+    renderLinkedResources();
+
+    // Handle next and previous clicks.
+    $(container).on('click', 'a.next, a.previous', function(e) {
+        e.preventDefault();
+        const thisButton = $(this);
+        // Note that we can use any base URL for this purpose.
+        const url = new URL(thisButton.attr('href'), 'http://foo');
+        renderLinkedResources(
+            url.searchParams.get('page'),
+            url.searchParams.get('resource_property')
+        );
+    });
+
+    // Handle page form submission.
+    $(container).on('submit', 'form', function(e) {
+        e.preventDefault();
+        const thisForm = $(this);
+        const searchParams = new URLSearchParams(thisForm.serialize());
+        renderLinkedResources(
+            searchParams.get('page'),
+            searchParams.get('resource_property')
+        );
+    });
+
+    // Handle resource property select.
+    $(container).on('change', '#resource-property-select', function(e) {
+        const thisSelect = $(this);
+        renderLinkedResources('1', thisSelect.val());
     });
 });

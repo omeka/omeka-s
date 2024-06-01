daf1601cffcbcf00d3e8a1c5b43af3b7444890d1
Jim Safley <jimsafley@gmail.com>
2024-06-01 07:32:33 -0400

    Add site config for IIIF viewer

--- application/src/Form/SiteSettingsForm.php
+++ application/src/Form/SiteSettingsForm.php
@@ -33,6 +33,7 @@ class SiteSettingsForm extends Form
             'browse' => 'Browse', // @translate
             'show' => 'Show', // @translate
             'search' => 'Search', // @translate
+            'iiif_viewer' => 'IIIF viewer', // @translate
         ]);
 
         // o:assign_new_items element is a pseudo-setting that's ultimately set
@@ -421,6 +422,34 @@ class SiteSettingsForm extends Form
             ],
             'attributes' => [
                 'value' => $settings->get('search_restrict_templates', false),
+            ],
+        ]);
+
+        // IIIF viewer section
+        $this->add([
+            'type' => 'checkbox',
+            'name' => 'iiif_viewer_sidebar',
+            'options' => [
+                'element_group' => 'iiif_viewer',
+                'label' => 'Show sidebar', // @translate
+            ],
+            'attributes' => [
+                'value' => $settings->get('iiif_viewer_sidebar', false),
+            ],
+        ]);
+        $this->add([
+            'type' => 'select',
+            'name' => 'iiif_viewer_theme',
+            'options' => [
+                'element_group' => 'iiif_viewer',
+                'label' => 'Theme', // @translate
+                'value_options' => [
+                    'light' => 'Light', // @translate
+                    'dark' => 'Dark', // @translate
+                ],
+            ],
+            'attributes' => [
+                'value' => $settings->get('iiif_viewer_theme'),
             ],
         ]);
 

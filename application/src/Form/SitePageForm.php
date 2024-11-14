e1564e4555bc3b10ed6c937ee3d198dafeec284b
Kim Nguyen <design@kimisgold.com>
2024-11-14 15:45:06 -0500

    Fix id for add to nav checkbox.

--- application/src/Form/SitePageForm.php
+++ application/src/Form/SitePageForm.php
@@ -41,6 +41,9 @@ class SitePageForm extends Form
                 'options' => [
                     'label' => 'Add to navigation', // @translate
                 ],
+                'attributes' => [
+                    'id' => 'add_to_navigation',
+                ],
             ]);
         }
 

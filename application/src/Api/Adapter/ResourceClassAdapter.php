612bfacc676eef3f42a85dc525b41e91607a4f0b
John Flatness <john@zerocrates.org>
2025-02-28 12:02:09 -0500

    Simplify query for resource classes used in site

--- application/src/Api/Adapter/ResourceClassAdapter.php
+++ application/src/Api/Adapter/ResourceClassAdapter.php
@@ -149,21 +149,15 @@ class ResourceClassAdapter extends AbstractEntityAdapter
         if (isset($query['site_id']) && is_numeric($query['site_id'])) {
             $siteAlias = $this->createAlias();
             $itemAlias = $this->createAlias();
-            $resourcesAlias = $this->createAlias();
-            $subquery = $this->getEntityManager()
-                ->createQueryBuilder()
-                ->select("IDENTITY($resourcesAlias.resourceClass)")
-                ->from('Omeka\Entity\Resource', $resourcesAlias)
-                ->join('Omeka\Entity\Site', $siteAlias)
+            $qb->join('Omeka\Entity\Site', $siteAlias)
                 ->join(
                     "$siteAlias.items",
                     $itemAlias,
                     'WITH',
-                    "$itemAlias.id = $resourcesAlias.id"
+                    "$itemAlias.resourceClass = omeka_root.id"
                 )
                 ->andWhere($qb->expr()->eq("$siteAlias.id",
                     $this->createNamedParameter($qb, $query['site_id'])));
-            $qb->andWhere($qb->expr()->in('omeka_root.id', $subquery->getDQL()));
         }
     }
 

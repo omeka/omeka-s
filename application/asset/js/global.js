omeka/omeka-s

commit 8465b95c0568180078dedcf4f7f76cf7b23a3903

Add property "has / does not have data type" to advanced search (#2246)

application/asset/js/global.js
@@ -322,10 +322,16 @@ var Omeka = {
     disableQueryTextInput: function() {
         var queryType = $(this);
         var queryText = queryType.siblings('.query-text');
-        if (queryType.val() === 'ex' || queryType.val() === 'nex') {
+        var queryTextDataType = queryType.siblings('.query-text-data-type');
+        if (['dt', 'ndt'].includes(queryType.val())) {
+            queryText.prop('disabled', true).val('');
+            queryTextDataType.prop('disabled', false).show();
+        } else if (['ex', 'nex'].includes(queryType.val())) {
             queryText.prop('disabled', true);
+            queryTextDataType.prop('disabled', true).hide();
         } else {
             queryText.prop('disabled', false);
+            queryTextDataType.prop('disabled', true).hide();
         }
     },
 

application/asset/js/advanced-search.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
         var template = fieldContainer.data('field-template');
         var newValue = $(template);
         newValue.children('input[type="text"]').val(null);
+        newValue.find('.query-text-data-type').prop('disabled', true).hide();
         newValue.children('select').prop('selectedIndex', 0);
         newValue.appendTo(fieldContainer.find('.inputs'));
         newValue.trigger('o:value-created');

application/src/Api/Adapter/AbstractResourceEntityAdapter.php
@@ -277,11 +277,11 @@ abstract class AbstractResourceEntityAdapter extends AbstractEntityAdapter imple
             }
 
             $positive = true;
-            if (in_array($queryType, ['neq', 'nin', 'nsw', 'new', 'nres', 'nex'])) {
+            if (in_array($queryType, ['neq', 'nin', 'nsw', 'new', 'nres', 'nex', 'ndt'])) {
                 $positive = false;
                 $queryType = substr($queryType, 1);
             }
-            if (!in_array($queryType, ['eq', 'in', 'sw', 'ew', 'res', 'ex'])) {
+            if (!in_array($queryType, ['eq', 'in', 'sw', 'ew', 'res', 'ex', 'dt'])) {
                 continue;
             }
 
@@ -380,6 +380,13 @@ abstract class AbstractResourceEntityAdapter extends AbstractEntityAdapter imple
 
                 case 'ex':
                     $predicateExpr = $qb->expr()->isNotNull("$valuesAlias.id");
+                    break;
+
+                case 'dt':
+                    $predicateExpr = $qb->expr()->eq(
+                        "$valuesAlias.type",
+                        $this->createNamedParameter($qb, $value)
+                    );
                     break;
 
                 default:

application/src/View/Helper/DataType.php
@@ -80,7 +80,7 @@ class DataType extends AbstractHelper
         $valueOptions = array_merge($valueOptions, $optgroupOptions);
 
         $element = new Select($name);
-        $element->setEmptyOption('')
+        $element->setEmptyOption($options['empty_option'] ?? '')
             ->setValueOptions($valueOptions)
             ->setAttributes($attributes);
         if (!$element->getAttribute('multiple') && is_array($value)) {

application/src/View/Helper/SearchFilters.php
@@ -21,13 +21,14 @@ class SearchFilters extends AbstractHelper
      */
     public function __invoke($partialName = null, array $query = null)
     {
+        $view = $this->getView();
         $partialName = $partialName ?: self::PARTIAL_NAME;
 
-        $translate = $this->getView()->plugin('translate');
+        $translate = $view->plugin('translate');
 
         $filters = [];
-        $api = $this->getView()->api();
-        $query ??= $this->getView()->params()->fromQuery();
+        $api = $view->api();
+        $query ??= $view->params()->fromQuery();
         $queryTypes = [
             'eq' => $translate('is exactly'),
             'neq' => $translate('is not exactly'),
@@ -41,6 +42,8 @@ class SearchFilters extends AbstractHelper
             'nres' => $translate('is not resource with ID'),
             'ex' => $translate('has any value'),
             'nex' => $translate('has no values'),
+            'dt' => $translate('has data type'),
+            'ndt' => $translate('does not have data type'),
         ];
 
         foreach ($query as $key => $value) {
@@ -117,6 +120,11 @@ class SearchFilters extends AbstractHelper
                                 }
                             }
 
+                            // If this is a data type query, convert the value to
+                            // the data type's label.
+                            $value = in_array($queryType, ['dt', 'ndt'])
+                                ? $view->dataType()->getLabel($value)
+                                : $value;
                             $filters[$filterLabel][] = $value;
                             $index++;
                         }
@@ -230,14 +238,14 @@ class SearchFilters extends AbstractHelper
             }
         }
 
-        $result = $this->getView()->trigger(
+        $result = $view->trigger(
             'view.search.filters',
             ['filters' => $filters, 'query' => $query],
             true
         );
         $filters = $result['filters'];
 
-        return $this->getView()->partial(
+        return $view->partial(
             $partialName,
             [
                 'filters' => $filters,

application/view/common/advanced-search/properties.phtml
@@ -85,8 +85,11 @@ if ($this->status()->isSiteRequest()) {
                 <?php echo $queryOption('nres', $property, 'type', $translate('is not resource with ID')); ?>
                 <?php echo $queryOption('ex', $property, 'type', $translate('has any value')); ?>
                 <?php echo $queryOption('nex', $property, 'type', $translate('has no values')); ?>
+                <?php echo $queryOption('dt', $property, 'type', $translate('has data type')); ?>
+                <?php echo $queryOption('ndt', $property, 'type', $translate('does not have data type')); ?>
             </select>
             <?php echo $queryText($property, $index); ?>
+            <?php echo $this->dataType()->getSelect($stem . '[text]', $property['text'] ?? null, ['class' => 'query-text-data-type'], ['empty_option' => $translate('Select data type…')]); ?>
             <button type="button" class="o-icon-delete remove-value button" aria-label="<?php echo $translate('Remove value'); ?>" title="<?php echo $translate('Remove value'); ?>"></button>
         </div>
         <?php

commit b15bb5c4939bc851fd118ebfbe678184df8c3548

Fix advanced search regression from #2246
The JS that "cleans" the search query was triggering on the second,
disabled "text" entry for every row, causing it to think the row is
empty and throw it out.

application/asset/js/global.js
@@ -337,7 +337,7 @@ var Omeka = {
 
     // Clean the search query of empty or otherwise unneeded inputs.
     cleanSearchQuery: function(form) {
-        form.find(':input').each(function(index) {
+        form.find(':input:enabled').each(function(index) {
             const input = $(this);
             const inputName = input.attr('name');
             const inputValue = input.val();
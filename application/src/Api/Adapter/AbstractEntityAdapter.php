7acf7dede2fdd66aadc2dee668edc1a2b69d0066
Jim Safley <jimsafley@gmail.com>
2026-04-24 15:45:56 -0400

    Validate vocabulary prefix against allowed character pattern (#2478)
    (fix #2475)

--- application/src/Api/Adapter/AbstractEntityAdapter.php
+++ application/src/Api/Adapter/AbstractEntityAdapter.php
@@ -10,6 +10,7 @@ use Omeka\Api\Response;
 use Omeka\Db\QueryBuilder as OmekaQueryBuilder;
 use Omeka\Entity\User;
 use Omeka\Entity\EntityInterface;
+use Omeka\Entity\Vocabulary;
 use Omeka\Stdlib\ErrorStore;
 use Laminas\EventManager\Event;
 
@@ -787,7 +788,7 @@ abstract class AbstractEntityAdapter extends AbstractAdapter implements EntityAd
      */
     public function isTerm($term)
     {
-        return (bool) preg_match('/^[a-z0-9-_]+:[a-z0-9-_]+$/i', $term);
+        return (bool) preg_match(Vocabulary::TERM_REGEX, $term);
     }
 
     /**

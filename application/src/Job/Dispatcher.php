ece9476a5a61e176134a56796d329140929b15cc
garrett armstrong <garrett.w.armstrong@gmail.com>
2026-08-24 09:43:45 -0400

    Update Dispatcher.php

--- application/src/Job/Dispatcher.php
+++ application/src/Job/Dispatcher.php
@@ -80,7 +80,11 @@ class Dispatcher
         $job->setStatus(Job::STATUS_STARTING);
         $job->setClass($class);
         $job->setArgs($args);
-        $owner = $this->auth->getIdentity(); if ($owner) { $owner = $this->entityManager->getReference(\Omeka\Entity\User::class, $owner->getId()); } $job->setOwner($owner);
+        $owner = $this->auth->getIdentity();
+        if ($owner) {
+            $owner = $this->entityManager->getReference(\Omeka\Entity\User::class, $owner->getId());
+        }
+        $job->setOwner($owner);
         $this->entityManager->persist($job);
         $this->entityManager->flush();
 
